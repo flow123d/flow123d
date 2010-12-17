050c5b7e7a8029b092d134905a6cf1fb87d982b8
Jiri Jenicek <jiri.jenicek@tul.cz>
2010-12-17 14:52:02 +0000

    * copyright info, svn:keywords and some documentation for source code

--- src/la_linsys.cc
+++ src/la_linsys.cc
@@ -1,9 +1,28 @@
 /*!
- * $Id: la_linsys.cc 613 2010-07-27 07:26:11Z jan.brezina $
- * $Revision: 613 $
- * $LastChangedBy: jan.brezina $
- * $LastChangedDate: 2010-07-27 09:26:11 +0200 (Tue, 27 Jul 2010) $
  *
+ * Copyright (C) 2007 Technical University of Liberec.  All rights reserved.
+ *
+ * Please make a following refer to Flow123d on your project site if you use the program for any purpose,
+ * especially for academic research:
+ * Flow123d, Research Centre: Advanced Remedial Technologies, Technical University of Liberec, Czech Republic
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License version 3 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if not,
+ * write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 021110-1307, USA.
+ *
+ *
+ * $Id$
+ * $Revision$
+ * $LastChangedBy$
+ * $LastChangedDate$
+ *
+ * @file
  * @brief   Wrappers for linear systems based on MPIAIJ and MATIS format.
  * @author  Jan Brezina
  *
