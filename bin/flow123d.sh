96768492697f4c58a29a7cb73cbbc7e0b268fe72
Jiri Hnidek <jiri.hnidek@tul.cz>
2011-08-19 08:41:38 +0000

    * WIP: script for running flow123d  - It parses more parameters and set up some variables

--- bin/flow123d.sh
+++ bin/flow123d.sh
@@ -30,7 +30,6 @@ FLOW123D="./flow123d"
 # Relative path to Flow123d binary from current/working directory
 FLOW123D="${0%/*}/${FLOW123D}"
 
-
 # Print help to this script
 function print_help {
 	echo "SYNTAX: flow123d.sh [OPTIONS] -i INI_FILE [ -f FLOW_PARAMS]"
@@ -39,6 +38,8 @@ function print_help {
 	echo "    -h              Print this help"
 	echo "    -t TIMEOUT      Flow123d can be executed only TIMEOU seconds"
 	echo "    -m MEM          Flow123d can use only MEM bytes"
+	echo "    -n NICE         Run Flow123d with changed (lower) priority"
+	echo "    -o OUT_FILE     Stdout and Stderr will be redirected to OUT_FILE"
 	echo "    -i INI_FILE     Flow123d will load configuration from INI_FILE"
 	echo "    -f FLOW_PARAMS  Flow123d will use it's specific params"
 	echo ""
@@ -48,7 +49,7 @@ function print_help {
 unset INI_FILE
 
 # Parse arguments with bash builtin command getopts
-while getopts ":ht:m:i:f:" opt
+while getopts ":ht:m:n:o:i:f:" opt
 do
 	case ${opt} in
 	h)
@@ -60,6 +61,12 @@ do
 		;;
 	m)
 		MEM="${OPTARG}"
+		;;
+	n)
+		NICE="${OPTARG}"
+		;;
+	o)
+		OUT_FILE="${OPTARG}"
 		;;
 	i)
 		# -i has to be followed by ini file; e.g.: "flow.ini"
@@ -79,17 +86,110 @@ do
 	esac
 done
 
+# Check if Flow123d exists and it is executable file
+if ! [ -x "${FLOW123D}" ]
+then
+	echo "Error: can't execute ${FLOW123D}"
+	exit 1
+fi
+
 # Was any ini file set?
 if [ ! -n "${INI_FILE}" ]
 then
 	echo ""
-	echo "Error: no ini file"
+	echo "Error: No ini file"
 	echo ""
 	print_help
 	exit 1
 else
-	#"${FLOW123D}" -S "${INI_FILE}" ${FLOW_PARAMS}
-	echo "timeout: ${TIMEOUT} mem_limit: ${MEM} ini_file: ${INI_FILE} ${FLOW_PARAMS}"
+
+	# Was memory limit set?
+	if [ -n "${MEM}" ]
+	then
+		# Set up memory limits that prevent too allocate too much memory.
+		# The limit of virtual memory is 200MB (memory, that could be allocated)
+		MEM_LIMIT=`expr ${MEM} \* 1000`
+		ulimit -S -v 200000
+	fi
+	
+	# Was nice set?
+	if [ ! -n "${NICE}" ]
+	then
+		NICE=0
+	fi
+	
+	# Was output file set?
+	if [ -n "${OUT_FILE}" ]
+	then
+		FLOW123D_OUTPUT="${OUT_FILE}"
+		# Clear output file.
+		echo "" > "${FLOW123D_OUTPUT}"
+	else
+		FLOW_OUTPUT=&1
+	fi
+	
+	echo "timeout: ${TIMEOUT}"
+	echo "mem_limit: ${MEM}"
+	echo "nice: ${NICE}"
+	echo "out_file: ${OUT_FILE}"
+	echo "ini_file: ${INI_FILE}"
+	echo "flow_params: ${FLOW_PARAMS}"
+	
+	# WIP exit :-)
+	exit 0
+		
+	# Was timeout set?
+	if [ -n "${TIMEOT}" ]
+	then
+		# Flow123d runs with changed priority (19 is the lowest priority)
+		nice --adjustment="${NICE}" "${FLOW123D}" -S "${INI_FILE}" ${FLOW_PARAMS} > "${FLOW123D_OUTPUT}" 2>&1 &
+		FLOW123D_PID=$!
+		
+		echo -n "Running flow123d [proc:${NP}] ${INI_FILE} ."
+		IS_RUNNING=1
+
+		# Wait max TIMEOUT seconds, then kill flow123d processes
+		while [ ${TIMER} -lt ${TIMEOUT} ]
+		do
+			TIMER=`expr ${TIMER} + 1`
+			echo -n "."
+			#ps -o "%P %p"
+			sleep 1
+
+			# Is mpiexec and flow123d still running?
+			ps | gawk '{ print $1 }' | grep -q "${PARENT_MPIEXEC_PID}"
+			if [ $? -ne 0 ]
+			then
+				# set up, that flow123d was finished in time
+				IS_RUNNING="0"
+				break 1
+			fi
+		done
+		
+		# Was Flow123d finished during TIMEOUT or is it still running?
+		if [ ${IS_RUNNING} -eq 1 ]
+		then
+			echo " [Failed:loop]"
+			# Send SIGTERM to flow123d.
+			kill -s SIGTERM ${FLOW123D_PID} #> /dev/null 2>&1
+			EXIT_STATUS=2
+			# No other test will be executed
+			break 2
+		else
+			# Get exit status variable of flow123d
+			wait ${FLOW123D_PID}
+			FLOW123D_EXIT_STATUS=$?
+
+			# Was Flow123d finished correctly?
+			if [ ${FLOW123D_EXIT_STATUS} -eq 0 ]
+			then
+				echo " [Success:${TIMER}s]"
+			fi
+		fi
+	else
+		nice --adjustment="${NICE}" "${FLOW123D}" -S "${INI_FILE}" ${FLOW_PARAMS} > "${FLOW123D_OUTPUT}" 2>&1 &
+	fi
+	
 fi
 
 
