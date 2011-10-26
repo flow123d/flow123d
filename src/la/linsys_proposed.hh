b607b1433a2eb7f56f348f3f0e8a2e078e685212
Jan Brezina <jan.brezina@tul.cz>
2011-10-26 19:36:07 +0000

    * proposal of unifiad linsys class

--- src/la/linsys_proposed.hh
+++ src/la/linsys_proposed.hh
@@ -51,12 +51,20 @@
 #include "system/par_distribution.hh"
 
 
-// **************************************************************
-/*!  @brief  Linear System structure accepted by Solver module
+/**
+ *  @brief  Abstract linear system class.
  *
- *  The system is based on PETSc matrix A and vector of RHS (b) and solution (x),
- *  both vectors are build on the regular arrays vx, vb.
- *  CSR storage are optional and generated on demand by LinSysSetCSR.
+ *  Linear system consists of Matrix, RHS and solution.
+ *  It provides general methods for:
+ *  - matrix preallocation
+ *  - assembling matrix and RHS
+ *  - application of linear constrains (Dirichlet conditions) either during assembly or
+ *    on assembled system
+ *  - solution of the system
+ *  - output in Matlab format
+ *
+ *  Method operates on the system as single object. But some methods for matrix only manipulation
+ *  can be provided until we have matrix as separate class.
  */
 
 class LinSys
@@ -71,77 +79,114 @@ public:
     } SetValuesMode;
 
     typedef enum {
-        MAT_MPIAIJ,
-        MAT_IS
-    } SetType;
+        PETSC_MPIAIJ_preallocate_by_assembly,
+        PETSC_MPIAIJ_assembly_by_triples,
+        BDDC,
+        PETSC_schur_complement   // possibly we can implement Schur as another kind of lin solver
+    } LinSysType;
 
-    /// Construct a parallel system with given local size.
+    /**
+     * Constructor.
+     * Constructor of abstract class should not be called directly, but is used for initialization of member common
+     * to all particular solvers.
+     *
+     * @param lsize - local size of the solution vector
+     * @param sol_array - optionally one can provide pointer to array allocated to size lsize, where
+     *  the solution should be stored,
+     */
     LinSys(unsigned int lsize, double *sol_array = NULL);
 
-    SetType  type;   ///< MAT_IS or MAT_MPIAIJ anyone can inquire my type
+    /// Particular type of the linear system.
+    LinSysType  type;   ///< MAT_IS or MAT_MPIAIJ anyone can inquire my type
 
-    /// @name access members @{
-    /// Get global system size.
+
+    /**
+     *  Returns global system size.
+     */
     inline unsigned int size()
     { return vec_ds.size(); }
-    /// Get local system size.
+
+    /**
+     * Returns local system size. (for partitioning of solution vectors)
+     * for PETSC_MPIAIJ it is also partitioning of the matrix
+     */
     inline unsigned int vec_lsize()
     { return vec_ds.lsize(); }
-    /// Get distribution of rows.
+
+    /**
+     * Returns whole Distribution class for distribution of the solution.
+     */
     inline const Distribution &ds()
     { return vec_ds; }
-    /// Get matrix.
+
+    /**
+     * Returns PETSC matrix (only for PETSC solvers)
+     */
     inline const Mat &get_matrix()
     { return matrix; }
 
-    /// Get subdomain matrix.
-    inline const Mat &get_matrix_sub()
-    { 
-       if      (type == MAT_IS)
-       {
-	  return local_matrix;
-       }
-    }
-
-    /// Get RHS.
+    /**
+     * Returns RHS vector  (only for PETSC solvers)
+     */
     inline const Vec &get_rhs()
     { return rhs; }
-    /// Get solution.
+
+    /**
+     *  Returns PETSC vector with solution. Underlying array can be provided on construction.
+     *  Can this be implemented for BDDC?
+     */
     inline const Vec &get_solution()
     { return solution; }
+
+    /**
+     * Returns local part of solution vector.
+     * Can this be implemented for BDDC?
+     */
     inline double *get_solution_array()
     { return v_solution; }
-    /// @}
 
+    /**
+     * Switch linear system into allocating assembly. (only for PETSC_MPIAIJ_preallocate_by_assembly)
+     */
     virtual void start_allocation()=0;
+
+    /**
+     * Switch linear system into adding assembly. (the only one supported by triplets ??)
+     */
     void start_add_assembly();
+    /**
+     * Switch linear system into insert assembly. (not currently used)
+     */
     void start_insert_assembly();
-    void finalize(MatAssemblyType assembly_type=MAT_FINAL_ASSEMBLY);
 
-    virtual void preallocate_matrix()=0;
-    virtual void preallocate_values(int nrow,int *rows,int ncol,int *cols)=0;
 
-    virtual void view_local_matrix()=0;
+    /**
+     * May not be necessary. For PETSC this should call MatEndAssembly with MAT_PARTIAL_ASSEMBLY
+     */
+    void partial_assembly();
+    /**
+     * Finish assembly of the whole system. For PETSC this should call MatEndAssembly with MAT_FINAL_ASSEMBLY
+     */
+    void finish_assembly();
 
-    /// Set full rectangular submatrix of the system matrix.
-    inline void mat_set_values(int nrow,int *rows,int ncol,int *cols,PetscScalar *vals)
-    {
-        switch (status) {
-            case INSERT:
-            case ADD:
-                MatSetValues(matrix,nrow,rows,ncol,cols,vals,(InsertMode)status); break;
-            case ALLOCATE:
-                preallocate_values(nrow,rows,ncol,cols); break;
-            default: DBGMSG("LS SetValues with non allowed insert mode.\n");
-        }
-    }
+    /**
+     *  Assembly full rectangular submatrix into the system matrix.
+     *  Should be virtual, implemented differently in  particular solvers.
+     */
+    virtual void mat_set_values(int nrow,int *rows,int ncol,int *cols,PetscScalar *vals) = 0;
 
-    /// Set one element of the system matrix.
 
+    /**
+     * Shortcut for assembling just one element into the matrix.
+     * Similarly we can provide method accepting armadillo matrices.
+     */
     inline void mat_set_value(int row,int col,PetscScalar val)
     { mat_set_values(1,&row,1,&col,&val); }
 
-    /// Set values of the system right-hand side.
+    /**
+     *  Set values of the system right-hand side.
+     *  Should be virtual, implemented differently in  particular solvers.
+     */
     inline void rhs_set_values(int nrow,int *rows,PetscScalar *vals)
     {
         switch (status) {
@@ -153,27 +198,83 @@ public:
         }
     }
 
-    /// Set one value in the right-hand side.
+    /**
+     * Shorcut for assembling just one element into RHS vector.
+     */
     inline void rhs_set_value(int row,PetscScalar val)
     { rhs_set_values(1,&row,&val); }
 
-    /// Set values in the system matrix and values in the right-hand side vector on corresponding rows.
-    inline void set_values(int nrow,int *rows,int ncol,int *cols,PetscScalar *mat_vals, PetscScalar *rhs_vals)
+    /**
+     * Shortcut to assembly into matrix and RHS in one call.
+     * This can also apply constrains at assembly time (only in add assembly regime).
+     *
+     * Constrains can either be set before through add_constrain. Or by additional parameters if we
+     * have only per element knowledge about boundary conditions.
+     *
+     */
+    inline void set_values(int nrow,int *rows,int ncol,int *cols,PetscScalar *mat_vals, PetscScalar *rhs_vals,
+            std::vector<bool> &constrains_row_mask=std::vector(0), double * constrain_values=NULL)
     {
         mat_set_values(nrow, rows, ncol, cols, mat_vals);
         rhs_set_values(nrow, rows, rhs_vals);
     }
 
+    /**
+     * Adds Dirichlet constrain.
+     * @param row - global numeb of row that should be eliminated.
+     * @param value - solution value at the given row
+     */
+    void add_constrain(int row, double value);
+
+    /**
+     * Apply constrains to assembled matrix. Constrains are given by pairs: global row index, value.
+     * i.e. typedef pair<unsigned int, double> Constrain;
+     *
+     * What is th meaning of ( const double factor ) form Cambridge code?
+     */
+
+    void apply_constrains(std::vector<Constrain> & constraints);
+
+
+    /**
+     * Solve the system.
+     *
+     * parameters should by provided in input file (currently INI file, but will be changed to JSON)
+     *
+     * If we realize that we need to set something, rather add some set_* function.
+     *
+     * double tol = 1.e-7,                        //!< tolerance on relative residual ||res||/||rhs||
+       int  numLevels = 2,                        //!< number of levels
+       std::vector<int> *  numSubAtLevels = NULL, //!< number of subdomains at levels
+       int verboseLevel = 0,                      //!< level of verbosity of BDDCML library
+                                                  //!< ( 0 - only fatal errors reported,
+                                                  //!<   1 - mild output,
+                                                  //!<   2 - detailed output )
+       int  maxIt = 1000,                         //!< maximum number of iterations
+       int  ndecrMax = 30 );                      //!< maximum number of iterations with non-decreasing residual
+                                                  //!< ( used to stop diverging process )
+     *
+     *
+     * Returns convergence reason (form PETSC)
+     */
+    virtual int solve();
+
+
+    /**
+     * Provides user knowledge about symmetry.
+     */
     inline void set_symmetric(bool flag = true)
     {
         symmetric = flag;
         if (!flag) set_positive_definite(false);
     }
 
-
     inline bool is_symmetric()
     { return symmetric; }
 
+    /**
+     * Provides user knowledge about positive definiteness.
+     */
     inline void set_positive_definite(bool flag = true)
     {
         positive_definite = flag;
@@ -184,12 +285,25 @@ public:
     { return positive_definite; }
 
 
-    /// Output the system in the Matlab format possibly with given ordering.
+    /**
+     *  Output the system in the Matlab format possibly with given ordering.
+     *  Rather we shoud provide output operator <<, since it is more flexible.
+     * /
     void view(std::ostream output_stream, int * output_mapping = NULL);
 
     virtual ~LinSys();
 
 protected:
+    /**
+     * Protected methods used in preallocate_by_assembly solvers.
+     */
+    virtual void preallocate_matrix()=0;
+    /**
+     * Protected methods used in preallocate_by_assembly solvers.
+     */
+    virtual void preallocate_values(int nrow,int *rows,int ncol,int *cols)=0;
+
+
     Distribution vec_ds;        ///< Distribution of continuous blocks of system rows among the processors.
     bool     symmetric;         ///< Flag for the symmetric system.
     bool     positive_definite; ///< Flag for positive definite system.
