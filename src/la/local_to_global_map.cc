e76120dd9ef902c2fefea5645f8d1687901acc01
Jan Brezina <jan.brezina@tul.cz>
2012-04-01 17:12:13 +0000

    * fixed include of distribution

--- src/la/local_to_global_map.cc
+++ src/la/local_to_global_map.cc
@@ -6,7 +6,7 @@
  */
 
 #include <la/local_to_global_map.hh>
-#include <la/distribution.hh>
+#include <system/par_distribution.hh>
 
 
 LocalToGlobalMap::LocalToGlobalMap(const Distribution &any_distr)
