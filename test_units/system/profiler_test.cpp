40713f3fe0c2bf753a53d2231a0af151830fe73d
Jan Brezina <jan.brezina@tul.cz>
2012-11-02 14:22:24 +0000

    * updated README - included information from both Windows HOWTO * delete Windows HOWTO * basic test of the Profiler

--- test_units/system/profiler_test.cpp
+++ test_units/system/profiler_test.cpp
@@ -7,8 +7,21 @@
 
 
 #define DEBUG
+#define DEBUG_PROFILER
 
 #define TEST_USE_MPI
-
 #include <gtest_mpi.hh>
 
+#include "system/sys_profiler.hh"
+
+TEST(Profiler, basic_usage) {
+
+    Profiler::initialize(MPI_COMM_WORLD);
+    START_TIMER("test_tag");
+
+    int j;
+    for(int i=0;i<1000000;i++) j=2*i;
+
+    END_TIMER("test_tag");
+    Profiler::uninitialize();
+}
