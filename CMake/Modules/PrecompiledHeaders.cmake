18b6fe5a052f3ff0a60d21ad9ccc0aea6a8317e9
Pavel Exner <pavelexner@gmail.com>
2012-11-20 11:36:49 +0000

    - ccache support created (link to the compiler with ccache) - to use install ccache and edit makefile.in.cmake

--- CMake/Modules/PrecompiledHeaders.cmake
+++ CMake/Modules/PrecompiledHeaders.cmake
@@ -10,7 +10,10 @@
 #
 # GCC: The precompiled header is always automatically included from
 # every header file.
+
+# _targetName is the name of the target which depends on the headers
 MACRO(ADD_PRECOMPILED_HEADER _targetName _input)
+  # gets file name without extension NAME_WE (possible PATH|ABSOLUTE|NAME|EXT|NAME_WE)
   GET_FILENAME_COMPONENT(_inputWe ${_input} NAME_WE)
   SET(pch_source ${_inputWe}.cpp)
   FOREACH(arg ${ARGN})
@@ -21,6 +24,7 @@ MACRO(ADD_PRECOMPILED_HEADER _targetName _input)
     ENDIF(arg STREQUAL FORCEINCLUDE)
   ENDFOREACH(arg)
 
+# for Microsoft Visual Studio
   IF(MSVC)
     GET_TARGET_PROPERTY(sources ${_targetName} SOURCES)
     SET(_sourceFound FALSE)
@@ -45,12 +49,16 @@ SET_SOURCE_FILES_PROPERTIES(${_source} PROPERTIES COMPILE_FLAGS "${PCH_COMPILE_F
     ENDIF(NOT _sourceFound)
   ENDIF(MSVC)
 
+# for G++
   IF(CMAKE_COMPILER_IS_GNUCXX)
     GET_FILENAME_COMPONENT(_name ${_input} NAME)
+    # sets .hh files from input as _source
     SET(_source "${CMAKE_CURRENT_SOURCE_DIR}/${_input}")
+    # sets name and makes output directory [headerfile].gch
     SET(_outdir "${CMAKE_CURRENT_BINARY_DIR}/${_name}.gch")
     MAKE_DIRECTORY(${_outdir})
-    SET(_output "${_outdir}/.c++")
+    # sets the name of the precompiled header
+    SET(_output "${_outdir}/${CMAKE_BUILD_TYPE}.c++")
     
     STRING(TOUPPER "CMAKE_CXX_FLAGS_${CMAKE_BUILD_TYPE}" _flags_var_name)
     SET(_compiler_FLAGS ${${_flags_var_name}})
@@ -64,13 +72,19 @@ SET_SOURCE_FILES_PROPERTIES(${_source} PROPERTIES COMPILE_FLAGS "${PCH_COMPILE_F
     LIST(APPEND _compiler_FLAGS ${_directory_flags})
 
     SEPARATE_ARGUMENTS(_compiler_FLAGS)
-    MESSAGE("${CMAKE_CXX_COMPILER} -DPCHCOMPILE ${_compiler_FLAGS} -x c++-header -o {_output} ${_source}")
+    MESSAGE("command: ${CMAKE_CXX_COMPILER} -DPCHCOMPILE ${_compiler_FLAGS} -x c++-header -o {_output} ${_source}")
     ADD_CUSTOM_COMMAND(
       OUTPUT ${_output}
+      # the same compilation flags as for the source file are needed
+      # "-DDEBUG" which is defined in makefile.in.cmake might be missing in here 
       COMMAND ${CMAKE_CXX_COMPILER} ${_compiler_FLAGS} -x c++-header -o ${_output} ${_source}
       DEPENDS ${_source} )
+    # adds the precompiled header to the target's dependencies
     ADD_CUSTOM_TARGET(${_targetName}_gch DEPENDS ${_output})
     ADD_DEPENDENCIES(${_targetName} ${_targetName}_gch)
+    # inclusion of the precompiled header to the target is sets
+    # and "-Winvalid-pch" makes sure that you get message if somethings worng and 
+	# precompiled header is not used
     SET_TARGET_PROPERTIES(${_targetName} PROPERTIES COMPILE_FLAGS "-include ${_name} -Winvalid-pch")
   ENDIF(CMAKE_COMPILER_IS_GNUCXX)
-ENDMACRO()
+ENDMACRO(ADD_PRECOMPILED_HEADER)
