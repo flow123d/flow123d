7d836e7e60ab03a7312a1ea1573b07720fe9dba3
Jan Stebel <jan.stebel@tul.cz>
2013-01-25 08:26:21 +0000

    Corrected mesh unit test.

--- test_units/mesh/gmsh_reader_test.cpp
+++ test_units/mesh/gmsh_reader_test.cpp
@@ -13,54 +13,6 @@
 #include "mesh/mesh.h"
 #include "mesh/msh_gmshreader.h"
 
-// simplest cube 123d
-string gmsh_mesh = R"CODE(
-$MeshFormat
-2.2 0 8
-$EndMeshFormat
-$Nodes
-8
-1 1 1 1
-2 -1 1 1
-3 -1 -1 1
-4 1 -1 1
-5 1 -1 -1
-6 -1 -1 -1
-7 1 1 -1
-8 -1 1 -1
-$EndNodes
-$Elements
-9
-1 1 2 37 20 7 3
-2 2 2 38 34 6 3 7
-3 2 2 38 36 3 1 7
-4 4 2 39 40 3 7 1 2
-5 4 2 39 40 3 7 2 8
-6 4 2 39 40 3 7 8 6
-7 4 2 39 42 3 7 6 5
-8 4 2 39 42 3 7 5 4
-9 4 2 39 42 3 7 4 1
-$EndElements
-)CODE";
-
-string ngh_input = R"CODE(
-$NeighbourFormat
-1.0 0 8
-$EndNeighbourFormat
-$Neighbours
-10
-0   11 2 4 2 5 3 
-1   11 2 5 2 6 3 
-2   11 2 7 2 8 3 
-3   11 2 8 2 9 3 
-4   20 1 2 1 1.0
-5   20 1 3 2 1.0
-6   20 3 4 3 1.0
-7   20 2 6 2 1.0
-8   20 2 7 3 1.0
-9   20 3 9 2 1.0
-$EndNeighbours
-)CODE";
 
 
 TEST(GMSHReader, read_mesh_from_stream) {
@@ -74,9 +26,6 @@ TEST(GMSHReader, read_mesh_from_stream) {
     GmshMeshReader reader(ss);
 
     reader.read_mesh(&mesh);
-
-    stringstream ngh_ss(ngh_input);
-    mesh.setup_topology(&ngh_ss);
 
     EXPECT_EQ(9, mesh.n_elements());
 }
