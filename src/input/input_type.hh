0dbbb1857ad50f2ed328eed0ff8e8fe1d8b576ee
Jan Brezina <jan.brezina@tul.cz>
2013-01-31 10:53:08 +0000

    * added Input::Record::opt_val() method for simplier access to optional keys * actualization of TODO notes for Input module

--- src/input/input_type.hh
+++ src/input/input_type.hh
@@ -6,43 +6,21 @@
  *
  *  todo:
 
- *  - If we require the types with complex construction are instantiated as static members,
- *    + we store pointers to them in the decalration phase, we may remove pimpl idiom, and disallow copies
- *    (careful! surly there were reason for it, try to forbid copy constr. first)
- *  - this way we can recognize keys with constructed record type in the declare_key, since they
- *    are already registered in LazyTypes static list, in this case we can use lazy initialization only if we need it
- *    Workaround: use reference (to static variable) when returning Record type so that declare_key get correct pointer
- *
- *  - type_use_case_test, + demonstrace vystupu dokumentace
- *  - match metody pro Scalarni typy + converze z default hodnot
- *  - instance InputType metod a dalsich sablon v Input namespace
+ *  - explicit instantiation of templates in accessors - not so easy
  *
  *  - TYPE is obligatory key of descendants of an AbstractRecord, for consistent documentation it should be reported as
  *    a obligatory key in these Records, however it is quite complicated to have it there , and it is not necessary for
  *    check of the input since we shall look for it explicitely.
+ *
  *  - Documentation of AbstractRecord should contain TYPE and common keys, descendants should report only nonderived keys.
- *  - Descendant of an AbstractRecord should not be used directly !!
  *
- *  - copy constructory od Record, abstractRecord a Selection a Array (Pimpl idiom) by mely
- *    kontrolovat, ze vnitrni ukazatele nejsou empty, jinak jsou kopie shared_ptr neplatne, popr. je v default constructoru musime inizializovat na nulu
- *\
- *  - Doxygen doc
- *
+ *  - better Doxygen doc
  *
  *  - detailni popis pouziti deklaraci typu na systemu trid (v TypeBase)
  *
  *  - Implementovat selection tak, aby typem Enum byla templatovana jen vnitrni datova struktura ( a pochopitelne access metody, ktere potrebuji
  *    presny typ, tj
- *  - zavest priznak pro record, ktery muze byt inicializovan z hodnoty jednoho klice (ten je treba zadat)
- *    a podobne pro pole.
  *    ...
- *
- *  - ?? predelat Default na hierarchii trid (opet problem s error hlaskou) tkato by se to hlasilo pri kompilaci.
- *    ( snad lepe factory staticke funkce defalut("..."), default(), default_obligatory() )
- *
- *  - Allow non integral values to selection ??
- *
- *  - declare_key should take TypeBase as parameter to be more flexible, but is not necessary
  *
  *  - have global list of Record and selection names and guarantee the they are unique, otherwise == can be incorrect.
  *
@@ -54,13 +32,19 @@
  *  constant string during compilation, in particular it should check validity of the key string and compute the hash.
  *  This can provide some speedup for reading if it will be needed (probably not).
  *
+ *  - under C++11 we can also construct Selection s from the enum type
  *
- *   - nevytvaret deklaraci Recordu a dalsich typu runtime, ale pri kompilaci
- *    tedy jako skutecnou hierarchii trid, to by umoznilo statickou kontrolu
- *    kompatibility typu v konstruktoru Iterator<T>
- *    ??? is it possible?
+ *  - Question: allow implicit value of the TYPE key (and selection the descendant) depending on the keys that appears on the input.
+ *  - Question: Support keys with multiple possible types:
+ *    we declare more keys like: region_set_INTEGER, region_set_STRING, region_set_ARRAY, but user specify just
+ *    region_set=..., and appropriate key is used.
+ *  - NEED better access method for optional keys, so that we do not need to declare iterator. So better:
+ *    if (rec.val( const string &key_name, TYPE & variable)) {
+ *       ... use variable
+ *    }
  *
- *
+ *  Notes:
+ *  - copy constructors and usage of Pimpl idiom for more complex types is necessary due to usage of shared pointers - to create them we need copies
  */
 
 #ifndef INPUTTYPE_HH_
