d477e81f55a47c146c8e503376af5ee631bfca09
Jan Brezina <jan.brezina@tul.cz>
2013-09-05 10:09:52 +0000

    * make path correction on Windows only out of cygwin

--- CMake/Modules/CorrectWindowsPaths.cmake
+++ CMake/Modules/CorrectWindowsPaths.cmake
@@ -5,10 +5,14 @@
 #  unix-style paths into paths useable by cmake on windows
 
 macro (CONVERT_CYGWIN_PATH _path)
-  if (CYGWIN)
+
+# Some cygwin utilities (namely make) do not like DOS paths, but are happy with UNIX paths
+# On the other hand out of the cygwin, the cygpath tool doesn't need to be available.
+
+  if (WIN32 AND NOT CYGWIN)
     EXECUTE_PROCESS(COMMAND cygpath.exe -m ${${_path}}
       OUTPUT_VARIABLE ${_path})
     string (STRIP ${${_path}} ${_path})
-  endif (CYGWIN)
+  endif (WIN32 AND NOT CYGWIN)
 endmacro (CONVERT_CYGWIN_PATH)
 
