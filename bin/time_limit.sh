52a97b05df6e5e13cd6e9113c47d517323db2e80
Jan Brezina <jan.brezina@tul.cz>
2013-10-11 10:35:01 +0000

    * introduce timeout for unit tests using bin/time_limit.sh * bin/time_limit.sh - run given command and kill it after given timeout

---
+++ bin/time_limit.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ "$1" == "-t" ]
+then
+  shift
+  TIME=$1
+  shift
+else
+  echo "Usage: time_limit.sh -t <time> <command>"
+  echo "Run <command> and kill it after <time> seconds."
+fi
+
+# run command on background, take its PID
+$@ &
+PID=$!
+
+(
+  sleep $TIME; 
+  # kill only if the program still runs
+  ps cax | grep "^ *${PID}" > /dev/null;
+  if [ $? -eq 0 ]; 
+  then  kill ${PID};
+  fi
+)&
+
+# wait until the program finish
+wait $PID
