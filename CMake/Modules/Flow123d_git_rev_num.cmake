55ad87cd56d68beb8545f0d458b245e9182b7f66
Jan Brezina <jan.brezina@tul.cz>
2013-10-22 15:13:33 +0200

    Fix reporting of build info.
    - fix creation of rev_num.h , now it includes info about current git branch
    - fix displaying of build info, display even wneh no input file is given

--- CMake/Modules/Flow123d_git_rev_num.cmake
+++ CMake/Modules/Flow123d_git_rev_num.cmake
@@ -6,10 +6,11 @@ include(FindGit)
 
 if (GIT_FOUND) 
   
+  message(STATUS "PWD: ${SOURCE_DIR}")
   # get GIT branch
   execute_process(
     COMMAND ${GIT_EXECUTABLE} rev-parse --abbrev-ref HEAD
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    WORKING_DIRECTORY ${SOURCE_DIR}
     OUTPUT_VARIABLE GIT_BRANCH
     OUTPUT_STRIP_TRAILING_WHITESPACE
   )
@@ -17,18 +18,41 @@ if (GIT_FOUND)
   # Get the describe string - tag + num. of commits since tag + short hash
   execute_process(
     COMMAND git describe
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    WORKING_DIRECTORY ${SOURCE_DIR}
     OUTPUT_VARIABLE GIT_DESCRIBE
     OUTPUT_STRIP_TRAILING_WHITESPACE
   )
   
+  # Get remote branch we are tracking
+  execute_process(
+    COMMAND git branch --list -vv ${GIT_BRANCH}
+    WORKING_DIRECTORY ${SOURCE_DIR}
+    OUTPUT_VARIABLE GIT_FULL_BRANCH_INFO
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+  STRING(REGEX REPLACE ".* \\[([^]]*)\\] .*" "\\1" GIT_REMOTE_BRANCH "${GIT_FULL_BRANCH_INFO}")
+  STRING(REGEX REPLACE "([^\\w]*)/.*" "\\1" GIT_REMOTE "${GIT_REMOTE_BRANCH}")
+  
+  # Get Fetch URL of the remote
+  execute_process(
+    COMMAND git remote show -n ${GIT_REMOTE}
+    WORKING_DIRECTORY ${SOURCE_DIR}
+    OUTPUT_VARIABLE GIT_FULL_URL_INFO
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+  STRING(REGEX MATCH "Fetch URL: [^\n]*" GIT_URL "${GIT_FULL_URL_INFO}")
+  
+  
   
   message(STATUS "GIT_BRANCH: ${GIT_BRANCH}")
   message(STATUS "GIT_DESCRIBE: ${GIT_DESCRIBE}")
+  message(STATUS "GIT_REMOTE_BRANCH: ${GIT_REMOTE_BRANCH}")
+  message(STATUS "GIT_URL: ${GIT_FULL_URL_INFO}")
+  message(STATUS "GIT_URL: ${GIT_URL}")
 endif()
 
 # write a file with the SVN VERSION and URL define
-file(WRITE rev_num.h.tmp "#define _PROGRAM_REVISION_ \"${GIT_DESCRIBE}\"\n#define _PROGRAM_BRANCH_ \"${GIT_BRANCH}\"\n")
+file(WRITE rev_num.h.tmp "#define _GIT_REVISION_ \"${GIT_DESCRIBE}\"\n#define _GIT_BRANCH_ \"${GIT_BRANCH}\"\n#define _GIT_URL_ \"${GIT_URL}\"")
 
 # copy the file to the final header only if the version changes
 # reduces needless rebuilds
