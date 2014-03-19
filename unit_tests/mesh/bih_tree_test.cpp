e913eca19751e499f8e47d63a5c8e75d517baccf
dflanderka <t426.001@centrum.cz>
2014-03-19 10:05:04 +0100

    Optimization of BIH tree unit test.
    - add test of find_point method
    - add using of timer to test
    - optimization of test class inner structure

--- unit_tests/mesh/bih_tree_test.cpp
+++ unit_tests/mesh/bih_tree_test.cpp
@@ -7,7 +7,8 @@
 
 
 
-#include <flow_gtest.hh>
+#define TEST_USE_MPI
+#include <flow_gtest_mpi.hh>
 #include <cmath>
 #include <algorithm>
 #include <fstream>
@@ -23,63 +24,101 @@ class BIH_test : public testing::Test {
 public:
 	void create_tree(const FilePath &mesh_file) {
 
+		START_TIMER("create mesh");
 		GmshMeshReader reader(mesh_file);
 		mesh = new Mesh();
 		reader.read_mesh(mesh);
+		END_TIMER("create mesh");
 
 	    int leaf_size_limit = 10;
+	    START_TIMER("create bih tree");
 	    bt = new BIHTree(mesh, leaf_size_limit);
+	    END_TIMER("create bih tree");
 
 	    EXPECT_EQ(mesh->n_elements(), bt->get_element_count());
+
+	    this->init_random_gen();
+
+	}
+
+	void init_random_gen() {
+		auto tbox_min = bt->tree_box().min();
+		auto tbox_max = bt->tree_box().max();
+		rx_ = std::uniform_real_distribution<>( tbox_min[0] , tbox_max[0] );
+		ry_ = std::uniform_real_distribution<>( tbox_min[0] , tbox_max[0] );
+		rz_ = std::uniform_real_distribution<>( tbox_min[0] , tbox_max[0] );
+	}
+
+	BoundingBox::Point r_point() {
+		return BoundingBox::Point( {rx_(this->r_gen), ry_(this->r_gen), rz_(this->r_gen)} );
 	}
 
 	void test_find_boxes() {
-		/*
-		auto compare = [this](unsigned int a, unsigned int b) -> bool {
-			return this->mesh->element[a].centre()[0] < this->mesh->element[b].centre()[0];
-		};*/
-		auto compare = [this](unsigned int a, unsigned int b) {
-			return this->mesh->element(a).id() < this->mesh->element(b).id();
-		};
+		test_insec_elements();
+		test_insec_points();
+
+		Profiler::instance()->output(MPI_COMM_WORLD, cout);
+	}
 
 
-		auto tbox_min = bt->tree_box().min();
-		auto tbox_max = bt->tree_box().max();
-		std::uniform_real_distribution<> rx( tbox_min[0] , tbox_max[0] );
-		std::uniform_real_distribution<> ry( tbox_min[1] , tbox_max[1] );
-		std::uniform_real_distribution<> rz( tbox_min[2] , tbox_max[2] );
-		auto g = this->r_gen;
-		auto r_point = [&]() {
-			return BoundingBox::Point( {rx(g), ry(g), rz(g)} );
-		};
-
+	void test_insec_elements() {
 		for(int i=0; i < n_test_trials; i++) {
 			BoundingBox box( vector<BoundingBox::Point>({r_point(), r_point()}) );
 
-
-			cout << "\n-------------------------" << endl;
-			cout << "box: " << box <<endl;
+			//cout << "\n-------------------------" << endl;
+			//cout << "box: " << box <<endl;
 
 			vector<unsigned int> bf_result;
 			FOR_ELEMENTS(mesh, ele) {
 				EXPECT_EQ( box.intersect(ele->bounding_box()) , ele->bounding_box().intersect(box) );
 				if (box.intersect(ele->bounding_box()) ) bf_result.push_back(ele.index());
 			}
-			std::sort(bf_result.begin(), bf_result.end(), compare);
 
 			vector<unsigned int> result_vec;
+			START_TIMER("find bounding box");
 			bt->find_bounding_box(box, result_vec);
-			std::sort(result_vec.begin(), result_vec.end(), compare);
+			END_TIMER("find bounding box");
+			std::sort(result_vec.begin(), result_vec.end());
 
-
-			cout << endl << "full search: " << endl;
-			for(auto i_el : bf_result) cout << " " << this->mesh->element(i_el).id();
-			cout << endl << "bih search: " << endl;
-			for(auto i_el : result_vec) cout << " " << this->mesh->element(i_el).id();
+			//cout << endl << "full search: " << endl;
+			//for(unsigned int i_el : bf_result) cout << " " << this->mesh->element(i_el).id();
+			//cout << endl << "bih search: " << endl;
+			//for(unsigned int i_el : result_vec) cout << " " << this->mesh->element(i_el).id();
 
 			ASSERT_EQ(bf_result.size(), result_vec.size());
 			for(int j=0; j< bf_result.size(); j++) {
 				EXPECT_EQ(bf_result[j], result_vec[j]);
+			}
+		}
+	}
+
+
+	void test_insec_points() {
+		for(int i=0; i < n_test_trials; i++) {
+			BoundingBox::Point point( r_point() );
+
+			//cout << "\n-------------------------" << endl;
+			//cout << "point: " << point << endl;
+
+			vector<unsigned int> bf_point_result;
+			FOR_ELEMENTS(mesh, ele) {
+				if (ele->bounding_box().contains_point(point) ) bf_point_result.push_back(ele.index());
+			}
+
+			vector<unsigned int> result_point_vec;
+			START_TIMER("find point");
+			bt->find_point(point, result_point_vec);
+			END_TIMER("find point");
+			std::sort(result_point_vec.begin(), result_point_vec.end());
+
+			//cout << endl << "full search: " << endl;
+			//for(unsigned int i_el : bf_point_result) cout << " " << this->mesh->element(i_el).id();
+			//cout << endl << "bih search: " << endl;
+			//for(unsigned int i_el : result_point_vec) cout << " " << this->mesh->element(i_el).id();
+
+			ASSERT_EQ(bf_point_result.size(), result_point_vec.size());
+			for(int j=0; j< bf_point_result.size(); j++) {
+				EXPECT_EQ(bf_point_result[j], result_point_vec[j]);
 			}
 		}
 	}
@@ -101,6 +140,7 @@ public:
 	std::mt19937	r_gen;
 	Mesh *mesh;
 	BIHTree *bt;
+	std::uniform_real_distribution<> rx_, ry_, rz_;
 	const static int n_test_trials=5;
 };
 
@@ -114,6 +154,27 @@ TEST_F(BIH_test, find_bounding_box_1) {
 
 TEST_F(BIH_test, find_bounding_box_2) {
 	FilePath mesh_file( string(UNIT_TESTS_SRC_DIR) + "/fields/simplest_cube_3d.msh", FilePath::input_file);
+	this->create_tree(mesh_file);
+
+	this->test_find_boxes();
+}
+
+TEST_F(BIH_test, find_bounding_box_3) {
+	FilePath mesh_file( string(UNIT_TESTS_SRC_DIR) + "/mesh/test_7590_elem.msh", FilePath::input_file);
+	this->create_tree(mesh_file);
+
+	this->test_find_boxes();
+}
+
+TEST_F(BIH_test, find_bounding_box_4) {
+	FilePath mesh_file( string(UNIT_TESTS_SRC_DIR) + "/mesh/test_188_elem.msh", FilePath::input_file);
+	this->create_tree(mesh_file);
+
+	this->test_find_boxes();
+}
+
+TEST_F(BIH_test, find_bounding_box_5) {
+	FilePath mesh_file( string(UNIT_TESTS_SRC_DIR) + "/mesh/test_27936_elem.msh", FilePath::input_file);
 	this->create_tree(mesh_file);
 
 	this->test_find_boxes();
