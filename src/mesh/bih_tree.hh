f01a61ef4dad9af88d1f69818f300b127c12c5f3
dflanderka <t426.001@centrum.cz>
2014-04-04 09:44:33 +0200

    Clean code of BIH tree.

--- src/mesh/bih_tree.hh
+++ src/mesh/bih_tree.hh
@@ -93,20 +93,6 @@ public:
     void find_point(const Space<3>::Point &point, std::vector<unsigned int> &result_list);
 
     /**
-     * Browse tree and get its typical parameters
-     * Method for gtests
-     *
-     * @param maxDepth Gets maximal depth of tree
-     * @param minDepth Gets minimal depth of tree
-     * @param avgDepth Gets average depth of tree
-     * @param leafNodesCount Gets count of all leaf nodes of tree
-     * @param innerNodesCount Gets count of all inner nodes of tree
-     * @param elementLeafCount Gets sum of elements contained in all leaf nodes
-     */
-    //void get_tree_params(unsigned int &maxDepth, unsigned int &minDepth, double &avgDepth, unsigned int &leafNodesCount,
-    //		unsigned int &innerNodesCount, unsigned int &sumElements);
-
-    /**
      * Get vector of mesh elements bounding boxes
      *
      * @return elements_ vector
@@ -116,23 +102,15 @@ public:
 protected:
     /// required reduction in size of box to allow further splitting
     static const double size_reduce_factor;
-    /// value indicates ratio of the number of element in node and number of elements of its children
-    //static const double max_grow_factor;
 
     /// create bounding boxes of element
     void element_boxes();
-    /**
-     * soft_leaf_size_limit - we try to split node if its size (number of elements) is
-     * greater then this limit. However we stop branching if number of redundant elements is to big.
-     */
-    //void create_tree(unsigned int soft_leaf_size_limit);
-    /// Creates root node of tree, finds its bounding coordinations
-    /// and pushes elements to the vector using for creating tree
-    //void create_root_node();
 
+    /// split tree node given by node_idx, distribute elements to child nodes
     void split_node(const BoundingBox &node_box, unsigned int node_idx);
-    void make_node(const BoundingBox &box, unsigned int node_idx);
 
+    /// create child nodes of node given by node_idx
+    void make_node(const BoundingBox &box, unsigned int node_idx);
 
     /**
      * For given node takes projection of centers of bounding boxes of its elements to axis given by
@@ -141,29 +119,7 @@ protected:
      * Precise median is computed for sets smaller then @p max_median_sample_size
      * estimate from random sample is used for larger sets.
      */
-    //void set_node_median(unsigned char axis, BIHNode &node);
     double estimate_median(unsigned char axis, const BIHNode &node);
-
-    /// Put indexes of elements to in_leaves_ vector if node is marked as leaf
-    //void put_leaf_elements();
-    /// Deallocate memory reserved by vectors
-    //void free_memory();
-    /// Test if processed node is at the end of level during creating of tree
-    //void test_new_level();
-    /**
-     * Sort elements in node to 3 groups (contained only in left child, in both children, only in right child)
-     *
-     * @param bound1 Bound of sorting between first and second group
-     * @param bound2 Bound of sorting between second and third group
-     */
-    //void sort_elements(unsigned int &bound1, unsigned int &bound2);
-    /**
-     * Distribute elements into subareas
-     *
-     * @param leftChild Left child of actually processed node
-     * @param rightChild Right child of actually processed node
-     */
-    //void distribute_elements(BIHNode &left_child, BIHNode &right_child);
 
     /// mesh
     Mesh* mesh_;
@@ -173,23 +129,13 @@ protected:
     std::vector<BIHNode> nodes_;
     /// Main bounding box.
     BoundingBox main_box_;
-
+    /// Maximal number of elements stored in a leaf node of BIH tree.
     unsigned int leaf_size_limit;
+    /// Maximal count of BIH tree levels
     unsigned int max_n_levels;
-
-
-    /// vector stored elements for level-order walk of tree
-    //std::deque<unsigned int> queue_;
-
-
-    //std::deque<BoundingBox> box_queue_;
 
     /// vector stored element indexes in leaf nodes
     std::vector<unsigned int> in_leaves_;
-    /// temporary vector stored element indexes in last complete level of the BFS tree
-    //std::vector<unsigned int> list_element_index_;
-    /// temporary vector stored element indexes in actually constructed level of the BFS tree
-    //std::vector<unsigned int> list_element_index_next_;
     /// temporary vector stored values of coordinations for calculating median
     std::vector<double> coors_;
 
