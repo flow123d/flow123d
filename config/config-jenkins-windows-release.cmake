5d776424b230ec8bf0568c7fba89153555fdcea8
Jan Brezina <jan.brezina@tul.cz>
2014-05-14 17:28:17 +0200

    Update configuration for Cygwin test server.

--- config/config-jenkins-windows-release.cmake
+++ config/config-jenkins-windows-release.cmake
@@ -1,8 +1,9 @@
 # Configuration for CI server
 
-set(FLOW_CC_FLAGS "-O3 -DDEBUG_PROFILER")
+set(FLOW_BUILD_TYPE release)
 
 set(CMAKE_VERBOSE_MAKEFILE on)
 
 set(PETSC_INSTALL_CONFIG "full")
 set(USE_PYTHON "yes")
+set(BDDCML_ROOT "_INSTALL_")
