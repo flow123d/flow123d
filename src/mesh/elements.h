39ff09b7354e6082b413dcea3873f288419a3c79
Viktor Friš <viktorfris420@gmail.com>
2014-05-15 21:39:44 +0200

    returning file elements.h from master

--- src/mesh/elements.h
+++ src/mesh/elements.h
@@ -1,31 +1,31 @@
 /*!
- *
- * Copyright (C) 2007 Technical University of Liberec.  All rights reserved.
- *
- * Please make a following refer to Flow123d on your project site if you use the program for any purpose,
- * especially for academic research:
- * Flow123d, Research Centre: Advanced Remedial Technologies, Technical University of Liberec, Czech Republic
- *
- * This program is free software; you can redistribute it and/or modify it under the terms
- * of the GNU General Public License version 3 as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with this program; if not,
- * write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 021110-1307, USA.
- *
- *
- * $Id$
- * $Revision$
- * $LastChangedBy$
- * $LastChangedDate$
- *
- * @file
- * @brief ???
- *
- */
+*
+* Copyright (C) 2007 Technical University of Liberec. All rights reserved.
+*
+* Please make a following refer to Flow123d on your project site if you use the program for any purpose,
+* especially for academic research:
+* Flow123d, Research Centre: Advanced Remedial Technologies, Technical University of Liberec, Czech Republic
+*
+* This program is free software; you can redistribute it and/or modify it under the terms
+* of the GNU General Public License version 3 as published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See the GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along with this program; if not,
+* write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 021110-1307, USA.
+*
+*
+* $Id$
+* $Revision$
+* $LastChangedBy$
+* $LastChangedDate$
+*
+* @file
+* @brief ???
+*
+*/
 
 #ifndef ELEMENTS_H
 #define ELEMENTS_H
@@ -58,64 +58,83 @@ public:
 
     inline unsigned int dim() const;
     inline unsigned int index() const;
-    unsigned int n_sides() const;    // Number of sides
+    unsigned int n_sides() const; // Number of sides
     unsigned int n_nodes() const; // Number of nodes
     
     ///Gets ElementAccessor of this element
-    ElementAccessor<3> element_accessor();
+    ElementAccessor<3> element_accessor() const;
     
     double measure() const;
     arma::vec3 centre() const;
     /**
-     * Quality of the element based on the smooth and scale-invariant quality measures proposed in:
-     * J. R. Schewchuk: What is a Good Linear Element?
-     *
-     * We scale the measure so that is gives value 1 for regular elements. Line 1d elements
-     * have always quality 1.
-     */
+* Quality of the element based on the smooth and scale-invariant quality measures proposed in:
+* J. R. Schewchuk: What is a Good Linear Element?
+*
+* We scale the measure so that is gives value 1 for regular elements. Line 1d elements
+* have always quality 1.
+*/
     double quality_measure_smooth();
 
     unsigned int n_sides_by_dim(unsigned int side_dim);
-    inline SideIter side(const unsigned int loc_index); // D
+    inline SideIter side(const unsigned int loc_index);
+    inline const SideIter side(const unsigned int loc_index) const;
     Region region() const;
     inline RegionIdx region_idx() const
         { return region_idx_; }
     
-    int      pid;       // Id # of mesh partition
+    unsigned int id() const;
+
+    int pid; // Id # of mesh partition
 
     // Type specific data
-    Node** node;    // Element's nodes // D
+    Node** node; // Element's nodes
 
 
     unsigned int *edge_idx_; // Edges on sides
     unsigned int *boundary_idx_; // Possible boundaries on sides (REMOVE) all bcd assembly should be done through iterating over boundaries
                            // ?? deal.ii has this not only boundary iterators
     /**
-     * Indices of permutations of nodes on sides.
-     * It determines, in which order to take the nodes of the side so as to obtain
-     * the same order as on the reference side (side 0 on the particular edge).
-     *
-     * Permutations are defined in RefElement::side_permutations.
-     */
+* Indices of permutations of nodes on sides.
+* It determines, in which order to take the nodes of the side so as to obtain
+* the same order as on the reference side (side 0 on the particular edge).
+*
+* Permutations are defined in RefElement::side_permutations.
+*/
     unsigned int *permutation_idx_;
 
     /**
-     * Computes bounding box of element
-     */
-    void get_bounding_box(BoundingBox &bounding_box);
+* Computes bounding box of element (OBSOLETE)
+*/
+    void get_bounding_box(BoundingBox &bounding_box) const;
+
+    /**
+* Return bounding box of the element.
+*/
+    inline BoundingBox bounding_box() {
+     return BoundingBox(this->vertex_list());
+    }
+
+    /**
+* Return list of element vertices.
+*/
+    inline vector<arma::vec3> vertex_list() {
+     vector<arma::vec3> vertices(this->n_nodes());
+     for(unsigned int i=0; i<n_nodes(); i++) vertices[i]=node[i]->point();
+     return vertices;
+    }
 
 
-    unsigned int      n_neighs_vb;   // # of neighbours, V-B type (comp.)
+    unsigned int n_neighs_vb; // # of neighbours, V-B type (comp.)
                             // only ngh from this element to higher dimension edge
     Neighbour **neigh_vb; // List og neighbours, V-B type (comp.)
 
 
-    Mesh    *mesh_; // should be removed as soon as the element is also an Accessor
+    Mesh *mesh_; // should be removed as soon as the element is also an Accessor
 
 
 protected:
     // Data readed from mesh file
-    RegionIdx  region_idx_;
+    RegionIdx region_idx_;
     unsigned int dim_;
 
     friend class GmshMeshReader;
@@ -128,12 +147,11 @@ protected:
 
 
 
-#define FOR_ELEMENT_NODES(i,j)  for((j)=0;(j)<(i)->n_nodes();(j)++)
-#define FOR_ELEMENT_SIDES(i,j)  for(unsigned int j=0; j < (i)->n_sides(); j++)
-#define FOR_ELM_NEIGHS_VB(i,j)  for((j)=0;(j)<(i)->n_neighs_vb;(j)++)
+#define FOR_ELEMENT_NODES(i,j) for((j)=0;(j)<(i)->n_nodes();(j)++)
+#define FOR_ELEMENT_SIDES(i,j) for(unsigned int j=0; j < (i)->n_sides(); j++)
+#define FOR_ELM_NEIGHS_VB(i,j) for((j)=0;(j)<(i)->n_neighs_vb;(j)++)
 
 
 #endif
 //-----------------------------------------------------------------------------
 // vim: set cindent:
-
