725b46369b36579b32cf761d02841527ad96ceac
dflanderka <t426.001@centrum.cz>
2014-07-24 09:17:06 +0200

    Change of speed test.
    - add template parameter of fixture class
    - create separate method for test of fields
    - add speed test of Field::value_list method

--- unit_tests/fields/field_speed_test.cpp
+++ unit_tests/fields/field_speed_test.cpp
@@ -25,10 +25,13 @@
 using namespace std;
 
 
+static const int loop_call_count = 10000;
+
+template <typename T>
 class FieldSpeed : public testing::Test {
 public:
 	typedef typename Space<3>::Point Point;
-	typedef FieldValue<3>::Scalar::return_type ReturnType;
+	typedef typename T::return_type ReturnType;
     typedef ReturnType(FieldSpeed::*FceType)(Point&, ElementAccessor<3>&);
 
     ReturnType fce1(Point &p, ElementAccessor<3> &elm) {
@@ -49,7 +52,9 @@ public:
         ifstream in(string( mesh_file ).c_str());
         mesh_->read_gmsh_from_stream(in);
 
-        point = Point("1 2 3");
+        point_ = Point("1 2 3");
+        point_list_.reserve(10);
+        for (int i=0; i<10; i++) point_list_.push_back( Point("1 2 3") );
 
     	fce_ = new FceType[mesh_->region_db().size()];
     	data_ = new ReturnType[mesh_->region_db().size()];
@@ -82,15 +87,47 @@ public:
 		delete mesh_;
 	}
 
+	double call_test() {
+		double sum = 0.0;
+		std::vector<ReturnType> value_list(10);
 
-	static const int loop_call_count = 100000;
+		START_TIMER("single_value");
+		for (int i=0; i<10*loop_call_count; i++)
+			FOR_ELEMENTS(this->mesh_, ele) {
+				ElementAccessor<3> elm = (*ele).element_accessor();
+				sum += this->field_->value( this->point_, elm);
+			}
+		END_TIMER("single_value");
+
+		START_TIMER("all_values");
+		for (int i=0; i<loop_call_count; i++)
+			for (int j=0; j<10; j++)
+				FOR_ELEMENTS(this->mesh_, ele) {
+					ElementAccessor<3> elm = (*ele).element_accessor();
+					sum += this->field_->value( this->point_list_[j], elm);
+				}
+		END_TIMER("all_values");
+
+		START_TIMER("value_list");
+		for (int i=0; i<loop_call_count; i++)
+			FOR_ELEMENTS(this->mesh_, ele) {
+				ElementAccessor<3> elm = (*ele).element_accessor();
+				this->field_->value_list( this->point_list_, elm, value_list);
+				sum += value_list[0];
+			}
+		END_TIMER("value_list");
+		return sum;
+	}
+
+
     FceType *fce_;
     ReturnType *data_;
     ReturnType data1_;
     ReturnType data2_;
     FieldAlgorithmBase<3, FieldValue<3>::Scalar > *field_;
 	Mesh *mesh_;
-	Point point;
+	Point point_;
+	std::vector< Point > point_list_;
 
     inline ReturnType value(Point &p, ElementAccessor<3> &elm) {
     	return (this->*fce_[elm.region_idx().idx()])(p,elm);
@@ -99,41 +136,47 @@ public:
 };
 
 
-TEST_F(FieldSpeed, scalar) {
-	double sum_func=0.0, sum_array=0.0, sum_const=0.0;
+typedef ::testing::Types< FieldValue<3>::Scalar > TestedTypes;
+TYPED_TEST_CASE(FieldSpeed, TestedTypes);
+
+
+TYPED_TEST(FieldSpeed, scalar) {
+	double sum_func=0.0, sum_array=0.0;
+
 
 	START_TIMER("Speed test");
 
 	START_TIMER("function");
-	for (int i=0; i<loop_call_count; i++)
-		FOR_ELEMENTS(mesh_, ele) {
+	for (int i=0; i<10*loop_call_count; i++)
+		FOR_ELEMENTS(this->mesh_, ele) {
 			ElementAccessor<3> elm = (*ele).element_accessor();
-			sum_func += this->value( point, elm);
+			sum_func += this->value( this->point_, elm);
 		}
 	END_TIMER("function");
 
 	START_TIMER("array");
-	for (int i=0; i<loop_call_count; i++)
-		FOR_ELEMENTS(mesh_, ele) {
+	for (int i=0; i<10*loop_call_count; i++)
+		FOR_ELEMENTS(this->mesh_, ele) {
 			ElementAccessor<3> elm = (*ele).element_accessor();
 			sum_array += this->data_[elm.region_idx().idx()];
 		}
 	END_TIMER("array");
 
-	START_TIMER("field_const");
-	field_ = new FieldConstant<3, FieldValue<3>::Scalar>();
-	((FieldConstant<3, FieldValue<3>::Scalar> *)field_)->set_value(1.5);
-	for (int i=0; i<loop_call_count; i++)
-		FOR_ELEMENTS(mesh_, ele) {
-			ElementAccessor<3> elm = (*ele).element_accessor();
-			sum_const += field_->value( point, elm);
-		}
-	END_TIMER("field_const");
-
 	END_TIMER("Speed test");
 
 	EXPECT_DOUBLE_EQ( sum_func, sum_array );
-	EXPECT_DOUBLE_EQ( sum_const, (1.5 * mesh_->n_elements() * loop_call_count) );
+
+	Profiler::instance()->output(MPI_COMM_WORLD, cout);
+
+}
+
+TYPED_TEST(FieldSpeed, field_constant) {
+	this->field_ = new FieldConstant<3, FieldValue<3>::Scalar>();
+	((FieldConstant<3, FieldValue<3>::Scalar> *)this->field_)->set_value(1.5);
+
+	double sum_const = this->call_test();
+
+	EXPECT_DOUBLE_EQ( sum_const, (21 * 1.5 * this->mesh_->n_elements() * loop_call_count) );
 
 	Profiler::instance()->output(MPI_COMM_WORLD, cout);
 
