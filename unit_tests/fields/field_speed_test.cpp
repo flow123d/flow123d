45df50176bfe6e7013f667354d7d3a6bbc60cc71
dflanderka <t426.001@centrum.cz>
2014-08-07 11:27:08 +0200

    Modification of speed test.
    - usage regions and region sets of fields

--- unit_tests/fields/field_speed_test.cpp
+++ unit_tests/fields/field_speed_test.cpp
@@ -32,6 +32,67 @@ static const int loop_call_count = 1;
 static const int list_size = 10;
 
 
+string field_input = R"JSON(
+[   
+    {
+        r_set="set_1",
+
+        constant_scalar={ TYPE="FieldConstant", value=1.75 },
+        constant_vector={ TYPE="FieldConstant", value=[1.75, 2.75, 3.75] },
+        constant_vector_fixed={ TYPE="FieldConstant", value=[1.75, 3.75, 5.75] },
+       
+        formula_const_scalar={ TYPE="FieldFormula", value="1.75" },
+        formula_const_vector={ TYPE="FieldFormula", value=["1.75", "2.75", "3.75"] },
+        formula_const_vector_fixed={ TYPE="FieldFormula", value=["1.75", "3.75", "5.75"] },
+
+        formula_simple_scalar={ TYPE="FieldFormula", value="x^2" },
+        formula_simple_vector={ TYPE="FieldFormula", value=["x^2", "y^2", "z^2"] },
+        formula_simple_vector_fixed={ TYPE="FieldFormula", value=["x^2", "y^2", "z^2"] },
+
+        formula_full_scalar={ TYPE="FieldFormula", value="x+y+z+x^2+y^2+z^2" },
+        formula_full_vector={ TYPE="FieldFormula", value=["x+z+x^2+y^2+z^2", "x+y+x^2+y^2+z^2", "y+z+x^2+y^2+z^2"] },
+        formula_full_vector_fixed={ TYPE="FieldFormula", value=["x+y+x^2+y^2+z^2", "y+z+x^2+y^2+z^2", "x+z+x^2+y^2+z^2"] },
+
+        python_scalar={ TYPE="FieldPython", function="func_const", script_string="func_const(x,y,z): return ( 1.75, )" },
+        python_vector={ TYPE="FieldPython", function="func_const", script_string="func_const(x,y,z): return ( 1.75, 2.75, 3.75 )" },
+        python_vector_fixed={ TYPE="FieldPython", function="func_const", script_string="func_const(x,y,z): return ( 1.75, 3.75, 5.75 )" },
+
+        elementwise_scalar={ TYPE="FieldElementwise", gmsh_file="fields/simplest_cube_data.msh", field_name="scalar" },
+        elementwise_vector={ TYPE="FieldElementwise", gmsh_file="fields/simplest_cube_data.msh", field_name="vector_fixed" },
+        elementwise_vector_fixed={ TYPE="FieldElementwise", gmsh_file="fields/simplest_cube_data.msh", field_name="vector_fixed" }
+    },
+    {
+        r_set="set_2",
+
+        constant_scalar={ TYPE="FieldConstant", value=1.25 },
+        constant_vector={ TYPE="FieldConstant", value=[1.25, 2.25, 3.25] },
+        constant_vector_fixed={ TYPE="FieldConstant", value=[1.25, 3.25, 5.25] },
+
+        formula_const_scalar={ TYPE="FieldFormula", value="1.25" },
+        formula_const_vector={ TYPE="FieldFormula", value=["1.25", "2.25", "3.25"] },
+        formula_const_vector_fixed={ TYPE="FieldFormula", value=["1.25", "3.25", "5.25"] },
+
+        formula_simple_scalar={ TYPE="FieldFormula", value="x^3" },
+        formula_simple_vector={ TYPE="FieldFormula", value=["x^3", "y^3", "z^3"] },
+        formula_simple_vector_fixed={ TYPE="FieldFormula", value=["x^3", "y^3", "z^3"] },
+
+        formula_full_scalar={ TYPE="FieldFormula", value="x+y+z+x^3+y^3+z^3" },
+        formula_full_vector={ TYPE="FieldFormula", value=["x+z+x^3+y^3+z^3", "x+y+x^3+y^3+z^3", "y+z+x^3+y^3+z^3"] },
+        formula_full_vector_fixed={ TYPE="FieldFormula", value=["x+y+x^3+y^3+z^3", "y+z+x^3+y^3+z^3", "x+z+x^3+y^3+z^3"] },
+
+        python_scalar={ TYPE="FieldPython", function="func_const", script_string="func_const(x,y,z): return ( 1.25, )" },
+        python_vector={ TYPE="FieldPython", function="func_const", script_string="func_const(x,y,z): return ( 1.25, 2.25, 3.25 )" },
+        python_vector_fixed={ TYPE="FieldPython", function="func_const", script_string="func_const(x,y,z): return ( 1.25, 3.25, 5.25 )" },
+
+        elementwise_scalar={ TYPE="FieldElementwise", gmsh_file="fields/simplest_cube_data.msh", field_name="scalar" },
+        elementwise_vector={ TYPE="FieldElementwise", gmsh_file="fields/simplest_cube_data.msh", field_name="vector_fixed" },
+        elementwise_vector_fixed={ TYPE="FieldElementwise", gmsh_file="fields/simplest_cube_data.msh", field_name="vector_fixed" }
+    }
+]
+)JSON";
+
+
+
 template <typename T>
 class FieldSpeed : public testing::Test {
 public:
@@ -74,7 +135,7 @@ public:
 		for (int i=0; i<list_size*loop_call_count; i++)
 			FOR_ELEMENTS(this->mesh_, ele) {
 				ElementAccessor<3> elm = (*ele).element_accessor();
-				test_result_sum_ += this->field_->value( this->point_, elm);
+				test_result_sum_ += field_.value( this->point_, elm);
 			}
 		END_TIMER("single_value");
 
@@ -83,7 +144,7 @@ public:
 			for (int j=0; j<list_size; j++)
 				FOR_ELEMENTS(this->mesh_, ele) {
 					ElementAccessor<3> elm = (*ele).element_accessor();
-					test_result_sum_ += this->field_->value( this->point_list_[j], elm);
+					test_result_sum_ += field_.value( this->point_list_[j], elm);
 				}
 		END_TIMER("all_values");
 
@@ -91,7 +152,7 @@ public:
 		for (int i=0; i<loop_call_count; i++)
 			FOR_ELEMENTS(this->mesh_, ele) {
 				ElementAccessor<3> elm = (*ele).element_accessor();
-				this->field_->value_list( this->point_list_, elm, value_list);
+				field_.value_list( this->point_list_, elm, value_list);
 				test_result_sum_ += value_list[0];
 			}
 		END_TIMER("value_list");
@@ -102,72 +163,67 @@ public:
         n_comp_ = 3;
 
         point_ = Point("1 2 3");
-        point_list_.reserve(10);
-        for (int i=0; i<10; i++) point_list_.push_back( Point("1 2 3") );
+        point_list_.reserve(list_size);
+        for (int i=0; i<list_size; i++) point_list_.push_back( Point("1 2 3") );
 
     	fce_ = new FceType[mesh_->region_db().size()];
     	data_ = new ReturnType[mesh_->region_db().size()];
 
     	set_data(data1_);
 
-    	vector<unsigned int> regions_1={0,3,5,6};
+    	vector<unsigned int> regions_1={0,3,5};
     	vector<unsigned int> regions_2={1,2,4,7};
 
     	RegionSet set_1,set_2;
+    	string set_1_name = "set_1";
+    	string set_2_name = "set_2";
     	for( auto i_reg : regions_1) {
-    	    set_1.push_back(mesh_->region_db().get_id(i_reg));
+    		unsigned int reg_id = mesh_->region_db().get_id(i_reg);
+    	    set_1.push_back(mesh_->region_db().find_id(reg_id));
     	    data_[i_reg]=data1_;
+    	    fce_[i_reg] = (&FieldSpeed::fce1);
     	}
-    	for( auto i_reg : regions_1) set_2.push_back(mesh_->region_db().get_id(i_reg));
-    	mesh_->region_db().add_set("set_1", set_1);
-    	mesh_->region_db().add_set("set_2", set_2);
-
-    	/*
-    	data_[0] = data1_;
-    	data_[1] = data2_;
-    	data_[2] = data2_;
-    	data_[3] = data1_;
-    	data_[4] = data2_;
-    	data_[5] = data1_;
-    	data_[6] = data1_;
-    	data_[7] = data2_;
-
-    	fce_[0] = (&FieldSpeed::fce1);
-        fce_[1] = (&FieldSpeed::fce2);
-        fce_[2] = (&FieldSpeed::fce2);
-        fce_[3] = (&FieldSpeed::fce1);
-        fce_[4] = (&FieldSpeed::fce2);
-        fce_[5] = (&FieldSpeed::fce1);
-        fce_[6] = (&FieldSpeed::fce1);
-        fce_[7] = (&FieldSpeed::fce2);
-        */
+    	for( auto i_reg : regions_2) {
+    		unsigned int reg_id = mesh_->region_db().get_id(i_reg);
+    		set_2.push_back(mesh_->region_db().find_id(reg_id));
+    		data_[i_reg]=data2_;
+    		fce_[i_reg] = (&FieldSpeed::fce2);
+    	}
+    	( const_cast<RegionDB&>(mesh_->region_db()) ).add_set(set_1_name, set_1);
+    	( const_cast<RegionDB&>(mesh_->region_db()) ).add_set(set_2_name, set_2);
 	}
 
 	void set_data(FieldValue<3>::Scalar::return_type val) {
-        data1_ = 1.25;
-    	data2_ = 2.50;
-    	field_const_val_ = 1.25;
-    	field_elementwise_val_ = 4.5;
+        data1_ = 1.75;
+    	data2_ = 1.25;
+    	expect_const_val_ = 13.75;
+    	expect_formula_simple_val_ = 9;
+    	expect_formula_full_val_ = 268;
+    	expect_elementwise_val_ = 4.5;
     	test_result_sum_ = 0.0;
     	input_type_name_ = "scalar";
     	value_list= std::vector<ReturnType>(list_size);
 	}
 
 	void set_data(FieldValue<3>::Vector::return_type val) {
-		data1_ = arma::vec("1.25 2.25 3.25");
-		data2_ = arma::vec("2.50 4.50 6.50");
-		field_const_val_ = arma::vec("1.75 2.75 3.75");
-		field_elementwise_val_ = arma::vec("9 18 27");
+		data1_ = arma::vec("1.75 2.75 3.75");
+		data2_ = arma::vec("1.25 2.25 3.25");
+		expect_const_val_ = arma::vec("13.75 22.75 31.75");
+		expect_formula_simple_val_ = arma::vec("9 52 153");
+		expect_formula_full_val_ = arma::vec("250 241 259");
+		expect_elementwise_val_ = arma::vec("9 18 27");
 		test_result_sum_ = arma::vec("0.0 0.0 0.0");
 		input_type_name_ = "vector";
 		value_list= std::vector<ReturnType>(list_size, ReturnType(n_comp_,1));
 	}
 
 	void set_data(FieldValue<3>::VectorFixed::return_type val) {
-		data1_ = arma::vec3("1.25 3.75 6.25");
-		data2_ = arma::vec3("2.50 7.50 12.50");
-		field_const_val_ = arma::vec3("1.75 3.75 5.75");
-		field_elementwise_val_ = arma::vec3("9 18 27");
+		data1_ = arma::vec3("1.75 3.75 5.75");
+		data2_ = arma::vec3("1.25 3.25 5.25");
+		expect_const_val_ = arma::vec3("13.75 31.75 49.75");
+		expect_formula_simple_val_ = arma::vec3("9 52 153");
+		expect_formula_full_val_ = arma::vec3("241 259 250");
+		expect_elementwise_val_ = arma::vec3("9 18 27");
 		test_result_sum_ = arma::vec3("0.0 0.0 0.0");
 		input_type_name_ = "vector_fixed";
 		value_list= std::vector<ReturnType>(list_size);
@@ -201,27 +257,21 @@ public:
 		os << "" << std::setfill('=') << setw(80) << "" << std::setfill(' ') << endl << endl;
 	}
 
-	void read_input(const string &input, const string &field_name) {
-	    this->field_.name("data");
-	    //field_->description("xyz");
-	    //field_->description("xyz");
+	void read_input(const string &field_name) {
+	    field_.name(field_name);
+	    field_.description("xyz");
 	    set_of_field_ += field_;
 
-
-	    Input::Type::Record  field_descriptor = set_of_field_.make_field_descriptor_type("FieldSpeedTest");
-	    //rec_type.declare_key(field_name, Input::Type::Array(FieldBaseType::input_type), Input::Type::Default::obligatory(),"" );
-	    //rec_type.finish();
-
-	    Input::Type::Array array_type = Input::Type::Array(FieldBaseType::input_type);
-
-	    Input::JSONToStorage reader( input, Input::Type::Array(field_descriptor ) );
+	    Input::Type::Array list_type = Input::Type::Array(set_of_field_.make_field_descriptor_type("FieldSpeedTest"));
+	    Input::JSONToStorage reader( field_input, list_type);
 	    Input::Array in_list=reader.get_root_interface<Input::Array>();
+	    field_.set_input_list(in_list);
 
-
-	    this->field_.set_mesh(*(this->mesh_));
-	    this->field_.set_input_list( in_list );
-	    TimeGovernor tg(0.0, 1.0);
-	    this->field_.set_time(tg);
+	    field_.set_mesh(*(this->mesh_));
+	    field_.set_n_components(n_comp_);
+	    field_.set_limit_side(LimitSide::right);
+	    TimeGovernor tg(0.0, 0.5);
+	    set_of_field_.set_time(tg);
 	}
 
 
@@ -230,8 +280,10 @@ public:
     ReturnType data1_;
     ReturnType data2_;
     ReturnType test_result_sum_;
-    ReturnType field_const_val_;
-    ReturnType field_elementwise_val_;
+    ReturnType expect_const_val_;
+    ReturnType expect_formula_simple_val_;
+    ReturnType expect_formula_full_val_;
+    ReturnType expect_elementwise_val_;
     std::vector<ReturnType> value_list;
     //FieldAlgorithmBase<3, T> *field_;
     FieldSet set_of_field_;
@@ -256,7 +308,7 @@ TYPED_TEST_CASE(FieldSpeed, TestedTypes);
 TYPED_TEST(FieldSpeed, array) {
     START_TIMER("array");
 	START_TIMER("single_value");
-	for (int i=0; i<10*loop_call_count; i++)
+	for (int i=0; i<list_size*loop_call_count; i++)
 		FOR_ELEMENTS(this->mesh_, ele) {
 			ElementAccessor<3> elm = (*ele).element_accessor();
 			this->test_result_sum_ += this->data_[elm.region_idx().idx()];
@@ -264,7 +316,7 @@ TYPED_TEST(FieldSpeed, array) {
 	END_TIMER("single_value");
 	END_TIMER("array");
 
-	this->test_result(this->data1_, 130);
+	this->test_result(this->expect_const_val_, 10);
 	this->profiler_output();
 }
 
@@ -272,7 +324,7 @@ TYPED_TEST(FieldSpeed, array) {
 TYPED_TEST(FieldSpeed, virtual_function) {
 	START_TIMER("virtual_function");
 	START_TIMER("single_value");
-	for (int i=0; i<10*loop_call_count; i++)
+	for (int i=0; i<list_size*loop_call_count; i++)
 		FOR_ELEMENTS(this->mesh_, ele) {
 			ElementAccessor<3> elm = (*ele).element_accessor();
 			this->test_result_sum_ += this->value( this->point_, elm);
@@ -281,7 +333,7 @@ TYPED_TEST(FieldSpeed, virtual_function) {
 
 	START_TIMER("all_values");
 	for (int i=0; i<loop_call_count; i++)
-		for (int j=0; j<10; j++)
+		for (int j=0; j<list_size; j++)
 			FOR_ELEMENTS(this->mesh_, ele) {
 				ElementAccessor<3> elm = (*ele).element_accessor();
 				this->test_result_sum_ += this->value( this->point_list_[j], elm);
@@ -289,94 +341,65 @@ TYPED_TEST(FieldSpeed, virtual_function) {
 	END_TIMER("all_values");
 	END_TIMER("virtual_function");
 
-	this->test_result(this->data1_, 2 * 130);
+	this->test_result(this->expect_const_val_, 20);
 	this->profiler_output();
 }
 
 
 
-string constant_input = R"JSON(
-{   
-    constant_expr_scalar=[
-        { TYPE="FieldConstant", value=1.25 }
-    ],
-    constant_expr_vector=[
-        { TYPE="FieldConstant", value=[1.75, 2.75, 3.75] }
-    ],
-    constant_expr_vector_fixed=[
-        { TYPE="FieldConstant", value=[1.75, 3.75, 5.75] }
-    ]
-}
-)JSON";
-
 TYPED_TEST(FieldSpeed, field_constant) {
-	//string key_name = "constant_expr_" + this->input_type_name_;
-	//this->read_input(constant_input, key_name);
-
-    this->field_ = new FieldConstant<3, TypeParam>(this->n_comp_);
-	((FieldConstant<3, TypeParam> *)this->field_)->set_value(this->field_const_val_);
+	string key_name = "constant_" + this->input_type_name_;
+	this->read_input(key_name);
 
 	START_TIMER("field_constant");
 	this->call_test();
 	END_TIMER("field_constant");
 
-	this->test_result( this->field_const_val_, (21 * this->mesh_->n_elements()) );
+	this->test_result( this->expect_const_val_, 21 );
 	this->profiler_output();
 }
 
 
-string formula_input = R"INPUT(
-{   
-   constant_expr_scalar={ 
-       TYPE="FieldFormula",
-       value="1.25"
-   },
-   constant_expr_vector={ 
-       TYPE="FieldFormula",
-       value=["1.75", "2.75", "3.75"]
-   },
-   constant_expr_vector_fixed={ 
-       TYPE="FieldFormula",
-       value=["1.75", "3.75", "5.75"]
-   },
-   simple_expr={ 
-       TYPE="FieldFormula",
-       value="x^2"
-   },
-   full_expr={ 
-       TYPE="FieldFormula",
-       value="x+y+z+x^2+y^2+z^2"
-   }
-}
-)INPUT";
+TYPED_TEST(FieldSpeed, field_formula_const) {
+	string key_name = "formula_const_" + this->input_type_name_;
+	this->read_input(key_name);
 
-TYPED_TEST(FieldSpeed, field_formula) {
-	typedef FieldAlgorithmBase<3, TypeParam > FieldBaseType;
-	string key_name = "constant_expr_" + this->input_type_name_;
-
-    Input::Type::Record rec_type("FieldFormulaTest","");
-    rec_type.declare_key(key_name, FieldBaseType::input_type, Input::Type::Default::obligatory(), "" );
-    rec_type.finish();
-
-    // read input string
-    Input::JSONToStorage reader( formula_input, rec_type );
-    Input::Record in_rec=reader.get_root_interface<Input::Record>();
-
-	this->field_ = new FieldFormula<3, TypeParam>(this->n_comp_);
-	this->field_->init_from_input(in_rec.val<Input::AbstractRecord>(key_name));
-	this->field_->set_time(0.0);
-
-	START_TIMER("field_formula");
+	START_TIMER("field_formula_const");
 	this->call_test();
-	END_TIMER("field_formula");
+	END_TIMER("field_formula_const");
 
-	cout << "Test result: " << this->test_result_sum_ << endl;
-	//this->test_result( this->field_const_val_, (9 * 21) );
+	this->test_result( this->expect_const_val_, 21 );
 	this->profiler_output();
 }
 
 
-#ifdef HAVE_PYTHON
+TYPED_TEST(FieldSpeed, field_formula_simple) {
+	string key_name = "formula_simple_" + this->input_type_name_;
+	this->read_input(key_name);
+
+	START_TIMER("field_formula_simple_expr");
+	this->call_test();
+	END_TIMER("field_formula_simple_expr");
+
+	this->test_result( this->expect_formula_simple_val_, 21 );
+	this->profiler_output();
+}
+
+
+TYPED_TEST(FieldSpeed, field_formula_full) {
+	string key_name = "formula_full_" + this->input_type_name_;
+	this->read_input(key_name);
+
+	START_TIMER("field_formula_full_expr");
+	this->call_test();
+	END_TIMER("field_formula_full_expr");
+
+	this->test_result( this->expect_formula_full_val_, 21 );
+	this->profiler_output();
+}
+
+
+/*#ifdef HAVE_PYTHON
 string python_input = R"CODE(
 def func_scalar(x,y,z):
     return ( 1.25, )     # one value tuple
@@ -390,58 +413,33 @@ def func_vector_fixed(x,y,z):
 )CODE";
 
 TYPED_TEST(FieldSpeed, field_python) {
-    this->field_ = new FieldPython<3, TypeParam>(this->n_comp_);
-    ((FieldPython<3, TypeParam> *)this->field_)->set_python_field_from_string(python_input, "func_" + this->input_type_name_);
-    this->field_->set_time(0.0);
+	string key_name = "python_" + this->input_type_name_;
+	this->read_input(key_name);
+
+	//this->field_ = new FieldPython<3, TypeParam>(this->n_comp_);
+    //((FieldPython<3, TypeParam>)this->field_).set_python_field_from_string(python_input, "func_" + this->input_type_name_);
+    //this->field_->set_time(0.0);
 
 	START_TIMER("field_python");
 	this->call_test();
 	END_TIMER("field_python");
 
-	this->test_result( this->field_const_val_, (9 * 21) );
+	cout << "Test result: " << this->test_result_sum_ << endl;
+	cout << "Expected:    " << 21 * this->expect_const_val_ << endl;
+	//this->test_result( this->expect_const_val_, (9 * 21) );
 	this->profiler_output();
 }
-#endif // HAVE_PYTHON
+#endif // HAVE_PYTHON */
 
-string elementwise_input = R"INPUT(
-{   
-   scalar={
-       TYPE="FieldElementwise",
-       gmsh_file="fields/simplest_cube_data.msh",
-       field_name="scalar"
-   },
-   vector={
-       TYPE="FieldElementwise",
-       gmsh_file="fields/simplest_cube_data.msh",
-       field_name="vector_fixed"
-   },
-   vector_fixed={
-       TYPE="FieldElementwise",
-       gmsh_file="fields/simplest_cube_data.msh",
-       field_name="vector_fixed"
-   }
-}
-)INPUT";
 
 TYPED_TEST(FieldSpeed, field_elementwise) {
-	Input::Type::Record  rec_type("Test","");
-    rec_type.declare_key("scalar", FieldElementwise<3, FieldValue<3>::Scalar >::input_type, Input::Type::Default::obligatory(),"" );
-    rec_type.declare_key("vector_fixed", FieldElementwise<3, FieldValue<3>::VectorFixed >::input_type, Input::Type::Default::obligatory(),"" );
-    rec_type.declare_key("vector", FieldElementwise<3, FieldValue<3>::Vector >::input_type, Input::Type::Default::obligatory(),"" );
-    rec_type.finish();
-
-    Input::JSONToStorage reader( elementwise_input, rec_type );
-    Input::Record rec=reader.get_root_interface<Input::Record>();
-
-    this->field_ = new FieldElementwise<3, TypeParam>(this->n_comp_);
-    this->field_->init_from_input( rec.val<Input::Record>(this->input_type_name_) );
-    this->field_->set_mesh(this->mesh_,false);
-    this->field_->set_time(0.0);
+	string key_name = "elementwise_" + this->input_type_name_;
+	this->read_input(key_name);
 
     START_TIMER("field_elementwise");
 	this->call_test();
 	END_TIMER("field_elementwise");
 
-	this->test_result( this->field_elementwise_val_, 21 );
+	this->test_result( this->expect_elementwise_val_, 21 );
 	this->profiler_output();
 }
