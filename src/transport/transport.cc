ab483417c7815b2de3b338a51de307ccd24f2045
jstebel <jan.stebel@tul.cz>
2014-10-09 11:33:18 +0200

    Fixed error in flux balance for ConvectionTransport. - flux was wrongly multiplied by porosity

--- src/transport/transport.cc
+++ src/transport/transport.cc
@@ -380,13 +380,13 @@ void ConvectionTransport::set_boundary_conditions()
                         if (balance_ != nullptr)
                         {
                         	for (unsigned int sbi=0; sbi<n_substances(); sbi++)
-                        		balance_->add_flux_vec_value(subst_idx[sbi], loc_b, flux*value[sbi]*por_m);
+                        		balance_->add_flux_vec_value(subst_idx[sbi], loc_b, flux*value[sbi]);
                         }
                     } else {
                     	if (balance_ != nullptr)
 						{
 							for (unsigned int sbi=0; sbi<n_substances(); sbi++)
-								balance_->add_flux_matrix_values(subst_idx[sbi], loc_b, {row_4_el[el_4_loc[loc_el]]}, {flux*por_m});
+								balance_->add_flux_matrix_values(subst_idx[sbi], loc_b, {row_4_el[el_4_loc[loc_el]]}, {flux});
 						}
                     }
                     ++loc_b;
