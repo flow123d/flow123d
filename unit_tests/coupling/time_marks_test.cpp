de45d02c66885e44a54209fef6eeb4c61b9332f5
Jan Brezina <jan.brezina@tul.cz>
2015-02-14 21:30:27 +0100

    Separate TimeMarks unit test.

---
+++ unit_tests/coupling/time_marks_test.cpp
@@ -0,0 +1,39 @@
+/*
+ * time_marks_test.cpp
+ *
+ *  Created on: Feb 12, 2015
+ *      Author: jb
+ */
+
+
+#include <flow_gtest.hh>
+
+#include "coupling/time_marks.hh"
+
+
+/**
+ * Test for class TimeMark.
+ */
+TEST (TimeMark, time_mark)
+{
+    // Constructor.
+    TimeMark tm1(-1.0, 0x01);
+    TimeMark tm2(0.0, TimeMark::every_type);
+    TimeMark tm3(10.0,0x05);
+    TimeMark::Type my_type = 0x0a;
+
+    //checking time values
+    EXPECT_EQ(tm1.time(), -1.0);
+    EXPECT_EQ(tm2.mark_type(), TimeMark::Type(~0x0));
+
+    //checking type values - comparing masks
+    EXPECT_TRUE(tm1.match_mask(0x01));
+    EXPECT_TRUE(tm2.match_mask(~0x00));
+
+    //adding type and checking mask
+    tm3.add_to_type(my_type);
+    EXPECT_TRUE(tm3.match_mask(0x0f));      //0x05 + 0x0a = 0x0f
+
+    //comparing times
+    EXPECT_TRUE( (tm1<tm2) && (tm2<tm3) );
+}
