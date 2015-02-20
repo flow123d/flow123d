ab1e57ec086171bd3a7e33f6822f2409a160062f
x3mSpeedy <jan.hybs@tul.cz>
2015-02-20 15:09:15 +0100

    Add doc to git rev_num, renamed argument

--- CMake/Modules/Flow123d_git_rev_num.cmake
+++ CMake/Modules/Flow123d_git_rev_num.cmake
@@ -1,9 +1,20 @@
 ####
 # this CMake script is called at build time to get revision of current working copy
+
+# Arguments:
+# FLOW123D_SOURCE_DIR 
+#   directory of CMAKE_SOURCE_DIR
+# OUTPUT_FILE_PATH
+#   location where result will be saved, i.e. /path_to_file/rev_num.h
+#   note:   in this action, temporary file (which is original file with suffix of .tmp 
+#           i.e. OUTPUT_FILE_PATH + '.tmp') will be used 
+#           after process is done, temporary file will be deleted
+    
+
 MESSAGE (STATUS "CREATING REV_NUM.H")
 
 # get tmp file path from given FILE_PATH variable (-DFILE_PATH=path/to/file.extension)
-set(TMP_FILE_PATH "${FILE_PATH}.tmp")
+set(OUTPUT_TMP_PATH "${OUTPUT_FILE_PATH}.tmp")
 
 # get git-related details
 include(${FLOW123D_SOURCE_DIR}/CMake/Modules/Flow123d_git_info.cmake)
@@ -11,9 +22,9 @@ include(${FLOW123D_SOURCE_DIR}/CMake/Modules/Flow123d_git_info.cmake)
 # write a file with the SVN VERSION and URL define
 #file(WRITE rev_num.h.tmp "#define _GIT_REVISION_ \"${GIT_DESCRIBE}\"\n#define _GIT_BRANCH_ \"${GIT_BRANCH}\"\n#define _GIT_URL_ \"${GIT_URL}\"")
 
-configure_file(${FLOW123D_SOURCE_DIR}/CMake/rev_num_h_template ${TMP_FILE_PATH})
+configure_file(${FLOW123D_SOURCE_DIR}/CMake/rev_num_h_template ${OUTPUT_TMP_PATH})
 
 # copy the file to the final header only if the version changes
 # reduces needless rebuilds
-execute_process(COMMAND ${CMAKE_COMMAND} -E copy_if_different ${TMP_FILE_PATH} ${FILE_PATH})
-execute_process(COMMAND ${CMAKE_COMMAND} -E remove ${TMP_FILE_PATH})
+execute_process(COMMAND ${CMAKE_COMMAND} -E copy_if_different ${OUTPUT_TMP_PATH} ${FILE_PATH})
+execute_process(COMMAND ${CMAKE_COMMAND} -E remove ${OUTPUT_TMP_PATH})
