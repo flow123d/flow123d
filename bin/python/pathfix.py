ba809dfc8e587581b649c5b4e33161eb7a46996d
Jan_Hybs <jan.hybs@tul.cz>
2015-04-28 12:48:35 +0200

    Add debug information about version and executable

--- bin/python/pathfix.py
+++ bin/python/pathfix.py
@@ -2,6 +2,9 @@ __author__ = 'Jan Hybs'
 
 import sys, os
 
+# print debug information about python
+print ("Python:    " + str (sys.version).replace("\n", "") + ", " + str (sys.executable))
+
 # add src/python into module path
 path = os.path.join('..', '..', 'src', 'python')
 sys.path.append (path)
