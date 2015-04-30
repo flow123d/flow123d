815af33686931a5a002ca6c2925a04a816edd691
Jan_Hybs <jan.hybs@tul.cz>
2015-04-30 12:13:53 +0200

    Add docs string to all classes and method
     - format code
     - split some one-liners
     - remove blank lines

--- bin/python/profiler_formatter_script.py
+++ bin/python/profiler_formatter_script.py
@@ -1,9 +1,37 @@
-__author__ = 'Jan Hybs'
+# encoding: utf-8
+# author:   Jan Hybs
+
+"""
+This script is simple iface to profiler_formatter_module module.
+Script is expected to run as main python file. Script takes arguments from console and calls methods of module.
+
+Usage: profiler_formatter_script.py [options]
+
+Options:
+  -h, --help            show this help message and exit
+  -i FILENAME, --input=FILENAME
+                        Absolute or relative path to JSON file which will be
+                        processed
+  -o FILENAME, --output=FILENAME
+                        Absolute or relative path output file which will be
+                        generated/overwritten
+  -f CLASSNAME, --formatter=CLASSNAME
+                        Classname of formatter which will be used, to list
+                        available formatters use option -l (--list)
+  -l, --list            Prints all formatters available in folder formatters
+                        (using duck-typing)
+  -s STYLES, --style=STYLES
+                        Additional styling options in name:value format (for
+                        example separator:  default is os separator)
+
+"""
 
 import pathfix
+
 pathfix.append_to_path ()
 
 import system.versions
+
 system.versions.require_version_2 ()
 
 import sys
@@ -11,24 +39,25 @@ from optparse import OptionParser
 from profiler.profiler_formatter_module import ProfilerFormatter
 
 
-
 def create_parser ():
-    parser = OptionParser()
+    """Creates command line parse"""
+    parser = OptionParser ()
     parser.add_option ("-i", "--input", dest="input", metavar="FILENAME", default=None,
-                      help="Absolute or relative path to JSON file which will be processed")
+                       help="Absolute or relative path to JSON file which will be processed")
     parser.add_option ("-o", "--output", dest="output", metavar="FILENAME", default=None,
-                      help="Absolute or relative path output file which will be generated/overwritten")
+                       help="Absolute or relative path output file which will be generated/overwritten")
     parser.add_option ("-f", "--formatter", dest="formatter", metavar="CLASSNAME", default="SimpleTableFormatter",
-                      help="Classname of formatter which will be used, to list available formatters use option -l (--list)")
+                       help="Classname of formatter which will be used, to list available formatters use option -l (--list)")
     parser.add_option ("-l", "--list", dest="list", default=False, action="store_true",
-                      help="Prints all formatters available in folder formatters (using duck-typing)")
+                       help="Prints all formatters available in folder formatters (using duck-typing)")
     parser.add_option ("-s", "--style", dest="styles", default=[], action="append",
-                      help="Additional styling options in name:value format (for example separator:\n default is os separator)")
+                       help="Additional styling options in name:value format (for example separator:\n default is os separator)")
     parser.set_usage ("""%prog [options]""")
     return parser
 
 
 def parse_args (parser):
+    """Parses argument using given parses and check resulting value combination"""
     (options, args) = parser.parse_args ()
 
     if options.list == True:
@@ -36,17 +65,15 @@ def parse_args (parser):
 
     if options.input == None:
         print "Error: No input file specified!"
-        parser.print_help()
-        sys.exit(1)
+        parser.print_help ()
+        sys.exit (1)
 
     if options.formatter == None:
         print "Error: No formatter specified!"
-        parser.print_help()
-        sys.exit(1)
+        parser.print_help ()
+        sys.exit (1)
 
     return (options, args)
-
-
 
 
 def main ():
@@ -55,22 +82,22 @@ def main ():
 
     # list formatters
     if options.list == True:
-        formatters = ProfilerFormatter.list_formatters()
-        print 'Formatter available: \n\t{:s}'.format ('\n\t'.join(formatters))
-        sys.exit(0)
+        formatters = ProfilerFormatter.list_formatters ()
+        print 'Formatter available: \n\t{:s}'.format ('\n\t'.join (formatters))
+        sys.exit (0)
 
     # call main method
-    formatter = ProfilerFormatter()
+    formatter = ProfilerFormatter ()
     result = formatter.convert (options.input, options.output, options.formatter, options.styles)
 
     # process result
     if result == True:
-        sys.exit(0)
+        sys.exit (0)
     else:
         print result
-        sys.exit(1)
+        sys.exit (1)
 
 
 # only if this file is main python file, read args
-if __name__ == "__main__" :
-    main()
+if __name__ == "__main__":
+    main ()
--- src/python/utils/strings.py
+++ src/python/utils/strings.py
@@ -1,8 +1,13 @@
-__author__ = 'Jan Hybs'
+# encoding: utf-8
+# author:   Jan Hybs
 
+"""
+Simple module which provides string/list utilities
+"""
 
 
 def join_iterable (iterable, prefix="", suffix="", separator=",", padding=None, extraSpace=2):
+    """Joins given iterable object with extra surrounding options"""
     result = ""
     result += prefix
 
@@ -11,9 +16,9 @@ def join_iterable (iterable, prefix="", suffix="", separator=",", padding=None, 
         if padding is None:
             result += str (iterable[i])
         else:
-            result += str (iterable[i]).center(padding[i] + extraSpace)
-        if i < size-1:
-            result += (suffix+separator+prefix)
+            result += str (iterable[i]).center (padding[i] + extraSpace)
+        if i < size - 1:
+            result += (suffix + separator + prefix)
     result += suffix
 
     return result
