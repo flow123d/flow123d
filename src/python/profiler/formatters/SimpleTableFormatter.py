cc6724da27a9ae6d0d8affd8276d9afcaf660263
Jan_Hybs <jan.hybs@tul.cz>
2015-05-21 10:56:34 +0200

    Remove needless run_duration field from profiler

--- src/python/profiler/formatters/SimpleTableFormatter.py
+++ src/python/profiler/formatters/SimpleTableFormatter.py
@@ -51,9 +51,8 @@ class SimpleTableFormatter (object):
         self.convert_style ()
 
         self.json = json
-        self.processHeader (json)
-        self.processBody (json, 0)
-        print self.maxBodySize
+        self.process_header (json)
+        self.process_body (json, 0)
         self.maxBodySize = [n + self.styles.padding for n in self.maxBodySize]
 
         self.maxNameSize = self.maxNameSize + self.styles.space_header
@@ -86,7 +85,7 @@ class SimpleTableFormatter (object):
         self.output += lineDivider
         return self.output
 
-    def appendToHeader (self, name, value=None, linebreak=False):
+    def append_to_header (self, name, value=None, linebreak=False):
         """Appends entry to header column list, if no value was given
         value from json object by given name will be taken
         """
@@ -96,7 +95,7 @@ class SimpleTableFormatter (object):
         if self.maxNameSize < len (str (name)):
             self.maxNameSize = len (str (name))
 
-    def appendToBody (self, values):
+    def append_to_body (self, values):
         """Appends entry to body row list.
         value is tupple of tupples, where inner tupper has two elements, first formatting character, and second value,
         formatting character is used in string format() method
@@ -115,41 +114,31 @@ class SimpleTableFormatter (object):
         for i in range (len (self.maxBodySize)):
             self.maxBodySize[i] = max (self.maxBodySize[i], len (str (values[i][1])))
 
-    def processHeader (self, json):
+    def process_header (self, json):
         """Appends header information"""
 
-        self.appendToHeader ("Program name")
-        self.appendToHeader ("Program version")
-        self.appendToHeader ("Program branch")
-        self.appendToHeader ("Program revision")
-        self.appendToHeader ("Program build")
+        self.append_to_header ("Program name")
+        self.append_to_header ("Program version")
+        self.append_to_header ("Program branch")
+        self.append_to_header ("Program revision")
+        self.append_to_header ("Program build")
 
         if 'source-dir' in json:
-            self.appendToHeader ("Source dir")
+            self.append_to_header ("Source dir")
 
-        self.appendToHeader ("Timer resolution", linebreak=self.styles.linesep)
+        self.append_to_header ("Timer resolution", linebreak=self.styles.linesep)
 
 
         desc = re.sub ("\s+", " ", json["task-description"], re.M)
-        self.appendToHeader ("Task description", desc)
-        self.appendToHeader ("Task size", linebreak=self.styles.linesep)
+        self.append_to_header ("Task description", desc)
+        self.append_to_header ("Task size", linebreak=self.styles.linesep)
 
-        self.appendToHeader ("Run process count")
-        self.appendToHeader ("Run started", json["run-started-at"])
-        self.appendToHeader ("Run ended", json["run-finished-at"])
-
-        if "run-duration" in json:
-            self.totalDuration = json["run-duration"]
-        else:
-            self.totalDuration = self.timedelta_milliseconds (json["run-finished-at"] - json["run-started-at"]) / 1000.0
-
-        # currently is overall duration information resolution in seconds, which is not enough
-        # manually increase value to 0.5
-        self.totalDuration = max (self.totalDuration, 0.5)
-        self.appendToHeader ("Run duration", self.totalDuration)
+        self.append_to_header ("Run process count")
+        self.append_to_header ("Run started", json["run-started-at"])
+        self.append_to_header ("Run ended", json["run-finished-at"])
 
 
-    def processBody (self, json, level):
+    def process_body (self, json, level):
         """Recursive body processing"""
 
         # first occurrence of cumul-time-sum is whole-program's measured time
@@ -165,7 +154,7 @@ class SimpleTableFormatter (object):
             if str(json["file-path"]).startswith (self.styles.remove_prefix):
                 path = path[len (self.styles.remove_prefix):]
 
-            self.appendToBody ((
+            self.append_to_body ((
                 ("<", "{abs_prc:6.2f} {leading} {rel_prc:5.2f} {tag}".format (
                     abs_prc=abs_prc, leading=self.styles.leading_char * (level - 1), rel_prc=rel_prc, tag=json["tag"])),
                 ("^", "{:d}".format (json["call-count"])),
@@ -179,7 +168,7 @@ class SimpleTableFormatter (object):
 
         try:
             for child in json["children"]:
-                self.processBody (child, level + 1)
+                self.process_body (child, level + 1)
         except Exception as e:
             pass
 
