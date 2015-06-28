9d31b9dc6578ef474de884916fb3dcc438a3a953
x3mSpeedy <jan.hybs@tul.cz>
2015-06-28 23:43:51 +0200

    Comment-out python debug sections, comment-out profiler CSV output Add docs to function args

--- src/python/utils/strings.py
+++ src/python/utils/strings.py
@@ -7,11 +7,24 @@ Simple module which provides string/list utilities
 
 
 def join_iterable (iterable, prefix="", suffix="", separator=",", padding=None, extra_space=2):
-    """Joins given iterable object with extra surrounding options"""
+    """
+    Joins given iterable object with extra surrounding options
+    :param iterable: iterable object
+    :param prefix: string part which will be added before iterable item value
+    :param suffix: string part which will be added after iterable item value
+    :param separator: string which separates items
+    :param padding: if list is specified, item string representation
+     will be padded with white space. Each entry must contain number - desired item length
+    :param extra_space: number of chars to be extra added to padding
+    :return:
+    """
+    size = len (iterable)
+    if not size:
+        return ""
+
     result = ""
     result += prefix
 
-    size = len (iterable)
     for i in range (size):
         if padding is None:
             result += str (iterable[i])
