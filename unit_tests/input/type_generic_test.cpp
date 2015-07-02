7cf84e05c6a4e3d12a36315a8dbb513df39f6c9b
dflanderka <t426.001@centrum.cz>
2015-07-02 09:20:17 +0200

    Add unit test of generic array.

--- unit_tests/input/type_generic_test.cpp
+++ unit_tests/input/type_generic_test.cpp
@@ -17,17 +17,23 @@
 
 using namespace Input::Type;
 
-TEST(GenericType, declare_key) {
-::testing::FLAGS_gtest_death_test_style = "threadsafe";
 
-	static Array parametrized_arr = Array(Parameter("parametric"));
 
-	std::vector<TypeBase::ParameterPair> vec;
-	vec.push_back( std::make_pair("parametric", boost::make_shared<Double>()) );
-
-	boost::shared_ptr<TypeBase> type_arr = parametrized_arr.make_instance(vec);
+static const Selection & get_colors_selection() {
+	return Selection("colors")
+			.add_value(0, "red")
+			.add_value(1, "green")
+			.add_value(2, "blue")
+			.close();
 }
 
+static const Selection & get_shapes_selection() {
+	return Selection("shapes")
+			.add_value(0, "rectangle")
+			.add_value(1, "circle")
+			.add_value(2, "triangle")
+			.close();
+}
 
 static const Instance & get_generic_record(const Selection *sel, int max_limit) {
 	std::vector<TypeBase::ParameterPair> param_vec;
@@ -44,25 +50,35 @@ static const Instance & get_generic_record(const Selection *sel, int max_limit) 
 			.close();
 }
 
+static const Instance & get_generic_array(const Selection *sel) {
+	static Array arr(Parameter("param"), 0, 100);
+
+	std::vector<TypeBase::ParameterPair> param_vec;
+	param_vec.push_back( std::make_pair("param", boost::make_shared<Selection>(*sel)) );
+
+	return Instance( arr, param_vec )
+			.close();
+}
+
+
 TEST(GenericType, generic_record) {
 	::testing::FLAGS_gtest_death_test_style = "threadsafe";
-	static Selection sel1 = Selection("colors")
-			.add_value(0, "red")
-			.add_value(1, "green")
-			.add_value(2, "blue")
-			.close();
-
-	static Selection sel2 = Selection("shapes")
-			.add_value(0, "rectangle")
-			.add_value(1, "circle")
-			.add_value(2, "triangle")
-			.close();
-
 	static Record problem = Record("problem", "desc.")
-			.declare_key("primary", get_generic_record(&sel1, 10), "Primary problem.")
-			.declare_key("secondary", get_generic_record(&sel2, 1000), "Secondary problem.")
+			.declare_key("primary", get_generic_record(&get_colors_selection(), 10), "Primary problem.")
+			.declare_key("secondary", get_generic_record(&get_shapes_selection(), 1000), "Secondary problem.")
 			.declare_key("bool", Bool(), "Some bool key.")
 			.close();
 
-	problem.finish();
+	TypeBase::lazy_finish();
+}
+
+
+TEST(GenericType, generic_array) {
+	::testing::FLAGS_gtest_death_test_style = "threadsafe";
+	static Record arr_rec = Record("rec_of_arr", "desc.")
+			.declare_key("array1", get_generic_array(&get_colors_selection()), "Primary problem.")
+			.declare_key("array2", get_generic_array(&get_shapes_selection()), "Secondary problem.")
+			.close();
+
+	TypeBase::lazy_finish();
 }
