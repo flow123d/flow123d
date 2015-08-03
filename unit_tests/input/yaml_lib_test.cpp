ad29d4a4af04be2810093a713870da82a197f279
dflanderka <t426.001@centrum.cz>
2015-08-03 13:10:10 +0200

    Modify unit test of yaml lib.
    - add test of map and sequence

--- unit_tests/input/yaml_lib_test.cpp
+++ unit_tests/input/yaml_lib_test.cpp
@@ -6,4 +6,10 @@ TEST(YamlCpp, parser) {
     YAML::Node config = YAML::LoadFile("test_input.yaml");
     EXPECT_TRUE(config["test_key"]);
     EXPECT_EQ(42, config["test_key"].as<int>());
+    EXPECT_TRUE(config["map_key"]);
+    EXPECT_TRUE(config["map_key"].IsMap());
+    EXPECT_TRUE(config["map_key"]["a"].IsScalar());
+    EXPECT_TRUE(config["seq_key"]);
+    EXPECT_TRUE(config["seq_key"].IsSequence());
+    EXPECT_TRUE(config["seq_key"][1].IsScalar());
 }
