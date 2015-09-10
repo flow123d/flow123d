233224a3db7745173960c9a9653c20f19f81d0d7
Jan_Hybs <jan.hybs@tul.cz>
2015-09-10 14:36:37 +0200

    Add more tests for python infrastructure
     - add test for correct module loading (load by path)
     - add test for correct loading from loaded module
     - add tests for profiler formatter correct behaviour (processing broken, missing and correct json files)

--- unit_tests/system/python_loader_test.cpp
+++ unit_tests/system/python_loader_test.cpp
@@ -60,5 +60,131 @@ TEST(PythonLoader, file_error) {
     		PythonLoader::ExcPythonError);
 }
 
+TEST(PythonLoader, module_retrieve) {
+  PyObject * python_module;
+
+  // grab module and function by importing module profiler_formatter_module.py
+  EXPECT_THROW ( { PythonLoader::load_module_by_name ("profiler.non_existing_module"); }, PythonLoader::ExcPythonError);
+  python_module = PythonLoader::load_module_by_name ("profiler.profiler_formatter_module");
+  EXPECT_THROW ( { PythonLoader::get_callable (python_module, "non_existing_callable" ); }, PythonLoader::ExcPythonError);
+  PythonLoader::get_callable (python_module, "convert" );
+  PythonLoader::check_error();
+}
+
+TEST(PythonLoader, profiler_broken_json) {
+  PyObject * python_module;
+  PyObject * convert_method;
+  PyObject * arguments;
+  PyObject * tmp;
+  int argument_index = 0;
+
+  // grab module and function by importing module profiler_formatter_module.py
+  python_module = PythonLoader::load_module_by_name ("profiler.profiler_formatter_module");
+  convert_method  = PythonLoader::get_callable (python_module, "convert" );
+
+  // generate argument tuple
+  arguments = PyTuple_New (3);
+  // set json path location as first argument
+  tmp = PyString_FromString ("python_profiler_files/broken.json");
+  PyTuple_SetItem (arguments, argument_index++, tmp);
+  // set output path location as second argument
+  tmp = PyString_FromString ("non-existent-file.txt");
+  PyTuple_SetItem (arguments, argument_index++, tmp);
+  // set Formatter class as third value
+  tmp = PyString_FromString ("SimpleTableFormatter");
+  PyTuple_SetItem (arguments, argument_index++, tmp);
+
+  // execute method with arguments
+  cerr << "Error traceback will appear below" << endl;
+  PyObject_CallObject (convert_method, arguments);
+  EXPECT_THROW ( { PythonLoader::check_error(); }, PythonLoader::ExcPythonError);
+}
+
+TEST(PythonLoader, profiler_missing_json) {
+  PyObject * python_module;
+  PyObject * convert_method;
+  PyObject * arguments;
+  PyObject * tmp;
+  int argument_index = 0;
+
+  // grab module and function by importing module profiler_formatter_module.py
+  python_module = PythonLoader::load_module_by_name ("profiler.profiler_formatter_module");
+  convert_method  = PythonLoader::get_callable (python_module, "convert" );
+
+  // generate argument tuple
+  arguments = PyTuple_New (3);
+  // set json path location as first argument
+  tmp = PyString_FromString ("python_profiler_files/missing.json");
+  PyTuple_SetItem (arguments, argument_index++, tmp);
+  // set output path location as second argument
+  tmp = PyString_FromString ("non-existent-file.txt");
+  PyTuple_SetItem (arguments, argument_index++, tmp);
+  // set Formatter class as third value
+  tmp = PyString_FromString ("SimpleTableFormatter");
+  PyTuple_SetItem (arguments, argument_index++, tmp);
+
+  // execute method with arguments
+  cerr << "Error traceback will appear below" << endl;
+  PyObject_CallObject (convert_method, arguments);
+  EXPECT_THROW ( { PythonLoader::check_error(); }, PythonLoader::ExcPythonError);
+}
+
+TEST(PythonLoader, profiler_empty_json) {
+  PyObject * python_module;
+  PyObject * convert_method;
+  PyObject * arguments;
+  PyObject * tmp;
+  int argument_index = 0;
+
+  // grab module and function by importing module profiler_formatter_module.py
+  python_module = PythonLoader::load_module_by_name ("profiler.profiler_formatter_module");
+  convert_method  = PythonLoader::get_callable (python_module, "convert" );
+
+  // generate argument tuple
+  arguments = PyTuple_New (3);
+  // set json path location as first argument
+  tmp = PyString_FromString ("python_profiler_files/empty.json");
+  PyTuple_SetItem (arguments, argument_index++, tmp);
+  // set output path location as second argument
+  tmp = PyString_FromString ("non-existent-file.txt");
+  PyTuple_SetItem (arguments, argument_index++, tmp);
+  // set Formatter class as third value
+  tmp = PyString_FromString ("SimpleTableFormatter");
+  PyTuple_SetItem (arguments, argument_index++, tmp);
+
+  // execute method with arguments
+  cerr << "Error traceback will appear below" << endl;
+  PyObject_CallObject (convert_method, arguments);
+  EXPECT_THROW ( { PythonLoader::check_error(); }, PythonLoader::ExcPythonError);
+}
+
+TEST(PythonLoader, profiler_ok_json) {
+  PyObject * python_module;
+  PyObject * convert_method;
+  PyObject * arguments;
+  PyObject * return_value;
+  PyObject * tmp;
+  int argument_index = 0;
+
+  // grab module and function by importing module profiler_formatter_module.py
+  python_module = PythonLoader::load_module_by_name ("profiler.profiler_formatter_module");
+  convert_method  = PythonLoader::get_callable (python_module, "convert" );
+
+  // generate argument tuple
+  arguments = PyTuple_New (3);
+  // set json path location as first argument
+  tmp = PyString_FromString ("python_profiler_files/ok.json");
+  PyTuple_SetItem (arguments, argument_index++, tmp);
+  // set output path location as second argument
+  tmp = PyString_FromString ("python_profiler_files/ok.txt");
+  PyTuple_SetItem (arguments, argument_index++, tmp);
+  // set Formatter class as third value
+  tmp = PyString_FromString ("SimpleTableFormatter");
+  PyTuple_SetItem (arguments, argument_index++, tmp);
+
+  // execute method with arguments
+  return_value = PyObject_CallObject (convert_method, arguments);
+  EXPECT_EQ (return_value, Py_True);
+}
 
 #endif // FLOW123D_HAVE_PYTHON
