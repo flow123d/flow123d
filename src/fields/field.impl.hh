240129bb5e0a7b5fd5475c10850583f1d199b6da
dflanderka <t426.001@centrum.cz>
2015-10-28 09:35:07 +0100

    Modify Fields - use region to search in region sets.

--- src/fields/field.impl.hh
+++ src/fields/field.impl.hh
@@ -317,12 +317,17 @@ void Field<spacedim,Value>::update_history(const TimeStep &time) {
 				}
 
 			} else if (shared_->list_it_->opt_val("region", domain_name)) {
+				domain = mesh()->region_db().get_region_set(domain_name);
+				if (domain.size() == 0) {
+					THROW( RegionDB::ExcUnknownSetOperand()
+							<< RegionDB::EI_Label(domain_name) << shared_->list_it_->ei_address() );
+				}
 				// try find region by label
-				Region region = mesh()->region_db().find_label(domain_name);
-				if(region.is_valid())
-				  domain.push_back(region);
-				else
-				  xprintf(Warn, "Unknown region with label: '%s'\n", domain_name.c_str());
+				//Region region = mesh()->region_db().find_label(domain_name);
+				//if(region.is_valid())
+				//  domain.push_back(region);
+				//else
+				//  xprintf(Warn, "Unknown region with label: '%s'\n", domain_name.c_str());
 
 			} else if (shared_->list_it_->opt_val("rid", id)) {
 				try {
