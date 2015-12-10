40b6b236b44db46443d61e6ca92e1fc6591f8686
Jan Hybš <x3mSpeedy@users.noreply.github.com>
2015-12-10 13:36:17 +0100

    Fixing python include, issue #480

--- src/system/sys_profiler.cc
+++ src/system/sys_profiler.cc
@@ -19,11 +19,13 @@
 #include <fstream>
 #include <iomanip>
 #include <sys/param.h>
-#include "Python.h"
+
+#ifdef FLOW123D_HAVE_PYTHON
+    #include "Python.h"
+#endif // FLOW123D_HAVE_PYTHON
 
 #include "sys_profiler.hh"
 #include "system/system.hh"
-#include "Python.h"
 #include "system/python_loader.hh"
 #include <boost/format.hpp>
 #include <iostream>
@@ -576,7 +578,7 @@ void Profiler::output_header (property_tree::ptree &root, int mpi_size) {
     root.put ("run-finished-at",    end_time_string);
 }
 
-
+#ifdef FLOW123D_HAVE_PYTHON
 void Profiler::transform_profiler_data (const string &output_file_suffix, const string &formatter) {
     PyObject * python_module;
     PyObject * convert_method;
@@ -641,6 +643,11 @@ void Profiler::transform_profiler_data (const string &output_file_suffix, const 
         cout << "Unknown result when executing Python: "<< endl;
     }
 }
+#else
+void Profiler::transform_profiler_data (const string &output_file_suffix, const string &formatter) {
+}
+
+#endif // FLOW123D_HAVE_PYTHON
 
 
 void Profiler::uninitialize() {
