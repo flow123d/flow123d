c1b69cb235eec73fec24435d7f1257422fbb4731
Jan Brezina <jan.brezina@tul.cz>
2016-01-10 13:55:25 +0100

    Update GeoMop

--- src/python/GeoMop/ModelEditor/util/constants.py
+++ src/python/GeoMop/ModelEditor/util/constants.py
@@ -3,6 +3,10 @@
 .. codeauthor:: Tomas Krizek <tomas.krizek1@tul.cz>
 """
 
+import platform
+
 CONTEXT_NAME = 'ModelEditor'
 
-DEFAULT_FONT = 'Courier,11,-1,5,50,0,0,0,0,0'  # Consolas for Win 10 (7, 8?)
+DEFAULT_FONT = 'Courier,11,-1,5,50,0,0,0,0,0'
+if platform.system() == 'Windows' and platform.release() != 'XP':
+    DEFAULT_FONT = 'Consolas,11,-1,5,50,0,0,0,0,0'
