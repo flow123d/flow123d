fa5b6417baa9d7015499272202ae2648a5255c34
Pavel Exner <pavelexner@gmail.com>
2016-02-09 11:42:23 +0100

    Add prolongation unit test for 1d-3d intersections.

---
+++ unit_tests/mesh/intersection_prolongation_test.cpp
@@ -0,0 +1,209 @@
+/*
+ * intersection_area_test.cpp
+ *
+ *  Created on: Nov 14, 2014
+ *      Author: VF, PE
+ */
+#define TEST_USE_PETSC
+#include <flow_gtest_mpi.hh>
+
+#include "system/system.hh"
+#include "system/sys_profiler.hh"
+#include "system/file_path.hh"
+#include "mesh/mesh.h"
+#include "mesh/msh_gmshreader.h"
+
+#include "mesh/ngh/include/point.h"
+#include "mesh/ngh/include/intersection.h"
+
+#include "intersection/inspectelements.h"
+#include "intersection/intersectionpoint.h"
+#include "intersection/intersectionline.h"
+
+#include <dirent.h>
+
+using namespace std;
+using namespace computeintersection;
+
+//static const std::string profiler_file = "intersection_profiler.log";
+//static const unsigned int profiler_loop = 10000;
+
+//*
+
+// ******************************************************************************************* TEST 1d-3d ****
+
+/// Create results for the meshes in directory 'site_13d'.
+void fill_13d_solution(std::vector<computeintersection::IntersectionLine> &ils)
+{
+    ils.clear();
+    ils.resize(1);
+    
+    ils[0].points().push_back(computeintersection::IntersectionPoint<1,3>(arma::vec::fixed<2>({1,0}),arma::vec::fixed<4>({1,1,1,1})/4));
+    ils[0].points().push_back(computeintersection::IntersectionPoint<1,3>(arma::vec::fixed<2>({1,1})/2,arma::vec::fixed<4>({4,3,0,3})*0.1));
+}
+
+
+//Permutes tetrahedron coordinates of IP<1,3> according to given permutation.
+// computeintersection::IntersectionLine permute_coords(computeintersection::IntersectionLine il, unsigned int permute[4])
+// {
+//     computeintersection::IntersectionLine new_il = il;
+//     std::vector<computeintersection::IntersectionPoint<1,3>> & points = il.points();
+//     for(unsigned int i = 0; i < points.size(); i++)
+//     {
+//         arma::vec::fixed<4> new_coords;
+//         for(unsigned int j = 0; j < 4; j++)
+//             new_coords[j] = points[i].local_bcoords_B()[permute[j]];
+//         
+//         new_il.points()[i].set_coordinates(points[i].local_bcoords_A(), new_coords);
+//     }
+//     return new_il;
+// }
+
+void compute_intersection_13d(Mesh *mesh, const computeintersection::IntersectionLine &il)
+{
+    double length = 0;
+
+    // compute intersection
+    DBGMSG("Computing intersection length by NEW algorithm\n");
+    InspectElements ie(mesh);
+    ie.compute_intersections<1,3>();
+    
+//     //test solution
+//     std::vector<computeintersection::IntersectionLine> pp = ie.list_intersection_lines(1);
+//     computeintersection::IntersectionLine ilc;
+//     // component = element index == 1
+//     if(pp.size() > 0)
+//     {
+//         ilc = pp[0];
+//         EXPECT_EQ(ilc.size(), il.size());
+//     }
+//     
+//     for(unsigned int i=0; i < ilc.size(); i++)
+//     {
+//         DBGMSG("---------- check IP[%d] ----------\n",i);
+//         EXPECT_DOUBLE_EQ(ilc[i].local_bcoords_A()[0], il[i].local_bcoords_A()[0]);
+//         EXPECT_DOUBLE_EQ(ilc[i].local_bcoords_A()[1], il[i].local_bcoords_A()[1]);
+//         EXPECT_DOUBLE_EQ(ilc[i].local_bcoords_B()[0], il[i].local_bcoords_B()[0]);
+//         EXPECT_DOUBLE_EQ(ilc[i].local_bcoords_B()[1], il[i].local_bcoords_B()[1]);
+//         EXPECT_DOUBLE_EQ(ilc[i].local_bcoords_B()[2], il[i].local_bcoords_B()[2]);
+//         EXPECT_DOUBLE_EQ(ilc[i].local_bcoords_B()[3], il[i].local_bcoords_B()[3]);
+//     }
+    
+    length = ie.line_length();
+    ie.print_mesh_to_file_1D("output_intersection");
+    DBGMSG("Length of intersection line: (intersections) %.16e\n", length);
+//     EXPECT_NEAR(length1, length2, 1e-12);
+    EXPECT_DOUBLE_EQ(length,1.5*std::sqrt(0.27)+0.35+0.2
+                            + std::sqrt(0.065) + 0.1*std::sqrt(18) + std::sqrt(0.105));
+}
+
+
+TEST(intersection_prolongation_13d, all) {
+    Profiler::initialize();
+    
+    // directory with testing meshes
+    string dir_name = string(UNIT_TESTS_SRC_DIR) + "/mesh/meshes_prolongation/";
+    std::vector<string> filenames;
+    
+    // read mesh file names
+    DIR *dir;
+    struct dirent *ent;
+    if ((dir = opendir (dir_name.c_str())) != NULL) {
+        // print all the files and directories within directory 
+        xprintf(Msg,"Testing mesh files: \n");
+        while ((ent = readdir (dir)) != NULL) {
+            string fname = ent->d_name;
+            // test extension ".msh"
+            if(fname.size() >= 4)
+            {
+                string ext = fname.substr(fname.size()-4);
+//                 xprintf(Msg,"%s\n",ext.c_str());
+                if(ext == ".msh"){
+                    filenames.push_back(ent->d_name);
+                    xprintf(Msg,"%s\n",ent->d_name);
+                }
+            }
+        }
+        closedir (dir);
+    } else {
+        ASSERT(0,"Could not open directory with testing meshes.");
+    }
+    
+    std::sort(filenames.begin(), filenames.end(), less<string>());
+    
+    std::vector<IntersectionLine> solution;
+    fill_13d_solution(solution);
+    
+    // for each mesh, compute intersection area and compare with old NGH
+    for(unsigned int s=0; s< filenames.size(); s++)
+    {
+//         const unsigned int np = 24;
+//         unsigned int permutations[np][4] = {{0,1,2,3},
+//                                                 {0,1,3,2},  // the tab means permutation with negative jacobian
+//                                             {0,3,1,2},
+//                                                 {0,3,2,1},
+//                                             {0,2,3,1},
+//                                                 {0,2,1,3},
+//                                                 {1,0,2,3},
+//                                             {1,0,3,2},
+//                                                 {1,3,0,2},
+//                                             {1,3,2,0},
+//                                                 {1,2,3,0},
+//                                             {1,2,0,3},
+//                                                 {2,1,0,3},
+//                                             {2,1,3,0},
+//                                                 {2,3,1,0},
+//                                             {2,3,0,1},
+//                                                 {2,0,3,1},
+//                                             {2,0,1,3},
+//                                                 {3,1,2,0},
+//                                             {3,1,0,2},
+//                                                 {3,0,1,2},
+//                                             {3,0,2,1},
+//                                                 {3,2,0,1},
+//                                             {3,2,1,0}};
+//         for(unsigned int p=0; p<np; p++)
+//         {
+            xprintf(Msg,"Computing intersection on mesh: %s\n",filenames[s].c_str());
+            FilePath::set_io_dirs(".","","",".");
+            FilePath mesh_file(dir_name + filenames[s], FilePath::input_file);
+            
+            Mesh mesh;
+            // read mesh with gmshreader
+            GmshMeshReader reader(mesh_file);
+            reader.read_mesh(&mesh);
+        
+//             // permute nodes:
+//             FOR_ELEMENTS(&mesh,ele)
+//             {
+//                 if(ele->dim() == 3)
+//                 {
+//                     Node* tmp[4];
+//                     for(unsigned int i=0; i<ele->n_nodes(); i++)
+//                     {
+//                         tmp[i] = ele->node[permutations[p][i]];
+//                     }
+//                     for(unsigned int i=0; i<ele->n_nodes(); i++)
+//                     {
+//                         ele->node[i] = tmp[i];
+// //                         ele->node[i]->point().print(cout);
+//                     }
+// //                     cout << p << ": jac = "  << ele->tetrahedron_jacobian() << endl;
+//                 }
+//             }
+            
+            mesh.setup_topology();
+            
+            xprintf(Msg, "==============\n");
+//             for(unsigned int loop = 0; loop < profiler_loop; loop++)
+                compute_intersection_13d(&mesh, solution[s]); //permute_coords(solution[s], permutations[p]));
+            xprintf(Msg, "==============\n");
+//         }
+    }
+//     std::fstream fs;
+//     fs.open(profiler_file.c_str(), std::fstream::out | std::fstream::app);
+//     Profiler::instance()->output(PETSC_COMM_WORLD, fs);
+    Profiler::uninitialize();
+}
+
+//*/
