7dadecdf64425bb712b25262ed2e9777c1a3ca26
Pavel Exner <pavelexner@gmail.com>
2016-03-15 09:53:23 +0100

    Add auxiliary intersection class.

---
+++ src/intersection/intersectionaux.cpp
@@ -0,0 +1,62 @@
+/*
+ *  Created on: 14.3.2016
+ *  Author: pex
+ */
+
+#include "intersectionaux.h"
+#include "intersectionpoint.h"
+
+
+namespace computeintersection{
+
+template<unsigned int dimA, unsigned int dimB>
+IntersectionAux<dimA,dimB>::IntersectionAux(unsigned int component_element_idx, unsigned int bulk_element_idx)
+: component_element_idx_(component_element_idx), bulk_element_idx_(bulk_element_idx)
+{}
+
+template<unsigned int dimA, unsigned int dimB>
+IntersectionAux<dimA,dimB>::IntersectionAux()
+{}
+
+template<unsigned int dimA, unsigned int dimB>
+IntersectionAux<dimA,dimB>::~IntersectionAux()
+{}
+
+
+
+// 1D-3D
+
+template<>
+double IntersectionAux<1,3>::compute_measure()
+{
+    ASSERT(i_points_.size() > 1, "Not enough intersetion points to define a line.");
+    double length = 0;
+    for(unsigned int i=0; i < i_points_.size()-1; i++)
+    {
+        length += std::abs(i_points_[i].local_bcoords_A()[1] - i_points_[i+1].local_bcoords_A()[1]);
+    }
+    return length;
+}
+
+
+
+// 2D-3D
+
+template<>
+double IntersectionAux<2,3>::compute_measure()
+{
+    double subtotal = 0.0;
+    for(unsigned int j = 2; j < i_points_.size();j++){
+        //xprintf(Msg, "volani %d %d\n",j, i_points_.size());
+        subtotal += fabs(i_points_[0].local_bcoords_A()(1)*(i_points_[j-1].local_bcoords_A()(2) - i_points_[j].local_bcoords_A()(2)) +
+                 i_points_[j-1].local_bcoords_A()(1)*(i_points_[j].local_bcoords_A()(2) - i_points_[0].local_bcoords_A()(2)) +
+                 i_points_[j].local_bcoords_A()(1)*(i_points_[0].local_bcoords_A()(2) - i_points_[j-1].local_bcoords_A()(2)));
+    }
+    return fabs(subtotal/2);
+}
+
+
+template class IntersectionAux<1,2>;
+template class IntersectionAux<1,3>;
+template class IntersectionAux<2,3>;
+}
---
+++ src/intersection/intersectionaux.h
@@ -0,0 +1,81 @@
+/*
+ *  Created on: 14.3.2016
+ *  Author: pex
+ */
+
+#ifndef INTERSECTIONAUX_H_
+#define INTERSECTIONAUX_H_
+
+#include "system/system.hh"
+
+namespace computeintersection{
+
+//forwward declare
+template<unsigned int, unsigned int> class IntersectionPoint;
+
+template<unsigned int dimA, unsigned int dimB>
+class IntersectionAux{
+
+    std::vector<IntersectionPoint<dimA,dimB>> i_points_;
+    unsigned int component_element_idx_;
+    unsigned int bulk_element_idx_;
+    bool pathologic_;
+    
+public:
+
+    IntersectionAux();                                       ///< Default constructor.
+    IntersectionAux(unsigned int component_element_idx, unsigned int bulk_element_idx);  ///< Constructor taking in element indices.
+    virtual ~IntersectionAux();                                      ///< Destructor.
+
+    /// Returns intersection points by a reference.
+    std::vector<IntersectionPoint<dimA,dimB>> &points();
+
+    /// Returns intersection points by a constant reference.
+    const std::vector<IntersectionPoint<dimA,dimB>> &points() const;
+
+    /// Returns intersection point of given @p index.
+    const IntersectionPoint<dimA,dimB> &operator[](unsigned int index) const;
+    
+    unsigned int size() const;              ///< Returns number of intersection points.
+    unsigned int component_ele_idx() const; ///< Returns index of component element.
+    unsigned int bulk_ele_idx() const;      ///< Returns index of bulk element.
+    unsigned int is_pathologic() const;      ///< Returns index of bulk element.
+    
+    /// Computes the relative measure of intersection object.
+    double compute_measure();
+};
+
+/********************************************* IMPLEMENTATION ***********************************************/
+
+template<unsigned int dimA, unsigned int dimB>
+inline std::vector< IntersectionPoint< dimA, dimB > >& IntersectionAux<dimA,dimB>::points()
+{   return i_points_; }
+
+template<unsigned int dimA, unsigned int dimB>
+inline const std::vector< IntersectionPoint< dimA, dimB > >& IntersectionAux<dimA,dimB>::points() const
+{   return i_points_; }
+
+template<unsigned int dimA, unsigned int dimB>
+inline const IntersectionPoint< dimA, dimB >& IntersectionAux<dimA,dimB>::operator[](unsigned int index) const
+{   ASSERT(index < i_points_.size(), "Index out of bounds.");
+    return i_points_[index]; }
+
+template<unsigned int dimA, unsigned int dimB>
+inline unsigned int IntersectionAux<dimA,dimB>::size() const
+{   return i_points_.size(); }
+
+template<unsigned int dimA, unsigned int dimB>
+inline unsigned int IntersectionAux<dimA,dimB>::component_ele_idx() const
+{   return component_element_idx_; }
+
+template<unsigned int dimA, unsigned int dimB>
+inline unsigned int IntersectionAux<dimA,dimB>::bulk_ele_idx() const
+{   return bulk_element_idx_; }
+
+template<unsigned int dimA, unsigned int dimB>
+inline unsigned int IntersectionAux<dimA,dimB>::is_pathologic() const
+{   return pathologic_; }
+
+
+} // END NAMESPACE
+#endif /* INTERSECTIONAUX_H_ */
