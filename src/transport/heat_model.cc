96d81c207dfa27de14a6b26c152cc9ee9a7972f4
jstebel <jan.stebel@tul.cz>
2016-03-17 12:56:43 +0100

    Apply disable_where() to input fields in TransportDG.

--- src/transport/heat_model.cc
+++ src/transport/heat_model.cc
@@ -79,12 +79,14 @@ HeatTransferModel::ModelEqData::ModelEqData()
             .input_default("0.0")
             .flags_add(in_rhs);
 	*this+=bc_flux
+	        .disable_where(bc_type, { bc_dirichlet, bc_inflow })
 			.name("bc_flux")
 			.description("Flux in Neumann boundary condition.")
 			.units( UnitSI().kg().m().s(-1).md() )
 			.input_default("0.0")
 			.flags_add(FieldFlag::in_rhs);
 	*this+=bc_robin_sigma
+	        .disable_where(bc_type, { bc_dirichlet, bc_inflow })
 			.name("bc_robin_sigma")
 			.description("Conductivity coefficient in Robin boundary condition.")
 			.units( UnitSI().m(4).s(-1).md() )
