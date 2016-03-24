ff8709d5a26aaa437db29f8d5db0b55e2d6dd35c
Pavel Exner <pavelexner@gmail.com>
2016-03-24 13:49:59 +0100

    Split compute intersection unit tests.

--- unit_tests/intersection/compute_intersection_13_test.cpp
+++ unit_tests/intersection/compute_intersection_13_test.cpp
@@ -185,7 +185,7 @@ TEST(intersections_12d, all) {
 
 // ******************************************************************************************* TEST 1d-3d ****
 
-/// Create results for the meshes in directory 'site_13d'.
+/// Create results for the meshes in directory 'simple_meshes_13d'.
 void fill_13d_solution(std::vector<computeintersection::IntersectionLocal<1,3>> &ils)
 {
     ils.clear();
@@ -401,139 +401,6 @@ TEST(intersections_13d, all) {
             xprintf(Msg, "==============\n");
         }
     }
-    std::fstream fs;
-    fs.open(profiler_file.c_str(), std::fstream::out | std::fstream::app);
-    Profiler::instance()->output(PETSC_COMM_WORLD, fs);
-    Profiler::uninitialize();
-}
-
-//*/
-
-//*
-
-
-// ******************************************************************************************* TEST 2d-3d ****
-void compute_intersection_area_23d(Mesh *mesh)
-{
-    double area1, area2 = 0;
-
-    // compute intersection by NGH
-    DBGMSG("Computing polygon area by NGH algorithm\n");
-    TTriangle ttr;
-    TTetrahedron tte;
-    TIntersectionType it = area;
-
-    FOR_ELEMENTS(mesh, elm) {
-        if (elm->dim() == 2) {
-        ttr.SetPoints(TPoint(elm->node[0]->point()(0), elm->node[0]->point()(1), elm->node[0]->point()(2)),
-                     TPoint(elm->node[1]->point()(0), elm->node[1]->point()(1), elm->node[1]->point()(2)),
-                     TPoint(elm->node[2]->point()(0), elm->node[2]->point()(1), elm->node[2]->point()(2)) );
-        }else if(elm->dim() == 3){
-        tte.SetPoints(TPoint(elm->node[0]->point()(0), elm->node[0]->point()(1), elm->node[0]->point()(2)),
-                     TPoint(elm->node[1]->point()(0), elm->node[1]->point()(1), elm->node[1]->point()(2)),
-                     TPoint(elm->node[2]->point()(0), elm->node[2]->point()(1), elm->node[2]->point()(2)),
-                     TPoint(elm->node[3]->point()(0), elm->node[3]->point()(1), elm->node[3]->point()(2)));
-//         elm->node[0]->point().print();
-//         elm->node[1]->point().print();
-//         elm->node[2]->point().print();
-//         elm->node[3]->point().print();
-//         
-//         elm->side(0)->node(0)->point().print();
-//         elm->side(0)->node(1)->point().print();
-//         elm->side(0)->node(2)->point().print();
-        }
-    }
-    GetIntersection(ttr, tte, it, area2);
-    
-    
-    // compute intersection
-    DBGMSG("Computing polygon area by NEW algorithm\n");
-    InspectElements ie(mesh);
-    ie.compute_intersections();
-    area1 = ie.measure_23();
-
-//     ie.print_mesh_to_file_23("output_intersection_23");
-    
-    DBGMSG("Polygon area: (intersections) %.16e,\t(NGH) %.16e\n", area1, area2);
-    EXPECT_NEAR(area1, area2, 1e-14);
-//     EXPECT_DOUBLE_EQ(area1,area2);
-}
-
-
-TEST(area_intersections, all) {
-    Profiler::initialize();
-    
-    // directory with testing meshes
-    string dir_name = string(UNIT_TESTS_SRC_DIR) + "/intersection/simple_meshes_23d/";
-    std::vector<string> filenames;
-    
-    // read mesh file names
-    DIR *dir;
-    struct dirent *ent;
-    if ((dir = opendir (dir_name.c_str())) != NULL) {
-        // print all the files and directories within directory 
-        xprintf(Msg,"Testing mesh files: \n");
-        while ((ent = readdir (dir)) != NULL) {
-            string fname = ent->d_name;
-            // test extension ".msh"
-            if(fname.size() >= 4)
-            {
-                string ext = fname.substr(fname.size()-4);
-//                 xprintf(Msg,"%s\n",ext.c_str());
-                if(ext == ".msh"){
-                    filenames.push_back(ent->d_name);
-                    xprintf(Msg,"%s\n",ent->d_name);
-                }
-            }
-        }
-        closedir (dir);
-    } else {
-        ASSERT(0,"Could not open directory with testing meshes.");
-    }
-    
-    std::sort(filenames.begin(), filenames.end(), less<string>());
-
-    // for each mesh, compute intersection area and compare with old NGH
-    for(auto &fname : filenames)
-    {
-        unsigned int permutations[6][3] = {{0,1,2},{0,2,1},{1,0,2},{1,2,0},{2,0,1},{2,1,0}};
-        for(unsigned int p=0; p<6; p++)
-        {
-            xprintf(Msg,"Computing intersection on mesh: %s\n",fname.c_str());
-            FilePath::set_io_dirs(".","","",".");
-            FilePath mesh_file(dir_name + fname, FilePath::input_file);
-            
-            Mesh mesh;
-            // read mesh with gmshreader
-            GmshMeshReader reader(mesh_file);
-            reader.read_mesh(&mesh);
-            
-            // permute nodes:
-            FOR_ELEMENTS(&mesh,ele)
-            {
-                if(ele->dim() == 2)
-                {
-                    Node* tmp[3];
-                    for(unsigned int i=0; i<ele->n_nodes(); i++)
-                    {
-                        tmp[i] = ele->node[permutations[p][i]];
-                    }
-                    for(unsigned int i=0; i<ele->n_nodes(); i++)
-                    {
-                        ele->node[i] = tmp[i];
-//                         ele->node[i]->point().print(cout);
-                    }
-                }
-            }
-            mesh.setup_topology();
-            
-            xprintf(Msg, "==============\n");
-            for(unsigned int loop = 0; loop < profiler_loop; loop++)
-                compute_intersection_area_23d(&mesh);
-            xprintf(Msg, "==============\n");
-        }
-    }
-    
     std::fstream fs;
     fs.open(profiler_file.c_str(), std::fstream::out | std::fstream::app);
     Profiler::instance()->output(PETSC_COMM_WORLD, fs);
