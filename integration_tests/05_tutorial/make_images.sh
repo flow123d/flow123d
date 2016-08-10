be1878e9b9824108584d221b79907b732546e026
jstebel <jan.stebel@tul.cz>
2016-08-10 10:51:18 +0200

    Reduce tutorials 2 and 3 to just one.

--- integration_tests/05_tutorial/make_images.sh
+++ integration_tests/05_tutorial/make_images.sh
@@ -8,24 +8,23 @@ pdfjam 01_mesh.pdf 01_bc.pdf 01_flow.pdf --nup 3x1 --papersize '{20cm,10cm}' --d
 rm 01_mesh.pdf 01_flow.pdf
 
 
-# Tutorial 03
+# Tutorial 02
 
 # make plot of mass and mass flux
 echo "
 set terminal pdf color enhanced lw 2
-set output '03_mass_plot.pdf'
+set output '02_mass_plot.pdf'
 set xlabel 'time [years]'
 set ylabel 'mass in the rock [kg]'
 set y2label 'mass flux through .surface/.tunnel [kg/s]'
 set key top center out horizontal
 set ytics nomirror
 set y2tics
-plot '<grep rock ref_out/03_column_transport/mass_balance.txt' u (\$1/86400/365):7 w l axes x1y1 t 'rock',\\
-     '<grep .surface ref_out/03_column_transport/mass_balance.txt' u (\$1/86400/365):4 w l axes x1y2 t '.surface',\\
-     '<grep .tunnel ref_out/03_column_transport/mass_balance.txt' u (\$1/86400/365):4 w l axes x1y2 t '.tunnel'" | gnuplot
+plot '<grep rock ref_out/02_column_transport/mass_balance.txt' u (\$1/86400/365):7 w l axes x1y1 t 'rock',\\
+     '<grep .surface ref_out/02_column_transport/mass_balance.txt' u (\$1/86400/365):4 w l axes x1y2 t '.surface',\\
+     '<grep .tunnel ref_out/02_column_transport/mass_balance.txt' u (\$1/86400/365):4 w l axes x1y2 t '.tunnel'" | gnuplot
 
 # make image with concentrations
 gmsh make_transport.geo
-#pnmcat -lr <(pngtopnm 03_transport_1.png) <(pngtopnm 03_transport_2.png) <(pngtopnm 03_transport_3.png) <(pngtopnm 03_transport_4.png) | pnmtopng > 03_transport.png
-pdfjam 03_transport_1.pdf 03_transport_2.pdf 03_transport_3.pdf 03_transport_4.pdf --nup 4x1 --papersize '{20cm,10cm}' --outfile 03_transport.pdf
-rm 03_transport_[1234].pdf
+pdfjam 02_transport_1.pdf 02_transport_2.pdf 02_transport_3.pdf 02_transport_4.pdf --nup 4x1 --papersize '{20cm,10cm}' --outfile 02_transport.pdf
+rm 02_transport_[1234].pdf
