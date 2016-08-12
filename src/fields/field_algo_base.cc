46e9e1aa95062c725113f3bab84177857e2961b0
David Flanderka <t426.001@centrum.cz>
2016-08-12 16:38:28 +0200

    Revert "Complete remove of Vector fields."
    This reverts commit b4fe6fd06d7b24c73e1e95a760bf5216453c3c05.

--- src/fields/field_algo_base.cc
+++ src/fields/field_algo_base.cc
@@ -41,7 +41,7 @@ template class FieldAddPotential<3, FieldValue<0>::Scalar >;
 //template class FieldAddPotential<2, FieldValue<0>::Scalar >;
 
 // temporary solution for computing more fields at once in python
-//template class FieldConstant<3, FieldValue<0>::Vector >; // Necessary due to default value of the abstract.
-//template class FieldPython<3, FieldValue<0>::Vector >;
+template class FieldConstant<3, FieldValue<0>::Vector >; // Necessary due to default value of the abstract.
+template class FieldPython<3, FieldValue<0>::Vector >;
 
 
