671acf1ae8393d053a46c52719e8cdc51060ff3d
David Flanderka <t426.001@centrum.cz>
2016-08-16 14:06:59 +0200

    Add unit test of binary VTK output.

--- unit_tests/output/output_vtk_test.cpp
+++ unit_tests/output/output_vtk_test.cpp
@@ -16,15 +16,19 @@
 #include "system/sys_profiler.hh"
 #include "system/logger_options.hh"
 
-const string test_output_time_input = R"JSON(
-{
-  file = "./test1.pvd", 
-  format = {
-    TYPE = "vtk", 
-    variant = "ascii"
-  } 
-}
-)JSON";
+FLOW123D_FORCE_LINK_IN_PARENT(field_constant)
+
+const string test_output_time_ascii = R"YAML(
+file: ./test1.pvd
+format: !vtk
+  variant: ascii
+)YAML";
+
+const string test_output_time_binary = R"YAML(
+file: ./test1.pvd
+format: !vtk
+  variant: binary
+)YAML";
 
 
 class TestOutputVTK : public testing::Test, public OutputVTK {
@@ -32,37 +36,79 @@ public:
     TestOutputVTK()
     : OutputVTK()
     {
-
-
         Profiler::initialize();
-        auto in_rec = Input::ReaderToStorage(test_output_time_input, OutputTime::get_input_type(), Input::FileFormat::format_JSON)
-                      .get_root_interface<Input::Record>();
-
         LoggerOptions::get_instance().set_log_file("");
 
-        FilePath mesh_file( string(UNIT_TESTS_SRC_DIR) + "/mesh/simplest_cube.msh", FilePath::input_file);
+        FilePath mesh_file( string(UNIT_TESTS_SRC_DIR) + "/fields/simplest_cube_3d.msh", FilePath::input_file);
         this->_mesh = new Mesh();
         ifstream in(string(mesh_file).c_str());
         this->_mesh->read_gmsh_from_stream(in);
-        this->init_from_input("dummy_equation", *(this->_mesh), in_rec);
-        
-        // create output mesh identical to computational mesh
-        this->output_mesh_ = std::make_shared<OutputMesh>(*(this->_mesh));
-        this->output_mesh_->create_identical_mesh();
+
+        component_names = { "comp_0", "comp_1", "comp_2" };
     }
 
     ~TestOutputVTK()
     {
         delete this->_mesh;
+        LoggerOptions::get_instance().reset();
     }
+
+    // initialize mesh with given yaml input
+    void init_mesh(string input_yaml)
+    {
+        auto in_rec = Input::ReaderToStorage(input_yaml, OutputTime::get_input_type(), Input::FileFormat::format_YAML)
+        				.get_root_interface<Input::Record>();
+        this->init_from_input("dummy_equation", *(this->_mesh), in_rec);
+
+        // create output mesh identical to computational mesh
+        this->output_mesh_ = std::make_shared<OutputMesh>(*(this->_mesh));
+        this->output_mesh_->create_identical_mesh();
+
+    }
+
+	template <class FieldType>
+	void set_field_data(string field_name, string init)
+    {
+
+		// make field init it form the init string
+	    FieldType field(field_name, false); // bulk field
+		field.input_default(init);
+		field.set_components(component_names);
+
+		field.set_mesh( *(this->_mesh) );
+		field.set_time(TimeGovernor(0.0, 1.0).step(), LimitSide::left);
+		field.units(UnitSI::one());
+
+        // create output mesh identical to computational mesh
+        this->output_mesh_ = std::make_shared<OutputMesh>( *(this->_mesh) );
+        this->output_mesh_->create_identical_mesh();
+
+        this->output_mesh_discont_ = std::make_shared<OutputMeshDiscontinuous>( *(this->_mesh) );
+        this->output_mesh_discont_->create_mesh(this->output_mesh_);
+
+		this->compute_field_data(ELEM_DATA, field);
+	}
+
+	std::vector<string> component_names;
 };
 
 
-TEST_F(TestOutputVTK, write_data) {
+TEST_F(TestOutputVTK, write_data_ascii) {
+	this->init_mesh(test_output_time_ascii);
     this->current_step=1;
     this->write_data();
     EXPECT_EQ("./test1.pvd", string(this->_base_filename));
     EXPECT_EQ("test1", this->main_output_basename_);
     EXPECT_EQ(".", this->main_output_dir_);
+}
+
+
+TEST_F(TestOutputVTK, write_data_binary) {
+	this->init_mesh(test_output_time_binary);
+    this->current_step=0;
+    set_field_data< Field<3,FieldValue<0>::Scalar> > ("scalar_field", "0.5");
+    set_field_data< Field<3,FieldValue<3>::VectorFixed> > ("vector_field", "[0.5, 1.0, 1.5]");
+    set_field_data< Field<3,FieldValue<3>::TensorFixed> > ("tensor_field", "[[1, 2, 3], [4, 5, 6], [7, 8, 9]]");
+    this->write_data();
 }
 
