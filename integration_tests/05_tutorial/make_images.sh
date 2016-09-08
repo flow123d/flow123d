4df6d292bb03aff1e5405d5ec171ed959188f6a4
jstebel <jan.stebel@tul.cz>
2016-09-08 12:11:47 +0200

    Tutorial 5 (heat transport in geothermal heat exchanger)

--- integration_tests/05_tutorial/make_images.sh
+++ integration_tests/05_tutorial/make_images.sh
@@ -57,3 +57,21 @@ plot '03_conc_tunnel.txt' u (timecolumn(1,\"%d.%m.%Y\")):2 w l lw 0.1 lc \"00000
 
 # make image of flux and pressure
 gmsh make_03.geo
+
+
+
+# Tutorial 05
+
+#image with bc and mesh
+gmsh make_05.geo
+pdfjam 05_bc.pdf 05_mesh.pdf --nup 2x1 --papersize '{20cm,10cm}' --outfile 05_bcmesh.pdf
+rm -rf 05_mesh.pdf
+
+#make plot of production power
+echo "
+set terminal pdf color enhanced lw 2
+set output '05_power.pdf'
+set xlabel 'time [years]'
+plot [1:] \"< sed -n '/\\\\.well[12]_surface\\\"/p' ref_out/05_heat/energy_balance.txt | awk 'BEGIN {t=0;v=0} {if (t==\$1) v+=\$4; else {print t, v; t=\$1;v=\$4}} END {print t,v}'\" u (\$1/365.25/86400):(-\$2/1e6) w lp t 'power [MW]',\\
+          25 dt 2 not
+" | gnuplot
