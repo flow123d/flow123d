f7e3af9d41b78bb92d276f4c496b85fdd01b0e01
David Flanderka <t426.001@centrum.cz>
2016-10-04 14:45:10 +0200

    Add new class FieldTimeFunction.

---
+++ src/fields/field_time_function.cc
@@ -0,0 +1,75 @@
+/*!
+ *
+﻿ * Copyright (C) 2015 Technical University of Liberec.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License version 3 as published by the
+ * Free Software Foundation. (http://www.gnu.org/licenses/gpl-3.0.en.html)
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ *
+ * @file    field_time_function.cc
+ * @brief
+ */
+
+#include "fields/field_time_function.hh"
+#include "fields/field_instances.hh"	// for instantiation macros
+#include "fields/table_function.hh"
+
+namespace it = Input::Type;
+
+FLOW123D_FORCE_LINK_IN_CHILD(field_time_function)
+
+
+template <int spacedim, class Value>
+const Input::Type::Record & FieldTimeFunction<spacedim, Value>::get_input_type()
+{
+    return it::Record("FieldTimeFunction", FieldAlgorithmBase<spacedim,Value>::template_name()+" Field time-dependent function in space.")
+        .derive_from(FieldAlgorithmBase<spacedim, Value>::get_input_type())
+		.declare_key("time_function", TableFunction<Value>::get_input_type(), it::Default::obligatory(),
+									"Values of time series initialization of Field.")
+        .allow_auto_conversion("time_function")
+		.close();
+}
+
+template <int spacedim, class Value>
+const int FieldTimeFunction<spacedim, Value>::registrar =
+		Input::register_class< FieldTimeFunction<spacedim, Value>, unsigned int >("FieldTimeFunction") +
+		FieldTimeFunction<spacedim, Value>::get_input_type().size();
+
+
+template <int spacedim, class Value>
+FieldTimeFunction<spacedim, Value>::FieldTimeFunction( unsigned int n_comp)
+: FieldConstant<spacedim, Value>(n_comp)
+{}
+
+
+template <int spacedim, class Value>
+void FieldTimeFunction<spacedim, Value>::init_from_input(const Input::Record &rec)
+{
+	this->in_rec_ = rec;
+}
+
+
+template <int spacedim, class Value>
+bool FieldTimeFunction<spacedim, Value>::set_time(const TimeStep &time)
+{
+	TableFunction<Value> table_function;
+
+	if (!Value::is_scalable()) {
+		WarningOut().fmt("Setting key 'time_function' of non-floating point field at address {}\nValues will be skipped.\n",
+				in_rec_.address_string());
+	}
+	table_function.init_from_input( in_rec_.val<Input::Record>("time_function") );
+	this->r_value_ = table_function.value( time.end() );
+
+	return true;
+}
+
+
+// Instantion of Fields
+INSTANCE_ALL(FieldTimeFunction)
+
---
+++ src/fields/field_time_function.hh
@@ -0,0 +1,65 @@
+/*!
+ *
+﻿ * Copyright (C) 2015 Technical University of Liberec.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License version 3 as published by the
+ * Free Software Foundation. (http://www.gnu.org/licenses/gpl-3.0.en.html)
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ *
+ * @file    field_time_function.hh
+ * @brief
+ */
+
+#ifndef FIELD_TIME_FUNCTION_HH_
+#define FIELD_TIME_FUNCTION_HH_
+
+#include "fields/field_constant.hh"
+
+/**
+ * Class representing spatially fields defined by time-dependent function.
+ *
+ */
+template <int spacedim, class Value>
+class FieldTimeFunction : public FieldConstant<spacedim, Value>
+{
+public:
+    typedef typename FieldAlgorithmBase<spacedim, Value>::Point Point;
+    typedef FieldAlgorithmBase<spacedim, Value> FactoryBaseType;
+
+    /**
+     * Return Record for initialization of FieldTimeFunction that is derived from Abstract given by @p a_type
+     * and the individual elements of the possible Value (vector, tensor) in given times have Input::Type @p eit.
+     */
+    static const Input::Type::Record & get_input_type();
+
+
+    /**
+     * Default constructor, optionally we need number of components @p n_comp in the case of Vector valued fields.
+     */
+    FieldTimeFunction(unsigned int n_comp=0);
+
+    /**
+     * This method initialize actual value of the field given from the given Input::Record @p rec.
+     */
+    void init_from_input(const Input::Record &rec);
+
+    /**
+     * Set time and init value_.
+     */
+    bool set_time(const TimeStep &time) override;
+
+private:
+    /// Registrar of class to factory
+    static const int registrar;
+
+    /// Accessor to Input::Record
+    Input::Record in_rec_;
+
+};
+
+#endif /* FIELD_TIME_FUNCTION_HH_ */
