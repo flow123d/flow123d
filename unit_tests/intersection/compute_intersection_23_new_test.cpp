6f5a418ef3529d36a0bc1c5d82899f84788ed793
Pavel Exner <pavelexner@gmail.com>
2016-10-04 18:15:45 +0200

    Create new analytical unit test for 23 intersections.

---
+++ unit_tests/intersection/compute_intersection_23_new_test.cpp
@@ -0,0 +1,234 @@
+/*
+ * intersection_area_test.cpp
+ *
+ *  Created on: Nov 14, 2014
+ *      Author: VF, PE
+ */
+#define TEST_USE_PETSC
+#include <flow_gtest_mpi.hh>
+
+#include "system/global_defs.h"
+#include "system/file_path.hh"
+#include "mesh/mesh.h"
+#include "mesh/msh_gmshreader.h"
+
+#include "mesh/ngh/include/point.h"
+#include "mesh/ngh/include/intersection.h"
+
+#include "intersection/inspect_elements.hh"
+#include "intersection/compute_intersection.hh"
+#include "intersection/intersection_point_aux.hh"
+#include "intersection/intersection_aux.hh"
+#include "intersection/intersection_local.hh"
+
+#include "arma_expect.hh"
+
+#include "compute_intersection_test.hh"
+
+using namespace std;
+using namespace computeintersection;
+
+
+/// Create results for the meshes in directory 'prolong_meshes_13d'.
+void fill_solution(std::vector<std::vector<arma::vec3>> &c)
+{
+//     unsigned int n_files=4;
+    c.clear();
+//     c.resize(n_files);
+    
+    c.push_back({
+                {0.2, 0, 0.2},
+                {0.2, 0.5, 0.2},
+                {0.4, 0, 0.3}});
+    c.push_back({
+                {0, 0.3, 0.1},
+                {0, 0.4, 0},
+                {0.2, 0.1, 0}});
+    c.push_back({
+                {0, 0.2, 0},
+                {0, 0.5, 0.5},
+                {0.2, 0.8, 0}});
+    c.push_back({
+                {0, 0, 0.6},
+                {0, 0.2, 0.8},
+                {0.6, 0, 0},
+                {0.8, 0.2, 0}});
+    c.push_back({
+                {0-1e-16, 0, 0.14},     //FIXME: rounding error, should be zero 
+                {0, 0.375, 0.125},
+                {0.2, 0.5, 0.2},
+                {0.4, 0, 0.3}});
+    c.push_back({
+                {0.05, 0.3, 0.2},
+                {0.1, 0.2, 0},
+                {0.3, 0.1, 0},
+                {0.35, 0.15, 0.2}});
+}
+
+/// auxiliary function for sorting intersection storage 13d
+/// TODO: move this into header and create a template for all dimension pairs
+// bool compare_is23(const computeintersection::IntersectionLocal<2,3>& a,
+//                   const computeintersection::IntersectionLocal<2,3>& b)
+// {
+//     if (a.component_ele_idx() == b.component_ele_idx())
+//         return a.bulk_ele_idx() <= b.bulk_ele_idx();
+//     else
+//         return a.component_ele_idx() < b.component_ele_idx();
+// }
+
+/// auxiliary function for sorting intersection point according to x,y local coordinates of component triangle
+bool compare_coords(const arma::vec3& a,
+                    const arma::vec3& b)
+{
+    double eps = std::numeric_limits<double>::epsilon();
+    if(std::fabs(a[0] - b[0]) <= eps){
+        if(std::fabs(a[1] - b[1]) < eps)
+            return a[2] <= b[2];
+        else
+            return a[1] < b[1];
+    }
+    else
+        return a[0] < b[0];
+}
+
+void compute_intersection_23d(Mesh *mesh, const std::vector<arma::vec3> &il){
+    
+    // fixed element indices in the tests
+    unsigned int triangle_ele_idx = 0,
+                 tetra_ele_idx = 1;
+    Simplex<2> triangle = create_simplex<2>(mesh->element(triangle_ele_idx));
+    Simplex<3> tetra = create_simplex<3>(mesh->element(tetra_ele_idx));
+    
+    IntersectionAux<2,3> is;
+    std::vector<unsigned int> prolong_table;    //FIXME with merge PE_prolong, remove prolong_table
+    ComputeIntersection< Simplex<2>, Simplex<3>> CI(triangle, tetra);
+    CI.init();
+//     CI.compute(is, prolong_table);
+    CI.compute(is);
+    
+//     cout << is;
+    for(IntersectionPointAux<2,3> &ip: is.points())
+    {
+        ip.coords(mesh->element(0)).print(std::cout,"ip");
+    }
+
+    EXPECT_EQ(il.size(), is.size());
+    
+    std::vector<arma::vec3> coords(is.size());
+    computeintersection::IntersectionLocal<2,3> temp_ilc(is);
+    //     std::cout << temp_ilc;
+    for(unsigned int i=0; i < is.size(); i++)
+    {
+        coords[i] = temp_ilc[i].coords(mesh->element(triangle_ele_idx));
+    }
+    // sort computed coords according to real coordinates
+    std::sort(coords.begin(), coords.end(),compare_coords);    
+    
+    
+    for(unsigned int i=0; i < coords.size(); i++)
+    {
+        MessageOut().fmt("---------- check IP[{}] ----------\n",i);
+        
+        EXPECT_ARMA_EQ(il[i],coords[i]);
+//         EXPECT_ARMA_EQ(il[i].comp_coords(), ilc[i].comp_coords());
+//         EXPECT_ARMA_EQ(il[i].bulk_coords(), ilc[i].bulk_coords());
+    }
+}
+
+void compare_with_ngh(Mesh *mesh)
+{
+    double area1, area2 = 0;
+
+    // compute intersection by NGH
+    MessageOut() << "Computing polygon area by NGH algorithm\n";
+    TTriangle ttr;
+    TTetrahedron tte;
+    TIntersectionType it = area;
+
+    FOR_ELEMENTS(mesh, elm) {
+        if (elm->dim() == 2) {
+        ttr.SetPoints(TPoint(elm->node[0]->point()(0), elm->node[0]->point()(1), elm->node[0]->point()(2)),
+                     TPoint(elm->node[1]->point()(0), elm->node[1]->point()(1), elm->node[1]->point()(2)),
+                     TPoint(elm->node[2]->point()(0), elm->node[2]->point()(1), elm->node[2]->point()(2)) );
+        }else if(elm->dim() == 3){
+        tte.SetPoints(TPoint(elm->node[0]->point()(0), elm->node[0]->point()(1), elm->node[0]->point()(2)),
+                     TPoint(elm->node[1]->point()(0), elm->node[1]->point()(1), elm->node[1]->point()(2)),
+                     TPoint(elm->node[2]->point()(0), elm->node[2]->point()(1), elm->node[2]->point()(2)),
+                     TPoint(elm->node[3]->point()(0), elm->node[3]->point()(1), elm->node[3]->point()(2)));
+//         elm->node[0]->point().print();
+//         elm->node[1]->point().print();
+//         elm->node[2]->point().print();
+//         elm->node[3]->point().print();
+//         
+//         elm->side(0)->node(0)->point().print();
+//         elm->side(0)->node(1)->point().print();
+//         elm->side(0)->node(2)->point().print();
+        }
+    }
+    GetIntersection(ttr, tte, it, area2);
+    
+    
+    // compute intersection
+    MessageOut() << "Computing polygon area by NEW algorithm\n";
+    InspectElements ie(mesh);
+    ie.compute_intersections(computeintersection::IntersectionType::d23);
+    area1 = ie.measure_23();
+
+//     ie.print_mesh_to_file_23("output_intersection_23");
+    
+    MessageOut().fmt("Polygon area: (intersections) {},\t(NGH) {}\n", area1, area2);
+    EXPECT_NEAR(area1, area2, 1e-14);
+//     EXPECT_DOUBLE_EQ(area1,area2);
+}
+
+
+TEST(area_intersections, all) {
+    // directory with testing meshes
+    FilePath::set_io_dirs(".",UNIT_TESTS_SRC_DIR,"",".");
+    string dir_name = string(UNIT_TESTS_SRC_DIR) + "/intersection/simple_meshes_23d_new/";
+    std::vector<string> filenames;
+    
+    read_files_form_dir(dir_name, "msh", filenames);
+    
+    //std::vector<computeintersection::IntersectionAux<2,3>> solution;
+    std::vector<std::vector<arma::vec3>> solution_coords;
+    fill_solution(solution_coords);
+    EXPECT_EQ(solution_coords.size(), filenames.size());
+    
+    // for each mesh, compute intersection area and compare with old NGH
+    unsigned int i_file=0;
+    for(auto &fname : filenames)
+    {
+        FilePath mesh_file(dir_name + fname, FilePath::input_file);
+        
+        const unsigned int np = permutations_triangle.size();
+        for(unsigned int p=0; p<np; p++){
+//             if (p>0) break; //FIXME dont forget to remove later
+            
+            const unsigned int npt = permutations_tetrahedron.size();
+            for(unsigned int pt=0; pt<npt; pt++){
+//                 if (pt>0) break; //FIXME dont forget to remove later
+                MessageOut().fmt("Computing intersection on mesh #{}: {} \n permutations  triangle #{}, tetrahedron #{}\n", i_file,  fname, p, pt);
+                
+                Mesh mesh;
+                // read mesh with gmshreader
+                GmshMeshReader reader(mesh_file);
+                reader.read_mesh(&mesh);
+                
+                // permute nodes:
+                FOR_ELEMENTS(&mesh,ele)
+                {
+                    if(ele->dim() == 2)
+                        permute_triangle(ele,p);
+                    if(ele->dim() == 3)
+                        permute_tetrahedron(ele,pt);
+                }
+                mesh.setup_topology();
+                
+                compare_with_ngh(&mesh);
+                compute_intersection_23d(&mesh, solution_coords[i_file]);
+            }
+        }
+        i_file++;
+    }
+}
