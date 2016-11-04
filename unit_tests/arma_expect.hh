d3fd8c5ca56d5913df18807e3fbe65e7d07a88e1
Jan Brezina <jan.brezina@tul.cz>
2016-11-04 23:54:00 +0100

    Fix ARMA_EXPECT, use also absolute criteria.

--- unit_tests/arma_expect.hh
+++ unit_tests/arma_expect.hh
@@ -29,21 +29,37 @@ arma::uvec mat_shape(const Mat &x) {
 template<class ArmaMat1, class ArmaMat2>
 bool expect_arma_eqal(const ArmaMat1 &ref_arma_mat, const ArmaMat2 &arma_mat)
 {
+    std::ostringstream fail_message;
+
+    bool no_failure=true;
     auto ref_shape = mat_shape(ref_arma_mat);
     auto shape = mat_shape(arma_mat);
     if (ref_shape[0] != shape[0] || ref_shape[1] != shape[1]) {
         EXPECT_EQ(ref_shape[0], shape[0]); // rows
         EXPECT_EQ(ref_shape[1], shape[1]); // cols
-        return false;
-    }
-    double magnitude = std::max( arma::norm(ref_arma_mat, 1), arma::norm(arma_mat, 1) );
-    double error = arma::norm(ref_arma_mat - arma_mat, 1)/magnitude;
-    if (error > 8*std::numeric_limits<double>::epsilon()) {
-        unsigned int w = 10* arma_mat.n_cols;
-        std::cerr << std::setw(w) << "Expected" << std::setw(w) << "Result" << std::endl;
-        for(unsigned int i_row = 0; i_row < arma_mat.n_rows; i_row++) {
-            std::cerr << std::setw(10) << ref_arma_mat.row(i_row) << std::setw(10) << arma_mat.row(i_row) << std::endl;
+        no_failure=false;
+    } else {
+        double magnitude = std::max( arma::norm(ref_arma_mat, 1), arma::norm(arma_mat, 1) );
+        // abs criterium
+        if (magnitude < 8*std::numeric_limits<double>::epsilon()) return true;
+
+        double error = arma::norm(ref_arma_mat - arma_mat, 1)/magnitude;
+        // rel criterium
+        if (error > 8*std::numeric_limits<double>::epsilon()) {
+            unsigned int w = 11* arma_mat.n_cols;
+            fail_message << std::setw(w) << "Expected" << std::setw(w) << "Result"
+                    << "rel. error: " << error << std::endl;
+            for(unsigned int i_row = 0; i_row < arma_mat.n_rows; i_row++) {
+                fail_message << std::setw(11) << ref_arma_mat.row(i_row)
+                             << std::setw(11) << arma_mat.row(i_row)
+                             << std::setw(20) << ref_arma_mat.row(i_row) - arma_mat.row(i_row) << std::endl;
+            }
+            no_failure=false;
         }
+    }
+
+    if (! no_failure) {
+        GTEST_NONFATAL_FAILURE_( fail_message.str().c_str() );
         return false;
     }
     return true;
