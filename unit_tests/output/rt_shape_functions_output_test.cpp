a67786625cbc0287d2c65310388ac910b0828842
Pavel Exner <pavelexner@gmail.com>
2016-11-05 13:06:41 +0100

    Fix and improve output rt shape functions output test.

--- unit_tests/output/rt_shape_functions_output_test.cpp
+++ unit_tests/output/rt_shape_functions_output_test.cpp
@@ -8,33 +8,48 @@
 #define TEST_USE_PETSC
 #define FEAL_OVERRIDE_ASSERTS
 #include <flow_gtest_mpi.hh>
+
+#include "io/equation_output.hh"
 #include "io/output_time.hh"
 #include "io/output_vtk.hh"
+#include "io/output_mesh.hh"
+
 #include "mesh/mesh.h"
 #include "input/reader_to_storage.hh"
+#include "input/input_type.hh"
 #include "system/sys_profiler.hh"
 
 #include <fields/field_fe.hh>
 #include <fields/field_set.hh>
 #include <fields/field_common.hh>
-#include "input/input_type.hh"
 
-#include "io/output_mesh.hh"
+#include "fem/mapping_p1.hh"
+#include "quadrature/quadrature_lib.hh"
+#include "fem/dofhandler.hh"
+#include "fem/fe_values.hh"
+
+#include "fem/fe_rt.hh"
+#include "fem/singularity.hh"
+#include "fem/fe_rt_xfem.hh"
+#include "fem/fe_p0_xfem.hh"
+
+
 
 FLOW123D_FORCE_LINK_IN_PARENT(field_constant)
 
-const string input_rt = R"INPUT(
+const std::string input_rt = R"INPUT(
 {   
    output_stream = {
-    file = "./test_shape.pvd", 
+    file = "test_shape", 
     format = {
         TYPE = "vtk", 
         variant = "ascii"
     },
     output_mesh = {
-        max_level = 6
+        max_level = 7
     }
   }
+  ,output = {fields = ["shape_func"]}
 }
 )INPUT";
 
@@ -55,24 +70,29 @@ $Elements
 $EndElements
 )CODE";
 
-#include "fem/mapping_p1.hh"
-#include "fem/fe_rt.hh"
-#include "fem/dofhandler.hh"
-#include "fem/fe_values.hh"
-#include "quadrature/quadrature_lib.hh"
-#include "fields/field_fe.hh"
 
-#include "fem/singularity.hh"
-#include "fem/fe_rt_xfem.hh"
-#include "fem/fe_p0_xfem.hh"
+bool replace_string(std::string& str, const std::string& from, const std::string& to) {
+    size_t start_pos = str.find(from);
+    if(start_pos == std::string::npos)
+        return false;
+    str.replace(start_pos, from.length(), to);
+    return true;
+}
+
 
 void output_field_fe(FiniteElement<1,3>& fe_1,
                      FiniteElement<2,3>& fe_2,
                      FiniteElement<3,3>& fe_3,
                      const std::map<unsigned int, double>& dof_values,
-                     bool is_scalar)
+                     bool is_scalar,
+                     std::string file_name)
 {
-// read mesh - simplset cube from test1
+    // replace correct output file name in input record string
+    std::string input_json = input_rt;
+    bool res = replace_string(input_json, "test_shape" ,file_name);
+    ASSERT(res);
+    
+    // read mesh
     Mesh* mesh = new Mesh();
     stringstream in(ref_element_mesh.c_str());
     mesh->read_gmsh_from_stream(in);
@@ -118,7 +138,7 @@ void output_field_fe(FiniteElement<1,3>& fe_1,
     output_field->output_type(OutputTime::CORNER_DATA);
     
     // create field set of output fields
-    FieldSet output_fields;
+    EquationOutput output_fields;
     output_fields += output_field->name("shape_func").units(UnitSI::dimensionless()).flags_add(FieldFlag::allow_output);
     
     // set time to all fields
@@ -126,19 +146,25 @@ void output_field_fe(FiniteElement<1,3>& fe_1,
 
 //     FilePath::set_io_dirs(".",UNIT_TESTS_SRC_DIR,"",".");
     FilePath::set_io_dirs(".",UNIT_TESTS_SRC_DIR,"","output");
+    
+    // create input record
+    Input::Type::Record rec_type = Input::Type::Record("ErrorFieldTest","")
+        .declare_key("output_stream", OutputTime::get_input_type(), Input::Type::Default::obligatory(), "")
+        .declare_key("output", output_fields.make_output_type("test_eq"), Input::Type::Default::obligatory(), "")
+        .close();
+
     // read input string
-    Input::Record in_rec = Input::ReaderToStorage( input_rt,
-                                   Input::Type::Record("ErrorFieldTest","")
-                                   .declare_key("output_stream", OutputTime::get_input_type(), Input::Type::Default::obligatory(), "")
-                                   .close(),
-                                   Input::FileFormat::format_JSON ).get_root_interface<Input::Record>();
-                                   
+    Input::ReaderToStorage reader( input_json, rec_type, Input::FileFormat::format_JSON );
+    Input::Record in_rec=reader.get_root_interface<Input::Record>();
+    
+        
     // create output
     std::shared_ptr<OutputTime> output = std::make_shared<OutputVTK>();
-    output->init_from_input("dummy_equation", in_rec.val<Input::Record>("output_stream"));
+    output->init_from_input("dummy_equation", *mesh, in_rec.val<Input::Record>("output_stream"));
+    output_fields.initialize(output, in_rec.val<Input::Record>("output"), TimeGovernor());
     
     // register output fields, compute and write data
-    output_fields.output(output);
+    output_fields.output(0.0);
     output->write_time_frame();
     
     delete output_field;
@@ -146,74 +172,65 @@ void output_field_fe(FiniteElement<1,3>& fe_1,
 }
 
 
-TEST(ShapeFunctionOutput, rt_shape) {
-    
-    FE_RT0<1,3> fe_rt1;
-    FE_RT0<2,3> fe_rt2;
-    FE_RT0<3,3> fe_rt3;
-    
-    std::map<unsigned int, double> dof_values = {
-        { 0, 1.0 }
-//         { 1, 1.0 }
-//         { 2, 1.0 }
-    };
-    
-    output_field_fe(fe_rt1, fe_rt2, fe_rt3, dof_values, false);
-}
-
-
 TEST(ShapeFunctionOutput, rt_xfem_shape) {
     
-    Singularity0D<3> func({0.2,0.2,0},0.05);
+    auto func = std::make_shared<Singularity0D<3>>(arma::vec({0.2,0.2,0}),0.05,
+                                                   arma::vec({0, 0, 1}), arma::vec({0, 0, 1}));
     
     FE_RT0<1,3> fe_rt1;
     FE_RT0<2,3> fe_rt2;
-    FE_RT0_XFEM<2,3> fe_rt_xfem(&fe_rt2,{&func});
+    FE_RT0_XFEM<2,3> fe_rt_xfem(&fe_rt2,{func});
     FE_RT0<3,3> fe_rt3;
     
-//     //precise enrichment function approx.
-//     std::map<unsigned int, double> dof_values = {
-//         { 0, 1.53846153846154 },    // interpolation dofs
-//         { 1, 1.53846153846154 },
-//         { 2, 2.35702260395516 },
-//         { 3, 1.0 },
-//         { 4, 1.0 },
-//         { 5, 1.0 }
-//     };
+   std::map<unsigned int, double> dof_values;
+
+    // print all shape functions
+    std::string filename = "test_rt_";
+    for(unsigned int i=0; i < fe_rt_xfem.n_dofs(); i++){
+        dof_values = {{i, 1.0}};  //select only one shape function
+        output_field_fe(fe_rt1, fe_rt_xfem, fe_rt3, dof_values, true, filename + std::to_string(i));
+    }
     
-    std::map<unsigned int, double> dof_values = {
-//        { 3, 1.0 }
-//        { 4, 1.0 }
-       { 5, 1.0 }
+    //     //precise enrichment function approx.
+    dof_values = {
+        { 0, 1.53846153846154 },    // interpolation dofs
+        { 1, 1.53846153846154 },
+        { 2, 2.35702260395516 },
+        { 3, 1.0 },
+        { 4, 1.0 },
+        { 5, 1.0 }
     };
     
-    output_field_fe(fe_rt1, fe_rt_xfem, fe_rt3, dof_values, false);
+    output_field_fe(fe_rt1, fe_rt_xfem, fe_rt3, dof_values, true, "test_rt");
 }
 
 
 TEST(ShapeFunctionOutput, p0_xfem) {
 
-    Singularity0D<3> func({0.2,0.2,0},0.05);
+    auto func = std::make_shared<Singularity0D<3>>(arma::vec({0.2,0.2,0}),0.05,
+                                                   arma::vec({0, 0, 1}), arma::vec({0, 0, 1}));
     
     FE_P_disc<0,1,3> fe_p_1;
     FE_P_disc<0,2,3> fe_p_2;
-    FE_P0_XFEM<2,3> fe_p0_xfem(&fe_p_2,{&func});
+    FE_P0_XFEM<2,3> fe_p0_xfem(&fe_p_2,{func});
     FE_P_disc<0,3,3> fe_p_3;
 
+    std::map<unsigned int, double> dof_values;
+
+    // print all shape functions
+    std::string filename = "test_p0_";
+    for(unsigned int i=0; i < fe_p0_xfem.n_dofs(); i++){
+        dof_values = {{i, 1.0}};  //select only one shape function
+        output_field_fe(fe_p_1, fe_p0_xfem, fe_p_3, dof_values, true, filename + std::to_string(i));
+    }
+    
 //     //precise enrichment function approx.    
-    std::map<unsigned int, double> dof_values = {
-       { 0, -0.192831240405992 },   // interpolation dof
+    dof_values = {
+       { 0, -0.192831240405992 },   // value of enrich function at interpolation dof point
        { 1, 1.0 },
        { 2, 1.0 },
        { 3, 1.0 }
     };
     
-//         std::map<unsigned int, double> dof_values = {
-// //        { 0, 1.0 }
-// //        { 1, 1.0 }
-// //        { 2, 1.0 }
-//        { 3, 1.0 }
-//     };
-    
-    output_field_fe(fe_p_1, fe_p0_xfem, fe_p_3, dof_values, true);
+    output_field_fe(fe_p_1, fe_p0_xfem, fe_p_3, dof_values, true, "test_p0");
 }
