6f8631760c99adf18122dee098254359206126d9
David Flanderka <t426.001@centrum.cz>
2017-04-21 18:21:02 +0200

    Complete and fix unit test of VTK reader.

--- unit_tests/mesh/vtk_reader_test.cpp
+++ unit_tests/mesh/vtk_reader_test.cpp
@@ -8,6 +8,7 @@
 #define FEAL_OVERRIDE_ASSERTS
 
 #include <flow_gtest.hh>
+#include <mesh_constructor.hh>
 
 #include <string>
 #include <iostream>
@@ -20,13 +21,21 @@ class VtkMeshReaderTest : public testing::Test, public VtkMeshReader {
 protected:
 	virtual void SetUp() {
 	    FilePath::set_io_dirs(".",UNIT_TESTS_SRC_DIR,"",".");
-	    FilePath vtu_file("output/test_output_vtk_binary_ref.vtu", FilePath::input_file);
-
-	    parse_result_ = doc_.load_file( ((std::string)vtu_file).c_str() );
-	    read_base_vtk_attributes();
 	}
 
 	virtual void TearDown() {}
+
+	void read_input_file(const std::string &file_name) {
+	    FilePath vtu_file(file_name, FilePath::input_file);
+	    parse_result_ = doc_.load_file( ((std::string)vtu_file).c_str() );
+	    this->read_base_vtk_attributes();
+		// data of appended tag
+		if (this->header_type_==DataType::undefined) { // no AppendedData tag
+			appended_pos_ = 0;
+		} else {
+			this->set_appended_stream(vtu_file);
+		}
+	}
 
 	std::string get_parse_result() {
 		std::stringstream ss; ss << this->parse_result_.description();
@@ -56,9 +65,10 @@ TEST(PugiXml, read_simple_xml) {
 
 
 // test of reading of VTU file
-TEST_F(VtkMeshReaderTest, read_xml_file) {
+TEST_F(VtkMeshReaderTest, read_ascii_vtu) {
+	read_input_file("output/test_output_vtk_ascii_ref.vtu");
 
-    //EXPECT_EQ( "No error", this->get_parse_result() );
+    EXPECT_EQ( "No error", this->get_parse_result() );
 
     {
     	// test of attributes
@@ -66,19 +76,73 @@ TEST_F(VtkMeshReaderTest, read_xml_file) {
 		EXPECT_EQ( 6, this->n_elements() );
     }
 
+    Mesh *mesh = mesh_constructor();
+
     {
     	// test of Points data section
-    	auto data_attr = this->get_data_array_attr(DataSections::points);
-    	EXPECT_EQ( DataType::float64, data_attr.type_ );
-    	EXPECT_EQ( DataFormat::appended, data_attr.format_ );
-    	EXPECT_EQ( 3, data_attr.n_components_ );
+        this->read_nodes(mesh);
+        EXPECT_EQ(8, mesh->n_nodes());
     }
 
     {
     	// test of connectivity data array
     	auto data_attr = this->get_data_array_attr(DataSections::cells, "connectivity");
     	EXPECT_EQ( DataType::uint32, data_attr.type_ );
-    	EXPECT_EQ( DataFormat::appended, data_attr.format_ );
+    	EXPECT_EQ( DataFormat::ascii, data_format_ );
     	EXPECT_EQ( 1, data_attr.n_components_ );
     }
 }
+
+
+TEST_F(VtkMeshReaderTest, read_binary_vtu) {
+	read_input_file("output/test_output_vtk_binary_ref.vtu");
+
+    {
+    	// test of attributes
+		EXPECT_EQ( 8, this->n_nodes() );
+		EXPECT_EQ( 6, this->n_elements() );
+    }
+
+    Mesh *mesh = mesh_constructor();
+
+    {
+    	// test of Points data section
+        this->read_nodes(mesh);
+        EXPECT_EQ(8, mesh->n_nodes());
+    }
+
+    {
+    	// test of connectivity data array
+    	auto data_attr = this->get_data_array_attr(DataSections::cells, "connectivity");
+    	EXPECT_EQ( DataType::uint32, data_attr.type_ );
+    	EXPECT_EQ( DataFormat::binary_uncompressed, data_format_ );
+    	EXPECT_EQ( 1, data_attr.n_components_ );
+    }
+}
+
+
+/*TEST_F(VtkMeshReaderTest, read_compressed_vtu) {
+	read_input_file("output/test_output_vtk_zlib_ref.vtu");
+
+    {
+    	// test of attributes
+		EXPECT_EQ( 8, this->n_nodes() );
+		EXPECT_EQ( 6, this->n_elements() );
+    }
+
+    Mesh *mesh = mesh_constructor();
+
+    {
+    	// test of Points data section
+        this->read_nodes(mesh);
+        EXPECT_EQ(8, mesh->n_nodes());
+    }
+
+    {
+    	// test of connectivity data array
+    	auto data_attr = this->get_data_array_attr(DataSections::cells, "connectivity");
+    	EXPECT_EQ( DataType::uint32, data_attr.type_ );
+    	EXPECT_EQ( DataFormat::binary_uncompressed, data_format_ );
+    	EXPECT_EQ( 1, data_attr.n_components_ );
+    }
+}*/
