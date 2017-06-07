43941cc668f26b964c1f565387acaeb2c14a5ad6
David Flanderka <t426.001@centrum.cz>
2017-06-07 09:14:39 +0200

    Rename key gmsh_file to mesh_file.

--- src/fields/field_interpolated_p0.cc
+++ src/fields/field_interpolated_p0.cc
@@ -39,7 +39,7 @@ const Input::Type::Record & FieldInterpolatedP0<spacedim, Value>::get_input_type
     return it::Record("FieldInterpolatedP0", FieldAlgorithmBase<spacedim,Value>::template_name()+" Field constant in space.")
         .derive_from(FieldAlgorithmBase<spacedim, Value>::get_input_type())
         .copy_keys(FieldAlgorithmBase<spacedim, Value>::get_field_algo_common_keys())
-        .declare_key("gmsh_file", IT::FileName::input(), IT::Default::obligatory(),
+        .declare_key("mesh_file", IT::FileName::input(), IT::Default::obligatory(),
                 "Input file with ASCII GMSH file format.")
         .declare_key("field_name", IT::String(), IT::Default::obligatory(),
                 "The values of the Field are read from the ```$ElementData``` section with field name given by this key.")
@@ -71,7 +71,7 @@ void FieldInterpolatedP0<spacedim, Value>::init_from_input(const Input::Record &
 	// read mesh, create tree
     {
        source_mesh_ = new Mesh( Input::Record() );
-       reader_file_ = FilePath( rec.val<FilePath>("gmsh_file") );
+       reader_file_ = FilePath( rec.val<FilePath>("mesh_file") );
        GmshMeshReader *reader = static_cast<GmshMeshReader *>( ReaderInstances::instance()->get_reader(reader_file_).get() );
        reader->read_raw_mesh( source_mesh_ );
 	   // no call to mesh->setup_topology, we need only elements, no connectivity
