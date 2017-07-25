a6da8bc0fb3cb33efd93ca80016de1e5153b89ca
David Flanderka <t426.001@centrum.cz>
2017-07-25 17:45:25 +0200

    Add case of native data to output VTK unit tests.

--- unit_tests/output/output_vtk_test.cpp
+++ unit_tests/output/output_vtk_test.cpp
@@ -23,6 +23,11 @@
 #include "system/logger_options.hh"
 #include "fields/field.hh"
 
+#include "fem/mapping_p1.hh"
+#include "fem/dofhandler.hh"
+#include "fem/fe_p.hh"
+#include "fields/field_fe.hh"
+
 FLOW123D_FORCE_LINK_IN_PARENT(field_constant)
 
 const string test_output_time_ascii = R"YAML(
@@ -94,6 +99,36 @@ public:
 		field.compute_field_data(ELEM_DATA, shared_from_this());
 	}
 
+	template <class FieldVal>
+	void set_native_field_data(string field_name, unsigned int size, double step)
+    {
+
+		// make field init it form the init string
+		Field<3, FieldVal> field(field_name, false);
+		field.set_components(component_names);
+		field.set_mesh( *(this->_mesh) );
+		field.units(UnitSI::one());
+
+		std::shared_ptr<DOFHandlerMultiDim> dh = make_shared<DOFHandlerMultiDim>( *(this->_mesh) );
+		FE_P_disc<0,1,3> fe1;
+		FE_P_disc<0,2,3> fe2;
+		FE_P_disc<0,3,3> fe3;
+		dh->distribute_dofs(fe1, fe2, fe3);
+
+		VectorSeqDouble v;
+        v.resize(size);
+        for (unsigned int i=0; i<size; ++i) v[i] = step*i;
+
+		auto native_data_ptr = make_shared< FieldFE<3, FieldVal> >();
+		native_data_ptr->set_fe_data(dh, &map1, &map2, &map3, &v);
+
+		field.set_field(_mesh->region_db().get_region_set("ALL"), native_data_ptr);
+		field.output_type(OutputTime::NATIVE_DATA);
+		field.set_time(TimeGovernor(0.0, 1.0).step(), LimitSide::left);
+
+		field.compute_native_data(NATIVE_DATA, shared_from_this());
+	}
+
 	// check result
 	void check_result_file(std::string result_file, std::string ref_file)
 	{
@@ -127,6 +162,9 @@ public:
 	}
 
 	std::vector<string> component_names;
+	MappingP1<1,3> map1;
+	MappingP1<2,3> map2;
+	MappingP1<3,3> map3;
 };
 
 
@@ -138,6 +176,7 @@ TEST(TestOutputVTK, write_data_ascii) {
 	output_vtk->set_field_data< Field<3,FieldValue<0>::Scalar> > ("scalar_field", "0.5");
 	output_vtk->set_field_data< Field<3,FieldValue<3>::VectorFixed> > ("vector_field", "[0.5, 1.0, 1.5]");
 	output_vtk->set_field_data< Field<3,FieldValue<3>::TensorFixed> > ("tensor_field", "[[1, 2, 3], [4, 5, 6], [7, 8, 9]]");
+	output_vtk->set_native_field_data< FieldValue<0>::Scalar >("flow_data", 6, 0.2);
 	output_vtk->write_data();
     EXPECT_EQ("./test1.pvd", output_vtk->base_filename());
     EXPECT_EQ("test1", output_vtk->main_output_basename());
