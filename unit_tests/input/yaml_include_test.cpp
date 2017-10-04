baa1ede581b4aaa24b39c8c61c16997ca9d756d1
David Flanderka <t426.001@centrum.cz>
2017-10-04 13:18:53 +0200

    Fix unit test.

--- unit_tests/input/yaml_include_test.cpp
+++ unit_tests/input/yaml_include_test.cpp
@@ -26,7 +26,7 @@ protected:
     virtual void TearDown() {
     };
 
-    static Type::Record & get_input_record() {
+    static Type::Record & get_input_include_record() {
     	static Type::Record sub_rec = Type::Record("SubRecord", "Sub record.")
     			.declare_key("a_key", Type::Array( Type::Double() ), Type::Default::obligatory(), "desc.")
     			.declare_key("b_key", Type::Array( Type::Integer() ), Type::Default::obligatory(), "desc.")
@@ -39,11 +39,33 @@ protected:
     			.close();
     };
 
+    static Type::Record & get_input_include_csv_record() {
+    	static Type::Record coords_rec = Type::Record("Coords", "3D coords.")
+    			.declare_key("x", Type::Double(), Type::Default::obligatory(), "desc.")
+    			.declare_key("y", Type::Double(), Type::Default::obligatory(), "desc.")
+    			.declare_key("z", Type::Double(), Type::Default::obligatory(), "desc.")
+    			.close();
+
+    	static Type::Record sub_rec = Type::Record("SubRecord", "Sub record.")
+    			.declare_key("coords", coords_rec, Type::Default::obligatory(), "desc.")
+    			.declare_key("a_key", Type::Integer(), Type::Default::obligatory(), "desc.")
+    			.declare_key("b_key", Type::Double(), Type::Default::obligatory(), "desc.")
+    			.declare_key("c_key", Type::Bool(), Type::Default::obligatory(), "desc.")
+    			.declare_key("d_key", Type::String(), Type::Default::obligatory(), "desc.")
+    			.declare_key("e_key", Type::Double(), Type::Default::obligatory(), "desc.")
+    			.close();
+
+    	return Type::Record("RootCsv", "Root record with CSV include.")
+    			.declare_key("sub_rec", Type::Array(sub_rec), Type::Default::obligatory(), "desc.")
+    			.declare_key("description", Type::String(), Type::Default::obligatory(), "desc.")
+    			.close();
+    };
+
     // overload parent class method in order to reset pointers
     void read_stream(istream &in, Type::TypeBase &root_type, FileFormat format = FileFormat::format_JSON) {
     	this->storage_ = nullptr;
     	this->root_type_ = nullptr;
-    	this->try_transpose_read_ = false;
+    	this->try_read_ = TryRead::none;
     	root_type.finish();
     	ReaderToStorage::read_stream(in, root_type, format);
     };
@@ -104,36 +126,76 @@ const string import_json_to_json = R"JSON(
 
 TEST_F(ReaderIncludeTest, include_yaml_to_yaml) {
     stringstream ss( import_yaml_to_yaml );
-    read_stream(ss, ReaderIncludeTest::get_input_record(), FileFormat::format_YAML);
+    read_stream(ss, ReaderIncludeTest::get_input_include_record(), FileFormat::format_YAML);
     check_simple_storage();
 }
 
 TEST_F(ReaderIncludeTest, include_json_to_yaml) {
     stringstream ss( import_json_to_yaml );
-    read_stream(ss, ReaderIncludeTest::get_input_record(), FileFormat::format_YAML);
+    read_stream(ss, ReaderIncludeTest::get_input_include_record(), FileFormat::format_YAML);
     check_simple_storage();
 }
 
 TEST_F(ReaderIncludeTest, include_yaml_auto_conversion) {
     stringstream ss( import_yaml_auto_conversion );
-    read_stream(ss, ReaderIncludeTest::get_input_record(), FileFormat::format_YAML);
+    read_stream(ss, ReaderIncludeTest::get_input_include_record(), FileFormat::format_YAML);
     check_simple_storage();
 }
 
 TEST_F(ReaderIncludeTest, include_json_auto_conversion) {
     stringstream ss( import_json_auto_conversion );
-    read_stream(ss, ReaderIncludeTest::get_input_record(), FileFormat::format_YAML);
+    read_stream(ss, ReaderIncludeTest::get_input_include_record(), FileFormat::format_YAML);
     check_simple_storage();
 }
 
 TEST_F(ReaderIncludeTest, include_yaml_to_json) {
     stringstream ss( import_yaml_to_json );
-    read_stream(ss, ReaderIncludeTest::get_input_record(), FileFormat::format_JSON);
+    read_stream(ss, ReaderIncludeTest::get_input_include_record(), FileFormat::format_JSON);
     check_simple_storage();
 }
 
 TEST_F(ReaderIncludeTest, include_json_to_json) {
     stringstream ss( import_json_to_json );
-    read_stream(ss, ReaderIncludeTest::get_input_record(), FileFormat::format_JSON);
+    read_stream(ss, ReaderIncludeTest::get_input_include_record(), FileFormat::format_JSON);
     check_simple_storage();
+}
+
+
+const string import_csv_to_yaml = R"YAML(
+sub_rec: !include_csv
+  file: include_simple.csv
+  format:
+    coords:
+      x: 1
+      y: 2
+      z: 3
+    a_key: 0
+    b_key: 4
+    c_key: 6
+    d_key: 7
+    e_key: 5
+description: Example of CSV include
+)YAML";
+
+const string import_csv_to_json = R"JSON(
+{
+  sub_rec = { 
+    TYPE = "include_csv", 
+    file = "include_simple.csv",
+    format = { coords={x=1, y=2, z=3}, a_key=0, b_key=4, c_key=6, d_key=7, e_key=5 }
+  },
+  description = "Example of CSV include"
+}
+)JSON";
+
+TEST_F(ReaderIncludeTest, include_csv_to_yaml) {
+    stringstream ss( import_csv_to_yaml );
+    read_stream(ss, ReaderIncludeTest::get_input_include_csv_record(), FileFormat::format_YAML);
+    //check_simple_storage();
+}
+
+TEST_F(ReaderIncludeTest, include_csv_to_json) {
+    stringstream ss( import_csv_to_json );
+    read_stream(ss, ReaderIncludeTest::get_input_include_csv_record(), FileFormat::format_JSON);
+    //check_simple_storage();
 }
