c22bde2c578e33548e50aa1f220a3518f64e9a48
Pavel Exner <pavelexner@gmail.com>
2017-10-06 16:11:05 +0200

    Fix quote syntax error in previous commit.

--- src/io/output_time.cc
+++ src/io/output_time.cc
@@ -47,7 +47,7 @@ const IT::Record & OutputTime::get_input_type() {
         .declare_key("output_mesh", OutputMeshBase::get_input_type(), IT::Default::optional(),
                 "Output mesh record enables output on a refined mesh [EXPERIMENTAL, VTK only]."
                 "Sofar refinement is performed only in discontinous sense."
-                "Therefore only corner and element data can be written on refined output mesh.
+                "Therefore only corner and element data can be written on refined output mesh."
                 "Node data are to be transformed to corner data, native data cannot be written."
                 "Do not include any node or native data in output fields.")
         .declare_key("precision", IT::Integer(0), IT::Default("5"),
