74239c606b855d71da8c6308d4fa1e8e3b28e645
Pavel Exner <pavelexner@gmail.com>
2017-11-23 13:14:14 +0100

    Fix usage of sparsity pattern in case of no elimination in local system.

--- src/la/local_system.cc
+++ src/la/local_system.cc
@@ -107,62 +107,66 @@ void LocalSystem::set_solution_cols(DofVec & loc_cols, const arma::vec &solution
 
 void LocalSystem::eliminate_solution()
 {
-    if (! n_elim_rows &&  ! n_elim_cols ) return;
-    
-    //DebugOut().fmt("elim rows: {} elim_cols: {}", n_elim_rows, n_elim_cols);
-    
-    arma::mat tmp_mat = matrix;
-    arma::vec tmp_rhs = rhs;
-    
-    unsigned int ic, ir, row, col, j;
-
-    // eliminate columns
-    for(ic=0; ic < n_elim_cols; ic++) {
-        col = elim_cols[ic];
-        tmp_rhs -= solution_cols[ic] * tmp_mat.col( col );
+    //if there is solution set, eliminate:
+    if (n_elim_rows ||  n_elim_cols )
+    {
+        //DebugOut().fmt("elim rows: {} elim_cols: {}", n_elim_rows, n_elim_cols);
         
-        // keep the structure of the matrix by setting almost_zero when eliminating dirichlet BC
-        for(j=0; j < tmp_mat.n_rows; j++)
-            if(tmp_mat.col(col)(j) != 0) tmp_mat.col(col)(j) = almost_zero;
-    }
-
-    // eliminate rows
-    for(ir=0; ir < n_elim_rows; ir++) {
-        row = elim_rows[ir];
-        tmp_rhs( row ) = 0.0;
+        arma::mat tmp_mat = matrix;
+        arma::vec tmp_rhs = rhs;
         
-        // keep the structure of the matrix by setting almost_zero when eliminating dirichlet BC
-        for(j=0; j < tmp_mat.n_cols; j++)
-            if(tmp_mat.row(row)(j) != 0) tmp_mat.row(row)(j) = almost_zero;
+        unsigned int ic, ir, row, col, j;
 
-        // fix global diagonal
+        // eliminate columns
         for(ic=0; ic < n_elim_cols; ic++) {
             col = elim_cols[ic];
-            if (row_dofs[row] == col_dofs[col]) {
-                ASSERT_DBG(fabs(solution_rows[ir] - solution_cols[ic]) <1e-12 );
-                // if preferred value is not set, then try using matrix value
-                double new_diagonal = matrix(row, col);
+            tmp_rhs -= solution_cols[ic] * tmp_mat.col( col );
+            
+            // keep the structure of the matrix by setting almost_zero when eliminating dirichlet BC
+            for(j=0; j < tmp_mat.n_rows; j++)
+                if(tmp_mat.col(col)(j) != 0) tmp_mat.col(col)(j) = almost_zero;
+        }
 
-                if (diag_rows[ir] != 0.0)    // if preferred value is set
-                    new_diagonal = diag_rows[ir];
-                else if(new_diagonal == 0.0)      // if an assembled value is not available
-                    new_diagonal = 1.0;
-                //                     double new_diagonal = fabs(matrix(sol_row,col));
-                //                     if (new_diagonal == 0.0) {
-                //                         if (matrix.is_square()) {
-                //                             new_diagonal = arma::sum( abs(matrix.diag())) / matrix.n_rows;
-                //                         } else {
-                //                             new_diagonal = arma::accu( abs(matrix) ) / matrix.n_elem;
-                //                         }
-                //                     }
-                tmp_mat(row,col) = new_diagonal;
-                tmp_rhs(row) = new_diagonal * solution_rows[ir];
+        // eliminate rows
+        for(ir=0; ir < n_elim_rows; ir++) {
+            row = elim_rows[ir];
+            tmp_rhs( row ) = 0.0;
+            
+            // keep the structure of the matrix by setting almost_zero when eliminating dirichlet BC
+            for(j=0; j < tmp_mat.n_cols; j++)
+                if(tmp_mat.row(row)(j) != 0) tmp_mat.row(row)(j) = almost_zero;
 
+            // fix global diagonal
+            for(ic=0; ic < n_elim_cols; ic++) {
+                col = elim_cols[ic];
+                if (row_dofs[row] == col_dofs[col]) {
+                    ASSERT_DBG(fabs(solution_rows[ir] - solution_cols[ic]) <1e-12 );
+                    // if preferred value is not set, then try using matrix value
+                    double new_diagonal = matrix(row, col);
+
+                    if (diag_rows[ir] != 0.0)    // if preferred value is set
+                        new_diagonal = diag_rows[ir];
+                    else if(new_diagonal == 0.0)      // if an assembled value is not available
+                        new_diagonal = 1.0;
+                    //                     double new_diagonal = fabs(matrix(sol_row,col));
+                    //                     if (new_diagonal == 0.0) {
+                    //                         if (matrix.is_square()) {
+                    //                             new_diagonal = arma::sum( abs(matrix.diag())) / matrix.n_rows;
+                    //                         } else {
+                    //                             new_diagonal = arma::accu( abs(matrix) ) / matrix.n_elem;
+                    //                         }
+                    //                     }
+                    tmp_mat(row,col) = new_diagonal;
+                    tmp_rhs(row) = new_diagonal * solution_rows[ir];
+
+                }
             }
         }
-    }
 
-    matrix = tmp_mat;
+        matrix = tmp_mat;
+        rhs = tmp_rhs;
+        n_elim_cols=n_elim_rows=0;
+    }
     
     // filling almost_zero according to sparsity pattern
     for(unsigned int i=0; i < matrix.n_rows; i++)
@@ -177,10 +181,7 @@ void LocalSystem::eliminate_solution()
             else
                 matrix(i,j) = matrix(i,j) + almost_zero * sparsity_pattern_.get_matrix()(i,j);
         }
-        
-    rhs = tmp_rhs;
-    n_elim_cols=n_elim_rows=0;
-
+    
     //DebugOut() << matrix;
     //DebugOut() << rhs;
 
