c9b2337b4f9cf4be785011cccbc14f8632ad7468
Jan Brezina <jan.brezina@tul.cz>
2017-12-31 15:57:02 +0100

    Remove depracaterd files and tests_old

--- bin/runtest.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Simple script which will use correct python version and execute runtest.py
-
-# populated by configure_file call
-/usr/bin/python2.7 /home/jb/workspace/flow123d/bin/python/runtest.py $@
