4a6156198feadfcc21c2804988003eba4182c292
Petr Král <petr.kral@tul.cz>
2018-01-04 10:02:07 +0100

    with <algorithm>

--- src/system/armor.hh
+++ src/system/armor.hh
@@ -1,6 +1,7 @@
-//#define ARMA_DONT_USE_WRAPPER
-//#define ARMA_NO_DEBUG
+#define ARMA_DONT_USE_WRAPPER
+#define ARMA_NO_DEBUG
 #include <armadillo>
+#include <algorithm>
 
 namespace Armor {
 
@@ -12,33 +13,36 @@ private:
     typedef typename arma::Mat<Type>::template fixed<nRows,nCols> ArmaType;
 public:
     Mat() {
-        uint size{nCols * nRows};
-        for (uint i{0}; i < size; ++i) {
-            *(*data + i) = 0;
-        }
+        std::fill(begin(), end(), 0);
     }
     Mat(std::initializer_list<std::initializer_list<Type>> list) {
-        uint i{(uint) -1};
-        uint j{0};
+        uint i{0}, j{0};
         for (auto & row : list) {
             for (auto & item : row) {
-                data[++i][j] = item;
+                data[i][j] = item;
+                ++i;
             }
-            i = (uint) -1;
+            i = 0;
             ++j;
         }
     }
     Mat(std::initializer_list<Type> list) {
-        uint i{(uint) -1};
-        for (auto & item : list) {
-            *(*data + ++i) = item;
-        }
+        std::copy(list.begin(), list.end(), begin());
+    }
+    inline const Type * begin() const {
+        return *data;
+    }
+    inline Type * begin() {
+        return *data;
+    }
+    inline const Type * end() const {
+        return *data + nCols * nRows;
+    }
+    inline Type * end() {
+        return *data + nCols * nRows;
     }
     inline Mat(const ArmaType & a) {
-        uint size{nCols * nRows};
-        for (uint i{0}; i < size; ++i) {
-            *(*data + i) = a[i];
-        }
+        std::copy(a.begin(), a.end(), begin());
     }
     inline const Type operator [](uint index) const {
         return *(*data + index);
@@ -53,42 +57,30 @@ public:
         return data[col][row];
     }
     inline ArmaType arma() const {
-        return ArmaType(data[0]);
-    }
-    inline operator ArmaType() const {
-        return this->arma();
+        return ArmaType(begin());
     }
     inline const Mat<Type, nRows, nCols> & operator=(const Mat<Type, nRows, nCols> & other) {
-        uint size{nCols * nRows};
-        for (uint i{0}; i < size; ++i) {
-            *(*data + i) = other[i];
-        }
+        std::copy(other.begin(), other.end(), begin());
         return *this;
     }
     inline const Mat<Type, nRows, nCols> & operator=(const ArmaType & other) {
-        uint size{nCols * nRows};
-        for (uint i{0}; i < size; ++i) {
-            *(*data + i) = other[i];
-        }
+        std::copy(other.begin(), other.end(), begin());
         return *this;
     }
     inline const Mat<Type, nRows, nCols> & operator=(std::initializer_list<std::initializer_list<Type>> list) {
-        uint i{(uint) -1};
-        uint j{0};
+        uint i{0}, j{0};
         for (auto & row : list) {
             for (auto & item : row) {
-                data[++i][j] = item;
+                data[i][j] = item;
+                ++i;
             }
-            i = (uint) -1;
+            i = 0;
             ++j;
         }
         return *this;
     }
     inline const Mat<Type, nRows, nCols> & operator=(std::initializer_list<Type> list) {
-        uint i{(uint) -1};
-        for (auto & item : list) {
-            *(*data + ++i) = item;
-        }
+        std::copy(list.begin(), list.end(), begin());
         return *this;
     }
     inline bool operator==(const ArmaType & other) {
@@ -101,24 +93,18 @@ public:
         return true;
     }
     inline bool operator==(const Mat<Type, nRows, nCols> & other) {
-        uint size = nRows * nCols;
-        for (uint i{0}; i < size; ++i) {
-            if (*(*data + i) != other[i]) {
-                return false;
-            }
-        }
-        return true;
+        return std::equal(begin(), end(), other.begin());
     }
-    uint size() const {
+    inline uint size() const {
         return nRows * nCols;
     }
-    double * memptr() {
-        return *data;
+    inline Type * memptr() {
+        return begin();
     }
 };
 
 template <class Type, uint nRows, uint nCols>
-inline double dot(const Mat<Type, nRows, nCols> & a, const Mat<Type, nRows, nCols> & b) {
+inline Type dot(const Mat<Type, nRows, nCols> & a, const Mat<Type, nRows, nCols> & b) {
     return arma::dot(a.arma(), b.arma());
 }
 
@@ -152,6 +138,16 @@ template <class Type, uint nRows, uint nCols>
 inline typename arma::Mat<Type>::template fixed<nRows,nCols> operator/(const Mat<Type, nRows, nCols> & a, Type number) {
     return a.arma() / number;
 }
+
+template <uint N>
+using Vec = Mat<double, N, 1>;
+
+/*
+template <uint N, uint M>
+class Mat : Mat<double, N, M> {
+
+};
+*/
 
 }
 
