040cf195181795440bc48673c052bb01f4542be7
Jan Hybš <jan.hybs@tul.cz>
2018-05-11 14:59:52 +0200

    Edit config.cmake for debug and release

--- config/config-jenkins-docker-release.cmake
+++ config/config-jenkins-docker-release.cmake
@@ -7,12 +7,10 @@ set(CMAKE_VERBOSE_MAKEFILE on)
 
 
 # external libraries
-set(PETSC_DIR           "/usr/lib/petsc/")
-set(PETSC_ARCH          "linux-Release")
-
-set(BDDCML_ROOT         "/usr/lib/bddcml_blopex/bddcml/Release")
-set(YamlCpp_ROOT_HINT   "/usr/lib/yamlcpp/Release")
-set(Armadillo_ROOT_HINT "/usr/lib/armadillo/Release")
+set(PETSC_DIR              /usr/local/petsc-3.8.3/)
+set(BDDCML_ROOT            /usr/local/bddcml-2.5.0/bddcml)
+set(Armadillo_ROOT_HINT    /usr/local/armadillo-8.3.4)
+set(YamlCpp_ROOT_HINT      /usr/local/yamlcpp-0.5.2)
 
 
 # additional info
