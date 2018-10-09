0deed752d70d615aa3c3f75a1d00cd788de1d278
David Flanderka <t426.001@centrum.cz>
2018-10-09 14:19:11 +0200

    Add unit test of VectorMPI class.

--- unit_tests/fields/vec_seq_double_test.cpp
+++ unit_tests/fields/vec_seq_double_test.cpp
@@ -26,3 +26,25 @@ TEST(VecSeqDouble, vec_data) {
 	EXPECT_DOUBLE_EQ(2.5, vals[0]);
 	EXPECT_DOUBLE_EQ(0.0, vals[1]);
 }
+
+
+TEST(VecMPI, vec_data) {
+	PetscInitialize(0, PETSC_NULL, PETSC_NULL, PETSC_NULL);
+
+	unsigned int data_size = 5;
+	VectorMPI v(data_size, PETSC_COMM_SELF);
+	typename VectorMPI::VectorDataPtr data_ptr = v.data_ptr();
+
+	EXPECT_EQ(data_size, data_ptr->size());
+	EXPECT_DOUBLE_EQ(0.0, v[0]);
+	v[0] = 2.5;
+	EXPECT_DOUBLE_EQ(2.5, v[0]);
+	EXPECT_DOUBLE_EQ((*data_ptr)[0], v[0]);
+
+	Vec petscVec = v.petsc_vec();
+	unsigned int indices[5] = { 0,1,2,3,4 };
+	double vals[5];
+	VecGetValues(petscVec, data_size, (PetscInt *)indices, vals);
+	EXPECT_DOUBLE_EQ(2.5, vals[0]);
+	EXPECT_DOUBLE_EQ(0.0, vals[1]);
+}
