2fadc5d6fdef24d692ee2a521c900683552ca414
David Flanderka <t426.001@centrum.cz>
2018-11-22 15:14:49 +0100

    Use new methods of OutputMesh in EquationOutput.

--- src/fields/equation_output.cc
+++ src/fields/equation_output.cc
@@ -255,9 +255,19 @@ void EquationOutput::make_output_mesh(bool parallel)
 	} else {
 		output_mesh_ = std::make_shared<OutputMesh>(*mesh_);
 	}
-	if ( stream_->n_proc() > 1) output_mesh_->create_sub_mesh();
-	else output_mesh_->create_mesh(); // serial case
-	stream_->set_output_data_caches(output_mesh_);
+	if ( stream_->n_proc() == 1) { // serial case
+		output_mesh_->create_mesh();
+		stream_->set_output_data_caches(output_mesh_);
+	}
+	else {
+		if (parallel) {
+			output_mesh_->create_full_sub_mesh();
+			stream_->set_output_data_caches(output_mesh_);
+		} else {
+			output_mesh_->create_sub_mesh();
+			stream_->set_output_data_caches(output_mesh_->make_serial_master_mesh(stream_->rank(), stream_->n_proc()));
+		}
+	}
 }
 
 
