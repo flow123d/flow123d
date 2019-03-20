9ff87df026d6a395ddb696e64c54bfce64ca2545
David Flanderka <t426.001@centrum.cz>
2019-03-20 09:40:10 +0100

    Separate method calculated native data.
    - add new method for elementwise fields
    - make different dofhandler

--- src/fields/field_fe.cc
+++ src/fields/field_fe.cc
@@ -349,15 +349,16 @@ void FieldFE<spacedim, Value>::make_dof_handler(const Mesh *mesh) {
 			ASSERT(false).error("Should not happen!\n");
 	}
 
-	DOFHandlerMultiDim dh_par( const_cast<Mesh &>(*mesh) );
+	std::shared_ptr<DOFHandlerMultiDim> dh_par = std::make_shared<DOFHandlerMultiDim>( const_cast<Mesh &>(*mesh) );
     std::shared_ptr<DiscreteSpace> ds = std::make_shared<EqualOrderDiscreteSpace>( &const_cast<Mesh &>(*mesh), fe0_, fe1_, fe2_, fe3_);
-	dh_par.distribute_dofs(ds);
-    dh_ = dh_par.sequential();
+	dh_par->distribute_dofs(ds);
+	if (this->discretization_ == OutputTime::DiscreteSpace::NATIVE_DATA) dh_ = dh_par;
+	else dh_ = dh_par->sequential();
     unsigned int ndofs = dh_->max_elem_dofs();
     dof_indices_.resize(ndofs);
 
 	if (this->boundary_domain_) fill_boundary_dofs(); // temporary solution for boundary mesh
-	else data_vec_ = VectorMPI::sequential( dh_->n_global_dofs() ); // allocate data_vec_
+	else data_vec_ = VectorMPI::sequential( dh_->lsize() ); // allocate data_vec_
 
 	// initialization data of value handlers
 	FEValueInitData init_data;
@@ -412,8 +413,10 @@ bool FieldFE<spacedim, Value>::set_time(const TimeStep &time) {
 	        THROW( ExcUndefElementValue() << EI_Field(field_name_) );
 	    }
 
-		if (is_native || this->interpolation_==DataInterpolation::identic_msh || this->interpolation_==DataInterpolation::equivalent_msh) {
+		if (is_native) {
 			this->calculate_native_values(input_data_cache);
+		} else if (this->interpolation_==DataInterpolation::identic_msh || this->interpolation_==DataInterpolation::equivalent_msh) {
+			this->calculate_elementwise_values(input_data_cache);
 		} else if (this->interpolation_==DataInterpolation::gauss_p0) {
 			this->interpolate_gauss(input_data_cache);
 		} else { // DataInterpolation::interp_p0
@@ -633,6 +636,46 @@ void FieldFE<spacedim, Value>::interpolate_intersection(ElementDataCache<double>
 
 template <int spacedim, class Value>
 void FieldFE<spacedim, Value>::calculate_native_values(ElementDataCache<double>::ComponentDataPtr data_cache)
+{
+	// Same algorithm as in output of Node_data. Possibly code reuse.
+	unsigned int dof_size, data_vec_i;
+	std::vector<unsigned int> count_vector(data_vec_.size(), 0);
+	data_vec_.zero_entries();
+	VectorMPI::VectorDataPtr data_vector = data_vec_.data_ptr();
+
+	// iterate through elements, assembly global vector and count number of writes
+	/*Mesh *mesh;
+	if (this->boundary_domain_) mesh = dh_->mesh()->get_bc_mesh();
+	else mesh = dh_->mesh();
+	for (auto ele : mesh->elements_range()) { // remove special case for rank == 0 - necessary for correct output
+		if (this->boundary_domain_) dof_size = value_handler1_.get_dof_indices( ele, dof_indices_ );
+		else dof_size = dh_->cell_accessor_from_element(ele.idx()).get_loc_dof_indices( dof_indices_ );
+		data_vec_i = ele.idx() * dof_indices_.size();
+		for (unsigned int i=0; i<dof_size; ++i, ++data_vec_i) {
+			(*data_vector)[ dof_indices_[i] ] += (*data_cache)[data_vec_i];
+			++count_vector[ dof_indices_[i] ];
+		}
+	}*/
+
+	// iterate through cells, assembly global vector and count number of writes - prepared solution for further development
+	for (auto cell : dh_->own_range()) {
+		dof_size = cell.get_loc_dof_indices(dof_indices_);
+		data_vec_i = cell.elm_idx() * dof_indices_.size();
+		for (unsigned int i=0; i<dof_size; ++i, ++data_vec_i) {
+			(*data_vector)[ dof_indices_[i] ] += (*data_cache)[data_vec_i];
+			++count_vector[ dof_indices_[i] ];
+		}
+	}
+
+	// compute averages of values
+	for (unsigned int i=0; i<data_vec_.size(); ++i) {
+		if (count_vector[i]>0) (*data_vector)[i] /= count_vector[i];
+	}
+}
+
+
+template <int spacedim, class Value>
+void FieldFE<spacedim, Value>::calculate_elementwise_values(ElementDataCache<double>::ComponentDataPtr data_cache)
 {
 	// Same algorithm as in output of Node_data. Possibly code reuse.
 	unsigned int dof_size, data_vec_i;
