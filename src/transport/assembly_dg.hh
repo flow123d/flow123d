b5f1aec81c82d42e5ebb9b9cd1edfa3286b43ee1
David Flanderka <t426.001@centrum.cz>
2019-06-20 12:31:53 +0200

    Proposal of AssemblyDG class.
    - object templated by dimension
    - holds finite elements and relative objects (moved from FEObjects)

---
+++ src/transport/assembly_dg.hh
@@ -0,0 +1,64 @@
+/*!
+ *
+﻿ * Copyright (C) 2015 Technical University of Liberec.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License version 3 as published by the
+ * Free Software Foundation. (http://www.gnu.org/licenses/gpl-3.0.en.html)
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ *
+ * @file    assembly_dg.hh
+ * @brief
+ */
+
+#ifndef ASSEMBLY_DG_HH_
+#define ASSEMBLY_DG_HH_
+
+#include "fields/fe_value_handler.hh"
+#include "fem/mapping_p1.hh"
+#include "fem/fe_p.hh"
+#include "fem/fe_rt.hh"
+#include "quadrature/quadrature_lib.hh"
+
+
+/**
+ * Auxiliary container class for Finite element and related objects of given dimension.
+ */
+template <unsigned int dim>
+class AssemblyDG
+{
+public:
+
+    /// Constructor.
+    AssemblyDG(unsigned int fe_order) {
+    	fe_ = new FE_P_disc<dim>(fe_order);
+    	fe_rt_ = new FE_RT0<dim>;
+    	quad_ = new QGauss<dim>(2*fe_order);
+    	mapping_ = new MappingP1<dim,3>;
+    }
+
+    /// Destructor.
+    ~AssemblyDG() {
+        delete fe_;
+        delete fe_rt_;
+        delete quad_;
+        delete mapping_;
+    }
+
+private:
+
+	FiniteElement<dim> *fe_;     ///< Finite element for the solution of the advection-diffusion equation.
+	FiniteElement<dim> *fe_rt_;  ///< Finite element for the water velocity field.
+	Quadrature<dim> *quad_;      ///< Quadrature used in assembling methods.
+	MappingP1<dim,3> *mapping_;  ///< Auxiliary mapping of reference elements.
+
+	friend class FEObjects;
+};
+
+
+
+#endif /* FE_VALUE_HANDLER_HH_ */
