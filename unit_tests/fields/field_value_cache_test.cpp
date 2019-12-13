a69a0d105b30eb77df782301a48ac9352c4bf03b
David Flanderka <t426.001@centrum.cz>
2019-12-13 15:20:34 +0100

    Unit test of global coords.

--- unit_tests/fields/field_value_cache_test.cpp
+++ unit_tests/fields/field_value_cache_test.cpp
@@ -15,13 +15,16 @@
 #include "fields/eval_points.hh"
 #include "fields/eval_subset.hh"
 #include "fields/field_value_cache.hh"
+#include "fields/field_value_cache.impl.hh"
 #include "fields/field_values.hh"
 #include "quadrature/quadrature.hh"
 #include "quadrature/quadrature_lib.hh"
 #include "fem/dofhandler.hh"
 #include "fem/dh_cell_accessor.hh"
+#include "fem/mapping_p1.hh"
 #include "mesh/mesh.h"
 #include "system/sys_profiler.hh"
+#include "arma_expect.hh"
 
 
 class FieldValueCacheTest : public testing::Test {
@@ -109,4 +112,42 @@ TEST_F(FieldValueCacheTest, element_cache_map) {
     cache_map.clear_elements_to_update();
     dh_cell1 = cache_map(dh_cell1);
     EXPECT_EQ(dh_cell1.element_cache_index(), 0);
+}
+
+TEST_F(FieldValueCacheTest, global_coords) {
+    std::shared_ptr<EvalPoints> eval_points = std::make_shared<EvalPoints>();
+    Quadrature *q_bulk = new QGauss(3, 1);
+    Quadrature *q_side = new QGauss(2, 1);
+    std::shared_ptr<EvalSubset> bulk_eval = eval_points->add_bulk<3>(*q_bulk );
+    std::shared_ptr<EvalSubset> side_eval = eval_points->add_side<3>(*q_side );
+
+    ElementCacheMap cache_map;
+    cache_map.init(3);
+    const ElementCacheMap::UpdateCacheHelper &update_cache_data = cache_map.update_cache_data();
+
+    { // add cells to cache
+        DHCellAccessor dh_cell1(dh_.get(), 1);
+        DHCellAccessor dh_cell2(dh_.get(), 2);
+        DHCellAccessor dh_cell3(dh_.get(), 3);
+        DHCellAccessor dh_cell6(dh_.get(), 6);
+        cache_map.add(dh_cell1);
+        cache_map.add(dh_cell2);
+        cache_map.add(dh_cell3);
+        cache_map.add(dh_cell6);
+    }
+
+    cache_map.prepare_elements_to_update(mesh_);
+    cache_map.clear_elements_to_update();
+
+    MappingP1<3,3> mapping;
+    cache_map.compute_global_coords<3>(eval_points, mapping, mesh_);
+
+    DHCellAccessor dh_cell(dh_.get(), 2);
+    BulkPoint bulk_point( dh_cell, bulk_eval, eval_points->subset_begin(bulk_eval->get_subset_idx()) );
+    EXPECT_ARMA_EQ( cache_map.global_coords(dh_cell.elm_idx(), bulk_point.eval_point_idx()), dh_cell.elm().centre() );
+    for (DHCellSide side : dh_cell.side_range()) {
+    	auto range_it = side_eval->points(side).begin();
+        EXPECT_ARMA_EQ( cache_map.global_coords(side.cell().elm_idx(), range_it->eval_point_idx()), side.side().centre() );
+    }
+
 }
