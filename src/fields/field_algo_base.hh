213ddd282bba48ac2e6f55107939edc15269818b
David Flanderka <t426.001@centrum.cz>
2020-02-07 14:01:44 +0100

    Switch off assert.
    - needs implementation of method in all FieldAlgo descendants

--- src/fields/field_algo_base.hh
+++ src/fields/field_algo_base.hh
@@ -238,7 +238,7 @@ public:
        virtual void cache_update(FieldValueCache<typename Value::element_type, typename Value::return_type> &data_cache,
                    unsigned int i_cache_el_begin, unsigned int i_cache_el_end,
    	               const std::vector< ElementAccessor<spacedim> > &element_set) {
-    	   ASSERT(false).error("Must be implemented in descendants!\n");
+    	   //ASSERT(false).error("Must be implemented in descendants!\n");
        }
 
        /**
