dc4b9da30862543fe67c9d3ea7999a2681d38496
David Flanderka <t426.001@centrum.cz>
2020-03-27 16:01:25 +0100

    Finish field model unit test.
    - check results of cache_update method

--- unit_tests/fields/field_model_test.cpp
+++ unit_tests/fields/field_model_test.cpp
@@ -20,6 +20,8 @@
 
 #include "fields/field_model.hh"
 #include "mesh/accessors.hh"
+#include "quadrature/quadrature.hh"
+#include "quadrature/quadrature_lib.hh"
 
 
 typedef Field<3, FieldValue<3>::Scalar > ScalarField;
@@ -39,7 +41,7 @@ public:
     > DepFields;
 
     Vector operator() (Scalar a, Vector v) {
-        return a * v;
+        return a(0) * v;
     }
 };
 
@@ -54,23 +56,83 @@ public:
     > DepFields;
 
     Vector operator() (Vector a, Scalar c, Vector b) {
-        return a + c * b;
+        return a + c(0) * b;
     }
 };
 
 
 // Test of FieldModel - used objects and functionalities in field_model.hh.
 TEST(FieldModelTest, own_model) {
-	ScalarField f_scal;
-	VectorField f_vec;
-	FieldValueCache<typename FieldValue<3>::VectorFixed::element_type> fvc(FieldValue<3>::VectorFixed::NRows_, FieldValue<3>::VectorFixed::NCols_);
-	std::vector< ElementAccessor<3> > element_set;
+    unsigned int n_items = 10; // number of tested items
 
-	auto f_product = Model<3, FieldValue<3>::VectorFixed>::create(FnProduct(), f_scal, f_vec);
-	f_product.cache_update(fvc, 0, fvc.size(), element_set);
+    ScalarField f_scal;
+    VectorField f_vec;
+    FieldValueCache<typename FieldValue<3>::VectorFixed::element_type> fvc(FieldValue<3>::VectorFixed::NRows_, FieldValue<3>::VectorFixed::NCols_);
+    std::vector< ElementAccessor<3> > element_set;
 
-    auto f_other = Model<3, FieldValue<3>::VectorFixed>::create(FnOther(), f_vec, f_scal, f_vec);
-    f_other.cache_update(fvc, 0, fvc.size(), element_set);
+    // initialize field caches
+    std::shared_ptr<EvalPoints> eval_points = std::make_shared<EvalPoints>();
+    Quadrature *q_bulk = new QGauss(3, 2);
+    Quadrature *q_side = new QGauss(2, 2);
+    eval_points->add_bulk<3>(*q_bulk );
+    eval_points->add_edge<3>(*q_side );
+    fvc.init(eval_points, ElementCacheMap::n_cached_elements);
+    f_scal.cache_allocate(eval_points);
+    f_vec.cache_allocate(eval_points);
+
+    // fill field caches
+    arma::mat::fixed<1,1> scalar_val;
+    arma::mat::fixed<3,1> vector_val;
+    for (unsigned int i=0; i<n_items; ++i) {
+        scalar_val(0,0) = 1.0 + i*0.5;
+        vector_val(0,0) = 1.5 + 2*i;
+        vector_val(1,0) = i + 0.1;
+        vector_val(2,0) = 0.5 + i%2;
+        f_scal.value_cache().data().set(i) = scalar_val;
+        f_vec.value_cache().data().set(i) = vector_val;
+    }
+
+    {
+        // FieldModel scalar * vector
+        std::vector<arma::vec3> expected_vals = {{  1.50,  0.10, 0.50},
+                                                 {  5.25,  1.65, 2.25},
+                                                 { 11.00,  4.20, 1.00},
+                                                 { 18.75,  7.75, 3.75},
+                                                 { 28.50, 12.30, 1.50},
+                                                 { 40.25, 17.85, 5.25},
+                                                 { 54.00, 24.40, 2.00},
+                                                 { 69.75, 31.95, 6.75},
+                                                 { 87.50, 40.50, 2.50},
+                                                 {107.25, 50.05, 8.25}};
+
+    	auto f_product = Model<3, FieldValue<3>::VectorFixed>::create(FnProduct(), f_scal, f_vec);
+        f_product.cache_update(fvc, 0, fvc.size(), element_set);
+        for (unsigned int i=0; i<n_items; ++i) {
+            auto val = fvc.data().template mat<3, 1>(i);
+            EXPECT_ARMA_EQ(val, expected_vals[i]);
+        }
+    }
+
+    {
+        // FieldModel vector + scalar * vector
+        std::vector<arma::vec3> expected_vals = {{  3.00,  0.20, 1.00},
+                                                 {  8.75,  2.75, 3.75},
+                                                 { 16.50,  6.30, 1.50},
+                                                 { 26.25, 10.85, 5.25},
+                                                 { 38.00, 16.40, 2.00},
+                                                 { 51.75, 22.95, 6.75},
+                                                 { 67.50, 30.50, 2.50},
+                                                 { 85.25, 39.05, 8.25},
+                                                 {105.00, 48.60, 3.00},
+                                                 {126.75, 59.15, 9.75}};
+
+        auto f_other = Model<3, FieldValue<3>::VectorFixed>::create(FnOther(), f_vec, f_scal, f_vec);
+        f_other.cache_update(fvc, 0, fvc.size(), element_set);
+        for (unsigned int i=0; i<n_items; ++i) {
+            auto val = fvc.data().template mat<3, 1>(i);
+            EXPECT_ARMA_EQ(val, expected_vals[i]);
+        }
+    }
 
 }
 
