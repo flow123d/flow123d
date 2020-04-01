edcb904529a9badf35dccb65cfd0156dabdd0657
David Flanderka <t426.001@centrum.cz>
2020-04-01 13:47:06 +0200

    Complete asserts.

--- src/fields/field_value_cache.impl.hh
+++ src/fields/field_value_cache.impl.hh
@@ -27,8 +27,11 @@ template<class elm_type>
 template<class Value>
 typename Value::return_type FieldValueCache<elm_type>::get_value(const ElementCacheMap &map,
         const DHCellAccessor &dh_cell, unsigned int eval_points_idx) {
+    static_assert( std::is_same<elm_type, typename Value::element_type>::value, "Wrong element type.");
 
     ASSERT(dh_cell.element_cache_index() != ElementCacheMap::undef_elem_idx)(dh_cell.elm_idx());
+    ASSERT_EQ_DBG(Value::NRows_, data_.n_rows());
+    ASSERT_EQ_DBG(Value::NCols_, data_.n_cols());
     int value_cache_idx = map.get_field_value_cache_index(dh_cell.element_cache_index(), eval_points_idx);
     ASSERT_GE(value_cache_idx, 0);
     return Value::get_from_array(data_, value_cache_idx);
