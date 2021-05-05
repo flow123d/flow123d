c8cffff93cb93f65d2ebd389f98050dfb94222ab
David Flanderka <t426.001@centrum.cz>
2021-05-05 15:09:22 +0200

    Improve readability of code.

--- src/fields/eval_subset.cc
+++ src/fields/eval_subset.cc
@@ -111,8 +111,9 @@ EdgePoint EdgePoint::point_on(DHCellSide edg_side) const {
  */
 
 BulkPoint CouplingPoint::lower_dim(DHCellAccessor cell_lower) const {
-	PatchCacheLoc c_pos(elm_cache_map_->position_in_cache(cell_lower.elm().mesh_idx()),
-	        this->eval_points()->subset_begin(cell_lower.dim(), integral_->get_subset_low_idx())+local_point_idx_);
+    unsigned int i_elm = elm_cache_map_->position_in_cache(cell_lower.elm().mesh_idx());
+    unsigned int i_ep = this->eval_points()->subset_begin(cell_lower.dim(), integral_->get_subset_low_idx()) + local_point_idx_;
+    PatchCacheLoc c_pos(i_elm, i_ep);
     return BulkPoint(elm_cache_map_, c_pos);
 }
 
@@ -122,7 +123,8 @@ BulkPoint CouplingPoint::lower_dim(DHCellAccessor cell_lower) const {
  */
 
 BulkPoint BoundaryPoint::point_bdr(ElementAccessor<3> bdr_elm) const {
-	PatchCacheLoc c_pos(elm_cache_map_->position_in_cache(bdr_elm.mesh_idx()),
-			this->eval_points()->subset_begin(bdr_elm.dim(), integral_->get_subset_low_idx())+local_point_idx_);
+    unsigned int i_elm = elm_cache_map_->position_in_cache(bdr_elm.mesh_idx());
+    unsigned int i_ep = this->eval_points()->subset_begin(bdr_elm.dim(), integral_->get_subset_low_idx()) + local_point_idx_;
+    PatchCacheLoc c_pos(i_elm, i_ep);
     return BulkPoint(elm_cache_map_, c_pos);
 }
