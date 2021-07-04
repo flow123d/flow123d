5ab17ea7f2908bd6fb7993f3b43f7e6a0ecdd076
Jan Brezina <jan.brezina@tul.cz>
2021-07-04 23:00:44 +0200

    Default cmake-config for 3.1.1 image

--- config.default.cmake
+++ config.default.cmake
@@ -3,7 +3,7 @@
 set(FLOW_BUILD_TYPE AUTO)
 
 set(USE_PYTHON          "yes")
-set(PETSC_DIR              /usr/local/petsc-3.8.3/)
-set(BDDCML_ROOT            /usr/local/bddcml-2.5.0/bddcml)
-set(Armadillo_ROOT_HINT    /usr/local/armadillo-8.3.4)
-set(YamlCpp_ROOT_HINT      /usr/local/yamlcpp-0.5.2)
+set(PETSC_DIR              /usr/local/petsc-3.15.1/)
+set(BDDCML_ROOT            /usr/local/bddcml-2.6/bddcml)
+set(Armadillo_ROOT_HINT    /usr/local/armadillo-10.5.2)
+set(YamlCpp_ROOT_HINT      /usr/local/yamlcpp-0.6.3)
