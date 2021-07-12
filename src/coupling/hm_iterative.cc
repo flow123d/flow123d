771e8c9487c221aecf8432f7bcab0fd6304ecf55
jstebel <jan.stebel@tul.cz>
2021-07-12 14:47:29 +0200

    Fix scaling of relaxation parameter in HM_Iterative.

--- src/coupling/hm_iterative.cc
+++ src/coupling/hm_iterative.cc
@@ -277,9 +277,11 @@ void HM_Iterative::update_flow_fields()
         auto elm = ele.elm();
         
         double alpha = data_.alpha.value(elm.centre(), elm);
+        double density = data_.density.value(elm.centre(), elm);
+        double gravity = data_.gravity.value(elm.centre(), elm);
         double young = mechanics_->data().young_modulus.value(elm.centre(), elm);
         double poisson = mechanics_->data().poisson_ratio.value(elm.centre(), elm);
-        double beta = beta_ * 0.5*alpha*alpha/(2*lame_mu(young, poisson)/elm.dim() + lame_lambda(young, poisson));
+        double beta = beta_ * gravity*density*0.5*alpha*alpha/(2*lame_mu(young, poisson)/elm.dim() + lame_lambda(young, poisson));
         
         double old_p = data_.old_pressure_ptr_->value(elm.centre(), elm);
         double p = field_ele_pressure.value(elm.centre(), elm);
