d77059308a86be0083af63287074bb519d720cdc
jstebel <jan.stebel@tul.cz>
2021-07-29 14:18:52 +0200

    Reduce tests in 50_mechanics.

--- tests/50_mechanics/02_pv_filter.py
+++ tests/50_mechanics/02_pv_filter.py
@@ -1,5 +1,4 @@
 import numpy as np
-import math
 
 # TERZAGHI'S 1D COLUMN TEST CASE
 # DATA:
@@ -7,7 +6,6 @@ import math
 # https://www.researchgate.net/publication/222660034_A_fully_coupled_3-D_mixed_finite_element_model_of_Biot_consolidation/figures?lo=1
 tunit = 1# 3600 # 1d=86400s
 
-# elastic moduli
 lamb = 4e7
 G = 4e7
 K = lamb + 2/3*G
@@ -18,25 +16,19 @@ print("K: {:e}, G: {:e}".format(K,G))
 print("E: {:e}, poiss: {}".format(E,poiss))
 print("lamb: {:e}, mu: {:e}".format(lamb,G))
 
-# hydraulic conductivity and density of fluid
 k = 1e-5
 rho = 1000
 
-# porosity, Biot coefficient, compressibilities
 por = 0.375
 alpha = 1
 beta_f = 4.4e-4 #* 1e-6 # for lower storativity not converging in Flow123d
 beta_b = 0
-
-# storage coefficient [Pa^{-1}]
 S = (por*beta_f + (alpha-por)*beta_b)
 
-# gravitational acceleration
-g = 9.81
 
-# column height
-L = 0.25
-# force on open end of column
+g = 9.81
+L = 15
+
 q = 1e4
 
 mv = 1.0/(K+4/3*G)
@@ -105,56 +97,25 @@ print("u0(top: z=0): {}".format(u0*(L)))
 #     u = -u
 #     return u
 
-def analytic_sol_pressure(z, t):
+def analytic_sol(z, t):
     """ Analytical solution of pressure head and displacement."""
     if t == 0:
         h = h0
-        return h
-
-    if (cv*t/(L*L)) < 0.01:
-        p = p0*math.erf( (L-z) / (2*math.sqrt(cv*t)) )
-    else:
-        nk = 1000
-        s_h = 0
-
-        for i in range(nk, 0, -1):
-            # Verruijt
-            a0 = (2 * i - 1) * np.pi / 2 / L
-            a1 = np.cos(a0 * z)
-            a2 = np.exp(-(a0 ** 2) * cv * t)
-            s_h = s_h + ((-1) ** (i - 1)) / (2 * i - 1) * a1 * a2
-    
-            # Ferronato 2010, range from 0, map z!
-            # a0 = (2*i+1) * np.pi/2 / L
-            # a1 = np.sin(a0*z)
-            # a2 = np.exp(-a0**2 * cv*t)
-            # s = s + 1/(2*i+1) * a1 * a2
-            #
-            # a3 = np.cos(a0*z)
-            # s = s + 1/(2*i+1)**2 * a3 * a2
-        p = p0 * 4 / np.pi * s_h
-
-    h = p / (rho * g)
-
-    return h
-
-
-def analytic_sol_displacement(z, t):
-    """ Analytical solution of pressure head and displacement."""
-    if t == 0:
         u = u0 * z
-        return u
+        return h, u
 
+    nk = 100
+    s_h = 0
     s_u = 0
-    nk = 1000
-
-    for i in range(nk, 0, -1):
+    for i in range(1, nk):
         # Verruijt
         a0 = (2 * i - 1) * np.pi / 2 / L
+        a1 = np.cos(a0 * z)
         a1_u = np.cos(a0 * (L-z))
-        a2 = np.exp(-(a0 ** 2) * cv * t)
+        a2 = np.exp(-a0 ** 2 * cv * t)
+        s_h = s_h + (-1) ** (i - 1) / (2 * i - 1) * a1 * a2
         s_u = s_u + 1 / (2 * i - 1) ** 2 * a1_u * a2
-    
+
         # Ferronato 2010, range from 0, map z!
         # a0 = (2*i+1) * np.pi/2 / L
         # a1 = np.sin(a0*z)
@@ -163,25 +124,30 @@ def analytic_sol_displacement(z, t):
         #
         # a3 = np.cos(a0*z)
         # s = s + 1/(2*i+1)**2 * a3 * a2
+    p = p0 * 4 / np.pi * s_h
+    h = p / (rho * g)
 
     u = 8 / (np.pi ** 2) * s_u
     u = mv * p0 * (z - L * u) + u0 * z
-    return u
+    return h, u
 
-
-times = np.array([1., 2., 5., 10., 100.]) # time scale for higher storativity
-zax = np.arange(0, L, L*0.01)
 
 def plot_presure_head():
     import matplotlib.pyplot as plt
     fig, ax1 = plt.subplots()
-    ax1.set_xlabel('pressure head [m]')
+    ax1.set_xlabel('pressure [Pa]')
     ax1.set_ylabel('z [m]')
+
+    # times = np.arange(60, 3600, 60)
+    # times = np.array([1, 10, 20, 60, 120, 600, 1200, 3600]) # time scale for lower storativity
+    times = np.array([1e4, 1e5, 1e6, 1e7, 5e7]) # time scale for higher storativity
+    zax = np.arange(0, 15, 0.1)
 
     vals = np.zeros(zax.shape)
     for t in times:
         for i in range(len(zax)):
-            vals[i] = analytic_sol_pressure(zax[i], t)
+            h, u = analytic_sol(zax[i], t)
+            vals[i] = h
 
         ax1.plot(vals, zax, label="t={}".format(t))
 
@@ -191,7 +157,7 @@ def plot_presure_head():
 
     fig.tight_layout()  # otherwise the right y-label is slightly clipped
     # plt.show()
-    plt.savefig("02_pressure_head.pdf")
+    plt.savefig("04_pressure_head.pdf")
 
 
 def plot_displacement():
@@ -200,10 +166,17 @@ def plot_displacement():
     ax1.set_xlabel('displacement [m]')
     ax1.set_ylabel('z [m]')
 
+    # times = np.arange(60, 3600, 60)
+    # times = np.array([1, 10, 20, 60, 120, 600, 1200, 3600]) # time scale for lower storativity
+    times = np.array([1e4, 1e5, 1e6, 1e7, 5e7]) # time scale for higher storativity
+    # times = np.array([1e4, 1e5])  # time scale for higher storativity
+    zax = np.arange(0, 15, 0.1)
+
     vals = np.zeros(zax.shape)
     for t in times:
         for i in range(len(zax)):
-            vals[i] = analytic_sol_displacement(zax[i], t)
+            h, u = analytic_sol(zax[i], t)
+            vals[i] = u
             # vals[i] = -u_sum(L-zax[i], t)
 
         ax1.plot(vals, zax, label="t={}".format(t))
@@ -214,7 +187,7 @@ def plot_displacement():
 
     fig.tight_layout()  # otherwise the right y-label is slightly clipped
     # plt.show()
-    plt.savefig("02_displacement.pdf")
+    plt.savefig("04_displacement.pdf")
 
 
 plot_presure_head()
@@ -239,8 +212,9 @@ for i in range(ncells):
     ttz = (z1+z2)/3
     
     ttz = ttz + L
-    pressureArray[i] = analytic_sol_pressure(ttz, time * tunit)
-    displacementArray[i] = analytic_sol_displacement(ttz, time * tunit)
+    h, u = analytic_sol(ttz, time * tunit)
+    pressureArray[i] = h
+    displacementArray[i] = u
 
 output.CellData.append(pressureArray, "exact_pressure_head")
 output.CellData.append(displacementArray, "exact_displacement")
