8a1b3774716da08258052d37d481107dd3642d24
Jan Brezina <jan.brezina@tul.cz>
2021-10-24 02:09:52 +0200

    revert relative balance error

--- src/coupling/balance.cc
+++ src/coupling/balance.cc
@@ -1067,8 +1067,8 @@ void Balance::output_csv(double time, char delimiter, const std::string& comment
             // error makes trouble due to cancellation - difference of two similar numbers
             // at least, w can make it relative, since masses are >> 1 most of the time
             // careful with zero division
-            if(std::abs(sum_masses_[qi])*std::numeric_limits<double>::epsilon() > 0)
-                error = error / sum_masses_[qi];
+/*            if(std::abs(sum_masses_[qi])*std::numeric_limits<double>::epsilon() > 0)
+                error = error / sum_masses_[qi];*/
             // DebugOut().fmt("error_[qi]={:.10e} sum_masses_[qi]={:.10e}, initial_mass_[qi]={:.10e}, integrated_sources_[qi]={:.10e}, integrated_fluxes_[qi]={:.10e}",
             //     error, sum_masses_[qi], initial_mass_[qi], integrated_sources_[qi], integrated_fluxes_[qi]);
 
