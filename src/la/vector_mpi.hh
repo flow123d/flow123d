486329006659f15a1e131936b04efdcd5d894db2
David Flanderka <t426.001@centrum.cz>
2021-11-09 09:30:20 +0100

    Revert assert.

--- src/la/vector_mpi.hh
+++ src/la/vector_mpi.hh
@@ -104,7 +104,6 @@ public:
     /// Return value on given position
     inline double get(unsigned int pos) const {
         ASSERT_PTR_DBG(data_ptr_).error("Uninitialized data vector.\n");
-        ASSERT_LT_DBG(pos, data_ptr_->size()).error("Given 'pos' out of vector size!\n");
         return (*data_ptr_)[pos];
     }
 
