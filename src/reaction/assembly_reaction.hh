17dab4191a314be4244adea19950637fe70b7c5d
David Flanderka <t426.001@centrum.cz>
2022-03-08 13:27:49 +0100

    Implement ReactionAssemblySorp class.

--- src/reaction/assembly_reaction.hh
+++ src/reaction/assembly_reaction.hh
@@ -21,6 +21,7 @@
 #include "coupling/generic_assembly.hh"
 #include "coupling/assembly_base.hh"
 #include "reaction/dual_porosity.hh"
+#include "reaction/isotherm.hh"
 #include "fem/fe_p.hh"
 #include "fem/fe_values.hh"
 #include "quadrature/quadrature_lib.hh"
@@ -248,6 +249,108 @@ private:
     FieldSet used_fields_;
 
     IntIdx dof_p0_;                                     ///< Index of local DOF
+
+    template < template<IntDim...> class DimAssembly>
+    friend class GenericAssembly;
+};
+
+template <unsigned int dim>
+class ReactionAssemblySorp : public AssemblyBase<dim>
+{
+public:
+    typedef typename SorptionBase::EqFields EqFields;
+    typedef typename SorptionBase::EqData EqData;
+
+    static constexpr const char * name() { return "ReactionAssemblySorp"; }
+
+    /// Constructor.
+    ReactionAssemblySorp(EqFields *eq_fields, EqData *eq_data)
+    : AssemblyBase<dim>(0), eq_fields_(eq_fields), eq_data_(eq_data) {
+        this->active_integrals_ = ActiveIntegrals::bulk;
+        this->used_fields_ += eq_fields_->scale_aqua;
+        this->used_fields_ += eq_fields_->scale_sorbed;
+        this->used_fields_ += eq_fields_->no_sorbing_surface_cond;
+        this->used_fields_ += eq_fields_->sorption_type;
+        this->used_fields_ += eq_fields_->distribution_coefficient;
+        this->used_fields_ += eq_fields_->isotherm_other;
+    }
+
+    /// Destructor.
+    ~ReactionAssemblySorp() {}
+
+    /// Initialize auxiliary vectors and other data members
+    void initialize(ElementCacheMap *element_cache_map) {
+        //this->balance_ = eq_data_->balance_;
+        this->element_cache_map_ = element_cache_map;
+    }
+
+
+    /// Assemble integral over element
+    inline void cell_integral(DHCellAccessor cell, unsigned int element_patch_idx)
+    {
+        ASSERT_EQ(cell.dim(), dim).error("Dimension of element mismatch!");
+
+        unsigned int i_subst, subst_id;
+        auto p = *( this->bulk_points(element_patch_idx).begin() );
+
+        reg_idx_ = cell.elm().region().bulk_idx();
+        dof_p0_ = cell.get_loc_dof_indices()[0];
+
+        double scale_aqua = eq_fields_->scale_aqua(p);
+        double scale_sorbed = eq_fields_->scale_sorbed(p);
+        double no_sorbing_surface_cond = eq_fields_->no_sorbing_surface_cond(p);
+
+        for(i_subst = 0; i_subst < eq_data_->n_substances_; i_subst++)
+        {
+            subst_id = eq_data_->substance_global_idx_[i_subst];
+
+            Isotherm & isotherm = eq_data_->isotherms[reg_idx_][i_subst];
+
+            bool limited_solubility_on = eq_data_->solubility_vec_[i_subst] > 0.0;
+
+            // in case of no sorbing surface, set isotherm type None
+            if( no_sorbing_surface_cond <= std::numeric_limits<double>::epsilon())
+            {
+                isotherm.reinit(Isotherm::none, false, eq_data_->solvent_density_,
+                                scale_aqua, scale_sorbed,
+                                0,0,0);
+            } else {
+                if ( scale_sorbed <= 0.0)
+                    THROW( SorptionBase::ExcNotPositiveScaling() << SorptionBase::EI_Subst(i_subst) );
+
+                isotherm.reinit(Isotherm::SorptionType(eq_fields_->sorption_type[i_subst](p)),
+                                limited_solubility_on, eq_data_->solvent_density_,
+                                scale_aqua, scale_sorbed,
+                                eq_data_->solubility_vec_[i_subst],
+                                eq_fields_->distribution_coefficient[i_subst](p),
+                                eq_fields_->isotherm_other[i_subst](p));
+            }
+
+            double c_aqua = eq_fields_->conc_mobile_fe[subst_id]->vec().get(dof_p0_);
+            double c_sorbed = eq_fields_->conc_solid_fe[subst_id]->vec().get(dof_p0_);
+            isotherm.compute(c_aqua, c_sorbed);
+            eq_fields_->conc_mobile_fe[subst_id]->vec().set(dof_p0_, c_aqua);
+            eq_fields_->conc_solid_fe[subst_id]->vec().set(dof_p0_, c_sorbed);
+
+            // update maximal concentration per region (optimization for interpolation)
+            if(eq_data_->table_limit_[i_subst] < 0)
+                eq_data_->max_conc[reg_idx_][i_subst] = std::max(eq_data_->max_conc[reg_idx_][i_subst],
+                                                      eq_fields_->conc_mobile_fe[subst_id]->vec().get(dof_p0_));
+        }
+    }
+
+
+private:
+    /// Data objects shared with Elasticity
+    EqFields *eq_fields_;
+    EqData *eq_data_;
+
+    /// Sub field set contains fields used in calculation.
+    FieldSet used_fields_;
+
+    IntIdx dof_p0_;                                   ///< Index of local DOF
+    int reg_idx_;                                     ///< Bulk region idx
+    //unsigned int sbi_;                                ///< Index of substance
 
     template < template<IntDim...> class DimAssembly>
     friend class GenericAssembly;
