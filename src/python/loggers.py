35515d2587515ae869bb34e1dc6038592342faf0
Pavel Exner <pavelexner@gmail.com>
2022-05-09 15:47:50 +0200

    Fix previous cherry-pick.

--- src/python/loggers.py
+++ src/python/loggers.py
@@ -71,7 +71,7 @@ class PrintFormat(object):
             levels=[
                 dict(name=LogLevels.NORMAL, no=60, color=''),
                 dict(name=LogLevels.IMPORTANT, no=60, color='<b>'),
-                dict(name=LogLevels.SUCCESS, no=60, color='<b><g>'),
+                dict(name=LogLevels.SUCCESS, color='<b><g>'),
                 dict(name=LogLevels.FAILED, no=60, color='<b><r>'),
                 dict(name=LogLevels.WARN, no=60, color='<b><y>'),
             ]
