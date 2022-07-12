127d9ed9bf5dd8535d2cca98d8b8ad33a977e543
Jan Brezina <jan.brezina@tul.cz>
2022-07-12 13:17:47 +0200

    Fixed docker image build

--- config/package/make_packages.sh
+++ config/package/make_packages.sh
@@ -40,7 +40,6 @@ build_container=contgnurelease
 imagesversion=`cat ${flow_repo_host}/config/docker/image_tag`
 release_version=`cat ${flow_repo_host}/version`      
 
-install_image="install-${environment}:${imagesversion}"
 
 
 
@@ -61,6 +60,7 @@ echo "target_image: '${target_image}'"
 
 # docker rm -f  || echo "container not running"
 bin/fterm update
+docker rm -f ${build_container}
 bin/fterm rel_$environment -- --privileged -di --name ${build_container} --volume `pwd`:${flow_repo_location}
 
 dexec="docker exec ${build_container}"      # execute command which will follow
@@ -71,8 +71,8 @@ function dexec_setvars_make {
     ${dexec} /bin/bash -c "cd ${flow_repo_location} && bin/setvars.sh && make $*"
 }
 
-git_hash=`${dexec} git rev-parse --short HEAD`
-git_branch=`${dexec} git rev-parse --abbrev-ref HEAD`
+git_hash=`${dexec} cd ${flow_repo_location} && rev-parse --short HEAD`
+git_branch=`${dexec} cd ${flow_repo_location} && git rev-parse --abbrev-ref HEAD`
 
 
 
@@ -87,11 +87,14 @@ else
     image_tag=${git_branch}_${git_hash}
 fi
 
+######################################################################################################### build flow123d install container
+
+
 # copy config
 #${dexec} ls ${flow_repo_location}
 ${dexec} cp ${flow_repo_location}/config/config-jenkins-docker-${build_type}.cmake ${flow_repo_location}/config.cmake
 
-# configure and compile
+# overload git protection
 set_safe_dir="${dexec} git config --global --add safe.directory"
 for d in \
     ${flow_repo_location} \
@@ -104,16 +107,55 @@ do
     ${set_safe_dir} $d
 done
 
-#${dexec} make -C ${flow_repo_location} -j4 all
+# compile
+${dexec} make -C ${flow_repo_location} -j4 all
+dexec_setvars_make package
+
+${dexec} ls ${flow_repo_location}/build_tree/
+${dexec} ls ${flow_repo_location}/build_tree/_CPack_Packages
+${dexec} ls ${flow_repo_location}/build_tree/_CPack_Packages/Linux
+${dexec} ls ${flow_repo_location}/build_tree/_CPack_Packages/Linux/TGZ
 
 #${dexec} make -C ${{flow_repo_location}} FORCE_DOC_UPDATE=1 ref-doc
 #${dexec} make -C ${{flow_repo_location}} html-doc
 #${dexec} make -C ${{flow_repo_location}} doxy-doc
 
+
+############################################################################################# docker image
+install_image="install-${environment}:${imagesversion}"
+target_tagged=flow123d/${target_image}:${image_tag}
+
+tmp_install_dir=${flow_repo_location}/build_tree/_CPack_Packages/Linux/TGZ/Flow123d-${release_version}-Linux
+
+# have to copy package dir out of the mounted volume ${flow_repo_location}
+# TODO: try to use the install target
+${dexec} cp -r ${tmp_install_dir} ${flow_install_location}/docker_package
+# we only use temporary installation and copy it directly from the build image to the target image
+# TODO: need to copy the package out of the vmount, but only for testing
+
+docker rmi -f ${target_tagged}
+docker rmi -f flow123d/temporary_build
+docker commit ${build_container} flow123d/temporary_build
+docker pull "flow123d/${install_image}"
+
+build_date=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
+
+#cp ${destination}/${cmake_package_name} project/src/docker/create/default/${cmake_package_name}
+docker build \
+     --build-arg base_image=flow123d/${install_image} \
+     --build-arg source_image=flow123d/temporary_build \
+     --build-arg source_location=${flow_install_location}/docker_package \
+     --build-arg flow_version=${release_version} \
+     --build-arg flow_install_location=${flow_install_location} \
+     --build-arg git_hash="${git_hash}" \
+     --build-arg build_date="${build_date}" \
+     --tag ${target_tagged} \
+     ${flow_repo_host}/config/package/dockerfile
+
+# Push only if $3 == push
+${docker_push} ${target_tagged}
+
 ##########################################################################################################šš
-
-
-
 
 
 # name of the archives from CMake CPack tool
@@ -135,7 +177,7 @@ pdf_location=doc/reference_manual/flow123d_doc.pdf
 ist_location=doc/reference_manual/input_reference.json
 
 
-target_tagged=flow123d/${target_image}:${image_tag}
+
 
 ############################################################################################ copy testsmake doc
 
@@ -166,38 +208,11 @@ ${dcp}:${flow_repo_location}/build_tree/doc/online-doc/flow123d/.   ${destinatio
 ${dcp}:${flow_repo_location}/${ist_location}                        ${destination}/input_reference.json
 ${dcp}:${flow_repo_location}/bin/fterm                              ${destination}/bin/fterm
 
-############################################################################################# docker image
-
-docker rmi -f ${target_tagged}
-docker commit ${build_container} flow123d/temporary_build
-docker pull "flow123d/${install_image}"
-
-build_date=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
-
-#cp ${destination}/${cmake_package_name} project/src/docker/create/default/${cmake_package_name}
-docker build \
-     --build-arg base_image=flow123d/${install_image} \
-     --build-arg source_image=flow123d/temporary_build \
-     --build-arg source_location=${flow_install_location}/build_package \
-     --build-arg release_version=${release_version} \
-     --build-arg flow_install_location=${flow_install_location} \
-     --build-arg git_hash=${git_hash} \
-     --build-arg build_date=${build_date} \
-     --tag ${target_tagged} \
-     ${ci_docker_file_path}
-
-# Push only if $3 == push
-${docker_push} ${target_tagged}
-     
 ############################################################################################## Linux package
 
 # TODO: simplify creation of the linux package to single command and run it inside the docker
 # ... does not need make, cmake and other tools out of the docker
 
-dexec_setvars_make package
-${dexec} cp -r ${flow_repo_location}/build_tree/_CPack_Packages/Linux/TGZ/Flow123d-${release_version}-Linux ${flow_install_location}/build_package
-# we only use temporary installation and copy it directly from the build image to the taget image
-# TODO: need to copy the package out of the vmount, but only for testing
 
 mkdir -p install-linux
 #  mkdir -p make parent directories as needed
