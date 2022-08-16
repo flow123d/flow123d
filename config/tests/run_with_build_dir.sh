0f1f55895327500e61787797a4db5eda73f961bb
Jan Brezina <jan.brezina@tul.cz>
2022-08-16 18:00:24 +0200

    Try to fix remaining tests

--- config/tests/run_with_build_dir.sh
+++ config/tests/run_with_build_dir.sh
@@ -21,10 +21,10 @@ build_dir_host=build-${git_branch}
 
 # Recreate build files and dirs
 rm -rf ${build_dir_host}
-ls
 mkdir ${build_dir_host} && tar xf build_dir.tar -C ${build_dir_host} --strip-components 1
 ln -s ${build_dir_host} build_tree
 cp ${build_dir_host}/_config.cmake config.cmake
+make update-submodules
 
 # run the command
 bin/fterm dbg_${env} --no-term  exec ${command_with_args}
