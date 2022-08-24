66dbdb4834f058e4499687091cc3fbc224a0db6c
David Flanderka <t426.001@centrum.cz>
2022-08-24 15:52:45 +0200

    Implement __getattr__ method.
    - allow direct access to items in field data dictionary

--- src/python/flowpy/python_field_base.py
+++ src/python/flowpy/python_field_base.py
@@ -7,3 +7,9 @@ import flowpy
 
 class PythonFieldBase(flowpy.PythonFieldBaseCPP):
     pass
+
+    # Allows direct access to items in 'f_dict' field data dictionary.
+    # Example: use 'self.field_name' instead of 'self.f_dict["field_name"]'
+    def __getattr__(self, attr):
+        return self.f_dict.get(attr, None)
+        
