e06375b9b932fe2e6df599593e543fc221a1e228
David Flanderka <t426.001@centrum.cz>
2022-09-07 10:59:01 +0200

    Optimize test script.

--- test_run.sh
+++ test_run.sh
@@ -3,8 +3,8 @@
 # This script adds paths to pybind repository and to flowpy library to PYTHONPATH variable and runs Python.
 
 # Path to pybind
-export PYTHONPATH="${PYTHONPATH}:third_party/pybind11-2.9.2/include"
+#export PYTHONPATH="${PYTHONPATH}:third_party/pybind11-2.9.2/include"
 # Path to actual building directory
-export PYTHONPATH="${PYTHONPATH}:build-DF_asm_field_python/src"
+export PYTHONPATH="${PYTHONPATH}:build_tree/src"
 # Set actual Python instalation
 python3.8
