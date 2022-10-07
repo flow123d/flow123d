cb6f611f10c6fd86d5bf4dfb49d873534c43c59d
David Flanderka <t426.001@centrum.cz>
2022-10-07 13:02:14 +0200

    Simplify env.sh script.

--- env.sh
+++ env.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
 export HOME=/home/flow/
-git config --global --add safe.directory /c/Users/davidflanderka/vyvoj/flow123d
-git config --global --add safe.directory /c/Users/davidflanderka/vyvoj/flow123d/bin/yaml_converter
-git config --global --add safe.directory /c/Users/davidflanderka/vyvoj/flow123d/src/dealii
-git config --global --add safe.directory /c/Users/davidflanderka/vyvoj/flow123d/third_party/bparser
-git config --global --add safe.directory /c/Users/davidflanderka/vyvoj/flow123d/third_party/gtest-1.10.0
-git config --global --add safe.directory /c/Users/davidflanderka/vyvoj/flow123d/third_party/json-3.10.5
-git config --global --add safe.directory /c/Users/davidflanderka/vyvoj/flow123d/third_party/pybind11-2.9.2
+make set-safe-directory
