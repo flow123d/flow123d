f2777275b26e0a0260bbc360baf5b70df9fcd429
David Flanderka <t426.001@centrum.cz>
2023-08-18 15:35:57 +0200

    Benchmark unit test of assembly algorithm.
    - first incomplete version

---
+++ unit_tests/fields/assembly_speed_test.cpp
@@ -0,0 +1,175 @@
+/*
+ * assembly_speed_test.cpp
+ *
+ *  Created on: Aug 16, 2023
+ *      Author: David Flanderka
+ *
+ *  Speed tests of assembly
+ */
+
+#define TEST_USE_PETSC
+#define FEAL_OVERRIDE_ASSERTS
+#include <flow_gtest_mpi.hh>
+
+#include "assembly_benchmark.hh"
+#include "assembly_speed_test.hh"
+
+
+/****************************************************************************************
+ *                 Speed test of Assembly
+ *
+ * Results:
+ * Mesh 27936 elements, 100 assemblation loops - TODO not actual
+ * Checked GenericAssembly with active bulk integral only vs. with all active integrals
+ *
+ *                           bulk      all
+ * add_integrals_to_patch   19.12    43.95
+ * create_patch              3.00    18.59
+ * cache_update              8.43    26.38
+ *
+ ****************************************************************************************/
+
+void AssemblyBenchmarkTest::initialize(const string &input, std::vector<std::string> substances) {
+    Input::ReaderToStorage reader( input, get_input_type(), Input::FileFormat::format_YAML );
+    in_rec_ = reader.get_root_interface<Input::Record>();
+
+    this->time_ = new TimeGovernor(0.0, 0.5);
+    eq_data_->substances_ = substances;
+
+    // Initialize output stream.
+    //std::shared_ptr<OutputTime> stream = OutputTime::create_output_stream("solute", in_rec.val<Input::Record>("output_stream"), time().get_unit_conversion());
+    //this->set_output_stream(stream);
+
+    eq_fields_->set_components(eq_data_->subst_names());
+    eq_fields_->set_input_list( in_rec_.val<Input::Array>("input_fields"), *this->time_ );
+    eq_fields_->initialize();
+
+    // DG stabilization parameters on boundary edges
+    eq_data_->gamma.resize(eq_data_->n_substances());
+    for (unsigned int sbi=0; sbi<eq_data_->n_substances(); sbi++)
+        eq_data_->gamma[sbi].resize(this->mesh_->boundary_.size());
+
+    // Resize coefficient array
+    //eq_data_->max_edg_sides = max(Model::mesh_->max_edge_sides(1), max(Model::mesh_->max_edge_sides(2), Model::mesh_->max_edge_sides(3)));
+
+    eq_data_->output_vec.resize(eq_data_->n_substances());
+    eq_fields_->output_field.set_components(eq_data_->subst_names());
+    eq_fields_->output_field.set_mesh(*this->mesh_);
+    eq_fields_->output_fields.set_mesh(*this->mesh_);
+    eq_fields_->output_type(OutputTime::CORNER_DATA);
+
+    eq_fields_->output_field.setup_components();
+    for (unsigned int sbi=0; sbi<eq_data_->n_substances(); sbi++)
+    {
+        // create shared pointer to a FieldFE, pass FE data and push this FieldFE to output_field on all regions
+        auto output_field_ptr = create_field_fe< 3, FieldValue<3>::Scalar >(eq_data_->dh_);
+        eq_fields_->output_field[sbi].set(output_field_ptr, 0);
+        eq_data_->output_vec[sbi] = output_field_ptr->vec();
+    }
+
+    // set time marks for writing the output
+    //eq_fields_->output_fields.initialize(Model::output_stream_, this->mesh_, in_rec_.val<Input::Record>("output"), this->time_);
+
+    // allocate matrix and vector structures
+    eq_data_->ls    = new LinSys*[eq_data_->n_substances()];
+    eq_data_->ls_dt = new LinSys*[eq_data_->n_substances()];
+    eq_data_->conc_fe.resize(eq_data_->n_substances());
+    std::string petsc_default_opts = "-ksp_type bcgs -pc_type ilu -pc_factor_levels 2 -ksp_diagonal_scale_fix -pc_factor_fill 6.0";
+
+    MixedPtr<FE_P_disc> fe(0);
+    shared_ptr<DiscreteSpace> ds = make_shared<EqualOrderDiscreteSpace>(this->mesh_, fe);
+    eq_data_->dh_p0 = make_shared<DOFHandlerMultiDim>(*this->mesh_);
+    eq_data_->dh_p0->distribute_dofs(ds);
+
+    //stiffness_matrix.resize(eq_data_->n_substances(), nullptr);
+    //mass_matrix.resize(eq_data_->n_substances(), nullptr);
+    //rhs.resize(eq_data_->n_substances(), nullptr);
+    //mass_vec.resize(eq_data_->n_substances(), nullptr);
+    eq_data_->ret_vec.resize(eq_data_->n_substances(), nullptr);
+
+    for (unsigned int sbi = 0; sbi < eq_data_->n_substances(); sbi++) {
+        eq_data_->ls[sbi] = new LinSys_PETSC(eq_data_->dh_->distr().get(), petsc_default_opts);
+        ( (LinSys_PETSC *)eq_data_->ls[sbi] )->set_from_input( in_rec_.val<Input::Record>("solver") );
+        eq_data_->ls[sbi]->set_solution(eq_data_->output_vec[sbi].petsc_vec());
+
+        eq_data_->ls_dt[sbi] = new LinSys_PETSC(eq_data_->dh_->distr().get(), petsc_default_opts);
+        ( (LinSys_PETSC *)eq_data_->ls_dt[sbi] )->set_from_input( in_rec_.val<Input::Record>("solver") );
+        eq_data_->conc_fe[sbi] = create_field_fe< 3, FieldValue<3>::Scalar >(eq_data_->dh_p0);
+
+        VecDuplicate(eq_data_->ls[sbi]->get_solution(), &eq_data_->ret_vec[sbi]);
+    }
+
+    // create assemblation object, finite element structures and distribute DOFs
+    mass_assembly_ = new GenericAssembly< MassAssembly >(eq_fields_.get(), eq_data_.get());
+    //stiffness_assembly_ = new GenericAssembly< StiffnessAssemblyDim >(eq_fields_.get(), eq_data_.get());
+    //sources_assembly_ = new GenericAssembly< SourcesAssemblyDim >(eq_fields_.get(), eq_data_.get());
+
+    int qsize = mass_assembly_->eval_points()->max_size();
+    eq_data_->dif_coef.resize(eq_data_->n_substances());
+    for (unsigned int sbi=0; sbi<eq_data_->n_substances(); sbi++)
+    {
+        eq_data_->dif_coef[sbi].resize(qsize);
+    }
+}
+
+void AssemblyBenchmarkTest::zero_time_step() {
+    eq_fields_->mark_input_times( *this->time_ );
+    eq_fields_->set_time(this->time_->step(), LimitSide::left);
+
+    for (unsigned int sbi = 0; sbi < eq_data_->n_substances(); sbi++)
+        ( (LinSys_PETSC *)eq_data_->ls[sbi] )->set_initial_guess_nonzero();
+
+    // preallocate system matrix
+    for (unsigned int i=0; i<eq_data_->n_substances(); i++)
+    {
+        // preallocate system matrix
+    	eq_data_->ls[i]->start_allocation();
+        //stiffness_matrix[i] = NULL;
+        //rhs[i] = NULL;
+
+        // preallocate mass matrix
+        eq_data_->ls_dt[i]->start_allocation();
+        //mass_matrix[i] = NULL;
+        VecZeroEntries(eq_data_->ret_vec[i]);
+    }
+    //stiffness_assembly_->assemble(eq_data_->dh_);
+    mass_assembly_->assemble(eq_data_->dh_);
+    //sources_assembly_->assemble(eq_data_->dh_);
+    for (unsigned int i=0; i<eq_data_->n_substances(); i++)
+    {
+      VecAssemblyBegin(eq_data_->ret_vec[i]);
+      VecAssemblyEnd(eq_data_->ret_vec[i]);
+    }
+
+    //output_data();
+}
+
+
+
+TEST_F(AssemblyBenchmarkTest, simple_asm) {
+    string eq_data_input = R"YAML(
+    solver: !Petsc
+      a_tol: 1.0e-12
+      r_tol: 1.0e-12
+    input_fields:
+      - region: .BOUNDARY
+        bc_type: diffusive_flux
+      - region: BULK
+        porosity: !FieldFormula
+          value: 0.1*X[0]*X[1]
+        init_conc:
+          - !FieldConstant
+            value: 0.31
+          - !FieldFormula
+            value: 1-X[0]*X[1]
+        diff_m:
+          - !FieldConstant
+            value: [ [ 0.04, 0.02, 0 ], [ 0.02, 0.01, 0 ], [ 0, 0, 0 ] ]
+          - !FieldFormula
+            value: "[ [ 0.01*X[0], 0.2*X[1], 1 ], [ 0.2*X[1], 0.01*X[0], 2 ], [ 1, 2, 3 ] ]"
+    )YAML";
+
+    this->create_and_set_mesh("mesh/cube_2x1.msh");
+    this->initialize( eq_data_input, {"A", "B"} );
+    this->zero_time_step();
+}
