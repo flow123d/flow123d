f4e6a1448da74bc0491f1c6b0df78c83e624916b
David Flanderka <t426.001@centrum.cz>
2023-08-22 10:48:30 +0200

    Fix MultiField transposition unit test.

--- unit_tests/fields/multi_field_test.cpp
+++ unit_tests/fields/multi_field_test.cpp
@@ -44,43 +44,49 @@ FLOW123D_FORCE_LINK_IN_PARENT(field_constant)
 FLOW123D_FORCE_LINK_IN_PARENT(field_formula)
 FLOW123D_FORCE_LINK_IN_PARENT(field_fe)
 
-//string field_constant_input = R"YAML(
-//common: !FieldConstant
-//  value:
-//   - 1
-//   - 2
-//   - 3
-//transposed:
-// - !FieldConstant
-//   value: 1
-// - !FieldConstant
-//   value: 2
-// - !FieldConstant
-//   value: 3
-//)YAML";
-//
-//TEST(MultiField, transposition) {
-//	MultiField<3, FieldValue<3>::Scalar> empty_mf;
-//	Input::Type::Record in_rec = Input::Type::Record("MultiFieldTest","")
-//	    .declare_key("common", empty_mf.get_multifield_input_type(), Input::Type::Default::obligatory(),"" )
-//	    .declare_key("transposed", empty_mf.get_multifield_input_type(), Input::Type::Default::obligatory(),"" )
-//	    .close();
-//
-//	Input::ReaderToStorage json_reader(field_constant_input, in_rec, Input::FileFormat::format_YAML);
-//	Input::Record input = json_reader.get_root_interface<Input::Record>();
-//
-//	Input::Array common = input.val<Input::Array>("common");
-//	Input::Array transposed = input.val<Input::Array>("transposed");
-//
-//	EXPECT_EQ(common.size(), transposed.size());
-//
-//	auto it_c = common.begin<Input::AbstractRecord>();
-//	for (auto it_t = transposed.begin<Input::AbstractRecord>(); it_t != transposed.end(); ++it_t, ++it_c) {
-//		Input::Record rec_t = (*it_t);
-//		Input::Record rec_c = (*it_c);
-//		EXPECT_DOUBLE_EQ( rec_t.val<double>("value"), rec_c.val<double>("value") );
-//	}
-//}
+string field_constant_input = R"YAML(
+common:
+  - 1
+  - 2
+  - 3
+transposed:
+ - !FieldConstant
+   value: 1
+ - !FieldConstant
+   value: 2
+ - !FieldConstant
+   value: 3
+)YAML";
+
+TEST(MultiField, transposition) {
+	MultiField<3, FieldValue<3>::Scalar> empty_mf;
+	Input::Type::Record in_rec = Input::Type::Record("MultiFieldTest","")
+	    .declare_key("common", empty_mf.get_multifield_input_type(), Input::Type::Default::obligatory(),"" )
+	    .declare_key("transposed", empty_mf.get_multifield_input_type(), Input::Type::Default::obligatory(),"" )
+	    .close();
+
+	Input::ReaderToStorage json_reader(field_constant_input, in_rec, Input::FileFormat::format_YAML);
+	Input::Record input = json_reader.get_root_interface<Input::Record>();
+
+	Input::Array common = input.val<Input::Array>("common");
+	Input::Array transposed = input.val<Input::Array>("transposed");
+
+	EXPECT_EQ(common.size(), transposed.size());
+
+    typename FieldValue<3>::Scalar::return_type r_value;
+	FieldValue<3>::Scalar value(r_value);
+
+	auto it_c = common.begin<Input::AbstractRecord>();
+	for (auto it_t = transposed.begin<Input::AbstractRecord>(); it_t != transposed.end(); ++it_t, ++it_c) {
+		Input::Record rec_t = (*it_t);
+		Input::Record rec_c = (*it_c);
+		value.init_from_input( rec_t.val<Input::Array>("value") );
+		double t_val = value.mem_ptr()[0];
+		value.init_from_input( rec_c.val<Input::Array>("value") );
+		double c_val = value.mem_ptr()[0];
+		EXPECT_DOUBLE_EQ( t_val, c_val );
+	}
+}
 
 
 
