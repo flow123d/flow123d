01cbee2388176de81d7c56d7c5cc217ed8ae3062
David Flanderka <t426.001@centrum.cz>
2023-10-05 15:41:55 +0200

    Improve benchmark postprocess script.
    - add mesh_size to data table
    - rename methods
    - improve documentation

--- unit_tests/coupling/dg_asm_bench_postprocess.py
+++ unit_tests/coupling/dg_asm_bench_postprocess.py
@@ -11,13 +11,17 @@ import csv
 import pandas as pd
 
 
+"""
+Hold processed profiler data and helper variables. 
+Object is used during processing of profiler JSON input.
+"""
 class ProfilerHandler:
     def __init__(self, profiler_file, program_params):
         self.profiler_file = profiler_file
         self.program_params = program_params
         
         # Define name of columns 
-        self.column_names = ['commit', 'run_id', 'domain_shape', 'mesh_size', 'spacedim', 'uniformity', 'n_repeats', 'field_variant',   
+        self.column_names = ['commit', 'run_id', 'domain_shape', 'mesh_size', 'spacedim', 'uniformity', 'n_mesh_elements', 'n_repeats', 'field_variant',   
             'assembly_variant', 'assembly_class', 'tag', 'mumber_of_calls', 'integral_type', 'time', 'time_fraction', 'time_fraction_of_reminder']
         
         # Create empty DataFrame object      
@@ -29,8 +33,8 @@ class ProfilerHandler:
         self.sum_subtags_time = 0.0
 
 
-# Store data of one assembly to dictionary
-def asm_profiler_data(json_in, asm_name, ph):
+# Load annd store data of one assembly to DataFrame
+def load_asm_data(json_in, asm_name, ph):
     integrals = {
         "assemble_volume_integrals": "cell_integral",
         "assemble_fluxes_boundary":  "boundary_side_integral",
@@ -57,17 +61,17 @@ def asm_profiler_data(json_in, asm_name, ph):
     ph.df.loc[len(ph.df.index)] = asm_data
 
 
-# Store data of set of assembly to dictionary
-def step_profiler_data(json_in, ph):
+# Load one simulation step, iterate over assembly sub-tags
+def load_simulation_step(json_in, ph):
     for i in json_in:
         if i['tag'] == "assembly":
             for j in i['children']:
                 asm_name = j['tag']
-                asm_profiler_data(j, asm_name, ph)
+                load_asm_data(j, asm_name, ph)
 
 
-# Load data from profilerJSON file
-def load_data(ph):
+# Load data from profiler JSON file
+def load_profiler_data(ph):
     # Load JSON file
     with open(ph.profiler_file) as f_in:
         profiler_data = json.load(f_in)
@@ -79,29 +83,33 @@ def load_data(ph):
     for i in whole_program['children']:
         key_mesh_name = i['tag']
         for j in i['children']:
-            # Store profiler data of one mesh to data table
+            # Process profiler data of one mesh to data table
             key_mesh_size = j['tag']
             mesh_time = j['cumul-time-sum']
-            mesh_repeats = j['children'][0]['call-count-sum']
             key_mesh_params = key_mesh_name.split('_')
-            ph.df.loc[len(ph.df.index)] = [ ph.program_params[0], ph.program_params[1], key_mesh_params[0], key_mesh_size, 
-                key_mesh_params[1], key_mesh_params[2], mesh_repeats, '', '', '', 'full_mesh', j['call-count-sum'], '', mesh_time, '', '' ]
 
             # Extract type of simulation and type of field
             for k in j['children']:
                 key_type = k['tag']
                 # Extract zero time step and simulation step
                 for m in k['children']:
-                    #if m['tag'] == "ZERO-TIME STEP": zero_step = m
-                    if m['tag'] == "SIMULATION-ONE STEP": simulation_step = m
+                    if m['tag'] == "ZERO-TIME STEP":
+                        mesh_repeats = m['call-count-sum']
+                    if m['tag'] == "SIMULATION-ONE STEP": 
+                        simulation_step = m
+                    if m['tag'] == "n_mesh_elements": 
+                        n_mesh_elements = m['call-count-sum']
 
-                key_mesh_type = key_mesh_name + '_' + key_mesh_size + '_' + key_type
                 type_params = key_type.split('_')
                 ph.common_row_data = [ ph.program_params[0], ph.program_params[1], key_mesh_params[0], key_mesh_size, key_mesh_params[1], 
-                    key_mesh_params[2], mesh_repeats, type_params[0], type_params[1] ]
-                #step_profiler_data(zero_step['children'], ph)
-                step_profiler_data(simulation_step['children'], ph)
+                    key_mesh_params[2], (n_mesh_elements/mesh_repeats), mesh_repeats, type_params[0], type_params[1] ]
+                load_simulation_step(simulation_step['children'], ph)
 
+            # Perform data of one mesh to data table
+            ph.df.loc[len(ph.df.index)] = [ ph.program_params[0], ph.program_params[1], key_mesh_params[0], key_mesh_size, key_mesh_params[1], 
+                key_mesh_params[2], (n_mesh_elements/mesh_repeats), mesh_repeats, '', '', '', 'full_mesh', j['call-count-sum']*mesh_repeats, '', 
+                mesh_time, '', '' ]
+            
     #print("------------")
     #print(ph.df)
     #print("============")
@@ -127,7 +135,7 @@ def main():
 
     # load and process JSON
     ph = ProfilerHandler(profiler_file, program_params)
-    load_data(ph)
+    load_profiler_data(ph)
     
     # output to CSV
     csv_output(csv_file, program_params, ph.df)
