854c3ee1cd28fcd655f81f3d6864caee9099abc9
David Flanderka <t426.001@centrum.cz>
2023-10-12 12:23:50 +0200

    Improve postprocess script.
    - use recursive DFS algorithm

--- unit_tests/coupling/dg_asm_bench_postprocess.py
+++ unit_tests/coupling/dg_asm_bench_postprocess.py
@@ -24,95 +24,100 @@ class ProfilerHandler:
         self.column_names = ['commit', 'run_id', 'domain_shape', 'mesh_size', 'spacedim', 'uniformity', 'n_mesh_elements', 'n_repeats', 'field_variant',   
             'assembly_variant', 'assembly_class', 'tag', 'mumber_of_calls', 'integral_type', 'time', 'time_fraction', 'time_fraction_of_reminder']
         
-        # Create empty DataFrame object      
-        self.df = pd.DataFrame( columns=self.column_names )
+        # Dictionary holds pairs tag-method, these methods are called for selected tags during processing of profiler tree
+        self.tag_method = {
+            'full_mesh':                     'full_mesh', 
+            'BaseMeshReader - mesh factory': 'empty', 
+            'ZERO-TIME STEP':                'empty', 
+            'MassAssembly':                  'assembly', 
+            'StiffnessAssembly':             'assembly', 
+            'SourcesAssembly':               'assembly'} 
         
         # Create other data members using during profiler processing
+        self.json_tree_nodes = []
+        self.n_mesh_elements = 0
+        self.mesh_repeats = 0
         self.common_row_data = []
-        self.profiler_subtree = []
         self.sum_subtags_time = 0.0
 
 
-# Load annd store data of one assembly to DataFrame
-def load_asm_data(json_in, asm_name, ph):
+# Process one assembly (Mass, Stiffness or Sources) and all subtags 
+def assembly(json_node, ph, df):
     integrals = {
-        "assemble_volume_integrals": "cell_integral",
-        "assemble_fluxes_boundary":  "boundary_side_integral",
-        "assemble_fluxes_elem_elem": "edge_integral",
-        "assemble_fluxes_elem_side": "dimjoin_intergral"
+        "assemble_volume_integrals": "cell",
+        "assemble_fluxes_boundary":  "boundary_side",
+        "assemble_fluxes_elem_elem": "edge",
+        "assemble_fluxes_elem_side": "dimjoin"
     }
-    
-    ph.profiler_subtree = json_in
+
+    ph.json_tree_nodes.append(json_node)
+    asm_name = json_node['tag']
+    #print(" -", json_node['tag'], ph.json_tree_nodes[-2]['tag'], ph.json_tree_nodes[-1]['tag'])
     ph.sum_subtags_time = 0.0
-    for i in json_in['children']:
+    for i in json_node['children']:
         tag = i['tag']
         time = i['cumul-time-sum']
         integral = None
         if tag in integrals:
             integral = integrals[tag]
-        tag_data = ph.common_row_data.copy()
-        tag_data.extend( [asm_name, tag, i['call-count-sum'], integral, time, (time/ph.profiler_subtree['cumul-time-sum']), 0] )
-        ph.df.loc[len(ph.df.index)] = tag_data
+        tag_row_data = ph.common_row_data.copy()
+        tag_row_data.extend( [asm_name, tag, i['call-count-sum'], integral, time, (time/json_node['cumul-time-sum']), 0] )
+        df = pd.concat([df, pd.DataFrame([tag_row_data], columns=ph.column_names)], ignore_index=True)
         ph.sum_subtags_time += time
-    
-    asm_time = json_in['cumul-time-sum']
-    asm_data = ph.common_row_data.copy()
-    asm_data.extend( [asm_name, asm_name, json_in['call-count-sum'], None, asm_time, 0, ((asm_time-ph.sum_subtags_time)/asm_time)] )
-    ph.df.loc[len(ph.df.index)] = asm_data
+
+    asm_time = json_node['cumul-time-sum']
+    asm_row_data = ph.common_row_data.copy()
+    asm_row_data.extend( [asm_name, asm_name, json_node['call-count-sum'], None, asm_time, 0, ((asm_time-ph.sum_subtags_time)/asm_time)] )
+    df2 = pd.concat([df, pd.DataFrame([asm_row_data], columns=ph.column_names)], ignore_index=True)
+    ph.json_tree_nodes.pop()
+    return df2
 
 
-# Load one simulation step, iterate over assembly sub-tags
-def load_simulation_step(json_in, ph):
-    for i in json_in:
-        if i['tag'] == "assembly":
-            for j in i['children']:
-                asm_name = j['tag']
-                load_asm_data(j, asm_name, ph)
+# We can skip some tags which are not to be processed (e.g. 'ZERO-TIME STEP')
+def empty(json_node, ph, df):
+    return df
+
+
+# Process 'full_mesh' tag
+def full_mesh(json_node, ph, df):
+    ph.mesh_repeats = json_node['call-count-sum']
+    ph.n_mesh_elements = json_node['children'][1]['call-count-sum']
+    mesh_full_name = ph.json_tree_nodes[-3]['tag'] + '_' + ph.json_tree_nodes[-2]['tag'] + '_' + ph.json_tree_nodes[-1]['tag']
+    mesh_params = mesh_full_name.split('_')
+    ph.common_row_data = [ ph.program_params[0], ph.program_params[1], mesh_params[0], mesh_params[3], mesh_params[1], 
+                    mesh_params[2], (ph.n_mesh_elements/ph.mesh_repeats), ph.mesh_repeats, mesh_params[5], mesh_params[4] ]
+    mesh_row_data = ph.common_row_data.copy()
+    mesh_row_data.extend( ['', 'full_mesh', json_node['call-count-sum']*ph.mesh_repeats, '',  json_node['cumul-time-sum'], '', ''] )
+    df2 = pd.concat([df, pd.DataFrame([mesh_row_data], columns=ph.column_names)], ignore_index=True)
+    return process_node(json_node, ph, df2)
+
+
+def process_node(json_node, ph, df):
+    ph.json_tree_nodes.append(json_node)
+    if 'children' in json_node:
+        for i in json_node['children']:
+            if i['tag'] in ph.tag_method.keys():
+                possibles = globals().copy()
+                possibles.update(locals())
+                method = possibles.get( ph.tag_method[ i['tag'] ] )
+                df = method(i, ph, df) 
+            else:
+                df = process_node(i, ph, df)
+    ph.json_tree_nodes.pop()
+    return df
 
 
 # Load data from profiler JSON file
-def load_profiler_data(ph):
+def load_profiler_data(ph, df):
     # Load JSON file
     with open(ph.profiler_file) as f_in:
         profiler_data = json.load(f_in)
     
     # Only one descendant 'Whole Program' of root tree exists 
     whole_program = profiler_data['children'][0]
-
-    # Iterate over data of meshes
-    for i in whole_program['children']:
-        key_mesh_name = i['tag']
-        for j in i['children']:
-            # Process profiler data of one mesh to data table
-            key_mesh_size = j['tag']
-            mesh_time = j['cumul-time-sum']
-            key_mesh_params = key_mesh_name.split('_')
-
-            # Extract type of simulation and type of field
-            for k in j['children']:
-                key_type = k['tag']
-                # Extract zero time step and simulation step
-                for m in k['children']:
-                    if m['tag'] == "ZERO-TIME STEP":
-                        mesh_repeats = m['call-count-sum']
-                    if m['tag'] == "SIMULATION-ONE STEP": 
-                        simulation_step = m
-                    if m['tag'] == "n_mesh_elements": 
-                        n_mesh_elements = m['call-count-sum']
-
-                type_params = key_type.split('_')
-                ph.common_row_data = [ ph.program_params[0], ph.program_params[1], key_mesh_params[0], key_mesh_size, key_mesh_params[1], 
-                    key_mesh_params[2], (n_mesh_elements/mesh_repeats), mesh_repeats, type_params[0], type_params[1] ]
-                load_simulation_step(simulation_step['children'], ph)
-
-            # Perform data of one mesh to data table
-            ph.df.loc[len(ph.df.index)] = [ ph.program_params[0], ph.program_params[1], key_mesh_params[0], key_mesh_size, key_mesh_params[1], 
-                key_mesh_params[2], (n_mesh_elements/mesh_repeats), mesh_repeats, '', '', '', 'full_mesh', j['call-count-sum']*mesh_repeats, '', 
-                mesh_time, '', '' ]
-            
-    #print("------------")
-    #print(ph.df)
-    #print("============")
+    
+    df = process_node(whole_program, ph, df)
+    return df
 
 
 # Perform outout to CSV file
@@ -126,21 +131,45 @@ def csv_output(csv_file, program_params, df):
 
 
 def main():
+    pass
+    
     # store names of input json and output csv file from arg
     profiler_file = sys.argv[1]
     # commit, run_id 
     program_params = sys.argv[2:4]
-    file_name_split = profiler_file.split(".")
-    csv_file = file_name_split[0] + '.csv'
+    csv_file = profiler_file + '.csv'
 
     # load and process JSON
     ph = ProfilerHandler(profiler_file, program_params)
-    load_profiler_data(ph)
+    df = pd.DataFrame( columns=ph.column_names )
+    df = load_profiler_data(ph, df)
     
     # output to CSV
-    csv_output(csv_file, program_params, ph.df)
+    csv_output(csv_file, program_params, df)
+
+# Processes one node of profiler tree: print tag and calls children nodes recursivelly
+def print_profiler_node(json_in, indent):
+    print(indent, json_in['tag'])
+    if 'children' in json_in:
+        indent2 = '    ' + indent
+        for i in json_in['children']:
+            print_profiler_node(i, indent2)
+
+"""
+Helper function. Prints tree of profiler nodes.
+
+Usage:
+  python3 ./dg_asm_bench_postprocess.py <profiler_output.json> <commit> <run_id>
+"""
+def print_profiler_tree():
+    profiler_file = sys.argv[1]
+    # Load JSON file
+    with open(profiler_file) as f_in:
+        profiler_data = json.load(f_in)
+    print_profiler_node(profiler_data['children'][0], '-') # Whole program tag
 
 
-
-main()
+if __name__ == "__main__":
+    #print_profiler_tree()
+    main()
 
