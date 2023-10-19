71ef84110c789057a1dd8ec05ca180473afcb67b
David Flanderka <t426.001@centrum.cz>
2023-10-19 16:20:17 +0200

    Improve determinant method.

--- src/fem/mapping.hh
+++ src/fem/mapping.hh
@@ -37,47 +37,50 @@ template<class T>
 double determinant(const T &M);
 
 
-template<> inline double determinant(const arma::mat::fixed<1,2> &M)
-{
-    return sqrt(M(0,0)*M(0,0)+M(0,1)*M(0,1));
+
+inline arma::mat::fixed<1,1> normal_matrix(const arma::mat::fixed<1,2> &A) {
+    arma::mat::fixed<1,1> res;
+    res(0,0) = A(0,0)*A(0,0)+A(0,1)*A(0,1);
+    return res;
 }
 
-template<> inline double determinant(const arma::mat::fixed<2,1> &M)
-{
-    return sqrt(M(0,0)*M(0,0)+M(1,0)*M(1,0));
+inline arma::mat::fixed<1,1> normal_matrix(const arma::mat::fixed<2,1> &A) {
+    arma::mat::fixed<1,1> res;
+    res(0,0) = A(0,0)*A(0,0)+A(1,0)*A(1,0);
+    return res;
 }
 
-template<> inline double determinant(FMT_UNUSED const arma::mat::fixed<0,3> &M)
-{
-    return 0;
+inline arma::mat::fixed<1,1> normal_matrix(const arma::mat::fixed<1,3> &A) {
+    arma::mat::fixed<1,1> res;
+    res(0,0) = A(0,0)*A(0,0)+A(0,1)*A(0,1)+A(0,2)*A(0,2);
+    return res;
 }
 
-template<> inline double determinant(FMT_UNUSED const arma::mat::fixed<3,0> &M)
-{
-    return 0;
+inline arma::mat::fixed<1,1> normal_matrix(const arma::mat::fixed<3,1> &A) {
+    arma::mat::fixed<1,1> res;
+    res(0,0) = A(0,0)*A(0,0)+A(1,0)*A(1,0)+A(2,0)*A(2,0);
+    return res;
 }
 
-template<> inline double determinant(const arma::mat::fixed<1,3> &M)
-{
-    return sqrt(M(0,0)*M(0,0)+M(0,1)*M(0,1)+M(0,2)*M(0,2));
+inline arma::mat::fixed<2,2> normal_matrix(const arma::mat::fixed<2,3> &A) {
+    arma::mat::fixed<2,2> res;
+    res(0,0) = A(0,0)*A(0,0)+A(0,1)*A(0,1)+A(0,2)*A(0,2);
+    res(0,1) = A(0,0)*A(1,0)+A(0,1)*A(1,1)+A(0,2)*A(1,2);
+    res(1,0) = A(1,0)*A(0,0)+A(1,1)*A(0,1)+A(1,2)*A(0,2);
+    res(1,1) = A(1,0)*A(1,0)+A(1,1)*A(1,1)+A(1,2)*A(1,2);
+    return res;
 }
 
-template<> inline double determinant(const arma::mat::fixed<3,1> &M)
-{
-    return sqrt(M(0,0)*M(0,0)+M(1,0)*M(1,0)+M(2,0)*M(2,0));
+inline arma::mat::fixed<2,2> normal_matrix(const arma::mat::fixed<3,2> &A) {
+    arma::mat::fixed<2,2> res;
+    res(0,0) = A(0,0)*A(0,0)+A(1,0)*A(1,0)+A(2,0)*A(2,0);
+    res(0,1) = A(0,0)*A(0,1)+A(1,0)*A(1,1)+A(2,0)*A(2,1);
+    res(1,0) = A(0,1)*A(0,0)+A(1,1)*A(1,0)+A(2,1)*A(2,0);
+    res(1,1) = A(0,1)*A(0,1)+A(1,1)*A(1,1)+A(2,1)*A(2,1);
+    return res;
 }
 
-template<> inline double determinant(const arma::mat::fixed<2,3> &M)
-{
-    return sqrt((M(0,0)*M(0,0)+M(0,1)*M(0,1)+M(0,2)*M(0,2))*(M(1,0)*M(1,0)+M(1,1)*M(1,1)+M(1,2)*M(1,2))
-               -(M(0,0)*M(1,0)+M(0,1)*M(1,1)+M(0,2)*M(1,2))*(M(0,0)*M(1,0)+M(0,1)*M(1,1)+M(0,2)*M(1,2)));
-}
 
-template<> inline double determinant(const arma::mat::fixed<3,2> &M)
-{
-    return sqrt((M(0,0)*M(0,0)+M(1,0)*M(1,0)+M(2,0)*M(2,0))*(M(0,1)*M(0,1)+M(1,1)*M(1,1)+M(2,1)*M(2,1))
-               -(M(0,0)*M(0,1)+M(1,0)*M(1,1)+M(2,0)*M(2,1))*(M(0,0)*M(0,1)+M(1,0)*M(1,1)+M(2,0)*M(2,1)));
-}
 
 template<> inline double determinant(const arma::mat::fixed<1,1> &M)
 {
@@ -93,6 +96,46 @@ template<> inline double determinant(const arma::mat::fixed<3,3> &M)
 {
     return ( M(0,0)*M(1,1)*M(2,2) + M(0,1)*M(1,2)*M(2,0) + M(0,2)*M(1,0)*M(2,1) )
          - ( M(2,0)*M(1,1)*M(0,2) + M(2,1)*M(1,2)*M(0,0) + M(2,2)*M(1,0)*M(0,1) );
+}
+
+template<> inline double determinant(FMT_UNUSED const arma::mat::fixed<0,3> &M)
+{
+    return 0;
+}
+
+template<> inline double determinant(FMT_UNUSED const arma::mat::fixed<3,0> &M)
+{
+    return 0;
+}
+
+template<> inline double determinant(const arma::mat::fixed<1,2> &M)
+{
+    return sqrt( determinant(normal_matrix(M)) );
+}
+
+template<> inline double determinant(const arma::mat::fixed<2,1> &M)
+{
+    return sqrt( determinant(normal_matrix(M)) );
+}
+
+template<> inline double determinant(const arma::mat::fixed<1,3> &M)
+{
+    return sqrt( determinant(normal_matrix(M)) );
+}
+
+template<> inline double determinant(const arma::mat::fixed<3,1> &M)
+{
+    return sqrt( determinant(normal_matrix(M)) );
+}
+
+template<> inline double determinant(const arma::mat::fixed<2,3> &M)
+{
+    return sqrt( determinant(normal_matrix(M)) );
+}
+
+template<> inline double determinant(const arma::mat::fixed<3,2> &M)
+{
+	return sqrt( determinant(normal_matrix(M)) );
 }
 
 
