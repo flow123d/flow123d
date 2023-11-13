9e66874312bce982bed30117ad03c8389fa87882
David Flanderka <t426.001@centrum.cz>
2023-11-13 15:57:09 +0100

    Test new methods of PatchFEValues in unit test.

--- unit_tests/fem/patch_fe_values_test.cpp
+++ unit_tests/fem/patch_fe_values_test.cpp
@@ -12,6 +12,8 @@
 #include <cmath>
 #include "arma_expect.hh"
 #include "armadillo"
+#include <mesh_constructor.hh>
+
 #include "system/armadillo_tools.hh"
 #include "system/sys_profiler.hh"
 #include "quadrature/quadrature_lib.hh"
@@ -25,17 +27,50 @@
 
 
 TEST(FeValues, patch_test_basic) {
-    PatchFEValues<3> patch_fe_values(15);
-    FEValues<3> fe_values;
+    FilePath::set_io_dirs(".",UNIT_TESTS_SRC_DIR,"",".");
+    Mesh * mesh = mesh_full_constructor("{ mesh_file=\"fem/small_mesh.msh\", optimize_mesh=false }");
 
-    patch_fe_values.resize(10);
-    EXPECT_EQ(patch_fe_values.used_size(), 10);
-    EXPECT_EQ(patch_fe_values.max_size(), 15);
-    patch_fe_values.resize(7);
-    EXPECT_EQ(patch_fe_values.used_size(), 7);
-    EXPECT_EQ(patch_fe_values.max_size(), 15);
-    patch_fe_values.resize(15);
-    EXPECT_EQ(patch_fe_values.used_size(), 15);
-    EXPECT_EQ(patch_fe_values.max_size(), 15);
+    unsigned int order = 2;
+    Quadrature *quad = new QGauss(2, order); // DIM = 2
+    shared_ptr<FiniteElement<2>> fe = std::make_shared< FE_P_disc<2> >(order);
+    UpdateFlags u = update_values | update_gradients | update_JxW_values | update_jacobians | update_quadrature_points;
+
+    PatchFEValues<3> patch_fe_values(mesh->n_elements());
+    FEValues<3> fe_values;
+    patch_fe_values.initialize(*quad, *fe, u);
+    fe_values.initialize(*quad, *fe, u);
+
+    // Fills vector of ElementAccessors
+    std::vector< ElementAccessor<3> > element_vec;
+    for (auto elm : mesh->elements_range()) {
+    	element_vec.push_back(elm);
+    }
+
+    // Reinitializes FE data of all 2D elements in one step
+    patch_fe_values.reinit(element_vec);
+    EXPECT_EQ(patch_fe_values.used_size(), 5);
+    EXPECT_EQ(patch_fe_values.max_size(), 5);
+
+    // Iterates over elements, reinitializes FEValues, compares values with FEPatchValues
+    for (uint i=0; i<element_vec.size(); ++i)
+    {
+        if (element_vec[i].dim() != 2) continue;
+        fe_values.reinit(element_vec[i]);
+        patch_fe_values.get_cell(i);
+
+        // Loops are swapped, JxW and determinant don't depend on dofs
+        for (unsigned int k=0; k<patch_fe_values.n_points(); k++)
+        {
+            for (unsigned int j=0; j<patch_fe_values.n_dofs(); j++)
+            {
+                EXPECT_EQ(fe_values.shape_value(j,k), patch_fe_values.shape_value(j,k));
+                EXPECT_ARMA_EQ(fe_values.shape_grad(j,k), patch_fe_values.shape_grad(j,k));
+            }
+            EXPECT_EQ(fe_values.JxW(k), patch_fe_values.JxW(k));
+            //EXPECT_EQ(fe_values.determinant(k), patch_fe_values.determinant(k));
+        }
+    }
+
+    delete mesh;
 }
 
