a45f1bb4e1d1890f010449f8c6c3749198b4e15d
Paulie14 <pavelexner@gmail.com>
2024-02-05 17:31:54 +0100

    Print home and HOME in auto build script.

--- config/build/auto_build.sh
+++ config/build/auto_build.sh
@@ -106,7 +106,7 @@ cp config/config-jenkins-docker-${build_type}.cmake config.cmake
 
 # add full version
 echo "set(FLOW_MANUAL_VERSION ${release_tag})" >> config.cmake
-${dexec} printenv "HOME"
+${dexec} printenv "home HOME"
 ${dexec} id
 ${dexec} make -C ${flow_repo_location} set-safe-directory
 ${dexec} git config --global --add safe.directory '*'
