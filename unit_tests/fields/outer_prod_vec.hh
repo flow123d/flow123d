cdb6bec43153b3c2a16b28f26ccb4fcc050079d3
David Flanderka <t426.001@centrum.cz>
2024-05-09 14:27:28 +0200

    Fix OuterProdVec class.
    - own implementation without inheritance

--- unit_tests/fields/outer_prod_vec.hh
+++ unit_tests/fields/outer_prod_vec.hh
@@ -7,25 +7,43 @@
 #include "system/asserts.hh"
 
 
-class OuterProdVec : public Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>
+class OuterProdVec
 {
 public:
-    using Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>::Matrix; // Inherit constructors
+    typename Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> data;
+
+    OuterProdVec() {}
+
+    OuterProdVec(int n) {
+        data.resize(n, 1);
+    }
+
+    OuterProdVec(int m, int n) {
+        data.resize(m, n);
+    }
+
+    inline double & operator()(std::size_t i, std::size_t j) {
+        return data(i, j);
+    }
 
     // Override the operator+
-    OuterProdVec operator+(const OuterProdVec& other) const
+    inline OuterProdVec operator+(const OuterProdVec& other) const
     {
-        ASSERT_EQ(this->rows(), other.rows());
-        ASSERT_EQ(this->cols(), other.cols());
-    	return OuterProdVec(static_cast<const Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>&>(*this) + other);
+        ASSERT_EQ(this->data.rows(), other.data.rows());
+        ASSERT_EQ(this->data.cols(), other.data.cols());
+        OuterProdVec res;
+        res.data = this->data + other.data;
+        return res;
     }
 
     // Override the operator*
-    OuterProdVec operator*(const OuterProdVec& other) const
+    inline OuterProdVec operator*(const OuterProdVec& other) const
     {
-        ASSERT_EQ(this->cols(), 1);
-        ASSERT_EQ(other.cols(), 1);
-        return OuterProdVec(static_cast<const Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>&>(*this) * other.transpose());
+        ASSERT_EQ(this->data.cols(), 1);
+        ASSERT_EQ(other.data.cols(), 1);
+        OuterProdVec res;
+        res.data = this->data * other.data.transpose();
+        return res;
     }
 };
 
@@ -36,14 +54,14 @@ int test_add_operator_simple()
     OuterProdVec arr2(3, 1);
 
     // Initialize arrays with some values
-    arr1 << 1, 2, 3;
-    arr2 << 4, 5, 6;
+    arr1.data << 1, 2, 3;
+    arr2.data << 4, 5, 6;
 
     // Add arrays using overridden operator+
     OuterProdVec result = arr1 + arr2;
 
     // Output the result
-    std::cout << "Result:\n" << result << std::endl;
+    std::cout << "Result:\n" << result.data << std::endl;
 
     return 0;
 }
@@ -54,14 +72,14 @@ int test_multi_operator_simple()
     OuterProdVec arr2(3, 1);
 
     // Initialize arrays with some values
-    arr1 << 1, 2, 3;
-    arr2 << 4, 5, 6;
+    arr1.data << 1, 2, 3;
+    arr2.data << 4, 5, 6;
 
     // Multiple arrays using overridden operator*
     OuterProdVec result = arr1 * arr2;
 
     // Output the result
-    std::cout << "Result:\n" << result << std::endl;
+    std::cout << "Result:\n" << result.data << std::endl;
 
     return 0;
 }
@@ -73,19 +91,19 @@ int test_add_operator()
 
     // Initialize arrays with some values
 	for (uint i=0; i<3; ++i) {
-        arr1(i).resize(3, 1);
-        arr1(i) << (i+1), (i+2), (i+3);
-        arr2(i).resize(3, 1);
-        arr2(i) << (2*i+1), (2*i+2), (2*i+3);
+        arr1(i).data.resize(3, 1);
+        arr1(i).data << (i+1), (i+2), (i+3);
+        arr2(i).data.resize(3, 1);
+        arr2(i).data << (2*i+1), (2*i+2), (2*i+3);
 	}
 
     // Add arrays using overridden operator+
     Eigen::Vector<OuterProdVec, 3> result = arr1 + arr2;
 
     // Output the result
-    std::cout << "Result 0:\n" << result(0) << std::endl;
-    std::cout << "Result 1:\n" << result(1) << std::endl;
-    std::cout << "Result 2:\n" << result(2) << std::endl;
+    std::cout << "Result 0:\n" << result(0).data << std::endl;
+    std::cout << "Result 1:\n" << result(1).data << std::endl;
+    std::cout << "Result 2:\n" << result(2).data << std::endl;
 
     return 0;
 }
@@ -97,28 +115,22 @@ int test_multi_operator()
 
     // Initialize arrays with some values
     for (uint i=0; i<3; ++i) {
-        arr1(i).resize(3, 1);
-        arr1(i) << (i+1), (i+2), (i+3);
-        arr2(i).resize(3, 1);
-        arr2(i) << (2*i+1), (2*i+2), (2*i+3);
+        arr1(i).data.resize(3, 1);
+        arr1(i).data << (i+1), (i+2), (i+3);
+        arr2(i).data.resize(3, 1);
+        arr2(i).data << (2*i+1), (2*i+2), (2*i+3);
     }
 
     // Multiple arrays using overridden operator*
-    //auto result = arr1 * arr2; // ERROR, during compilation, see log
-
-    // compilable and functional code, fix of line 107
-    Eigen::Array<OuterProdVec, 3, 3> result;
-    for (uint i=0; i<result.rows(); ++i)
-        for (uint j=0; j<result.cols(); ++j)
-            result(i,j) = arr1(i) * arr2(j);
+    auto result = arr1 * arr2;
 
     // Output the result
-    std::cout << "Array1 0:\n" << arr1(0) << std::endl;
-    std::cout << "Array1 1:\n" << arr1(1) << std::endl;
-    std::cout << "Array2 0:\n" << arr2(0) << std::endl;
-    std::cout << "Array2 1:\n" << arr2(1) << std::endl;
-    std::cout << "Result 0:\n" << result(0,0) << std::endl;
-    std::cout << "Result 1:\n" << result(1,1) << std::endl;
+    std::cout << "Array1 0:\n" << arr1(0).data << std::endl;
+    std::cout << "Array1 1:\n" << arr1(1).data << std::endl;
+    std::cout << "Array2 0:\n" << arr2(0).data << std::endl;
+    std::cout << "Array2 1:\n" << arr2(1).data << std::endl;
+    std::cout << "Result 0:\n" << result(0,0).data << std::endl;
+    std::cout << "Result 1:\n" << result(1,1).data << std::endl;
 
     return 0;
 }
