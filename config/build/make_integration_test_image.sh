341399f936dd003d9f8c9ce82e59020b9eaf06eb
MocStepan <stepan.moc@tul.cz>
2024-10-09 21:08:05 +0200

    add make_integration_test_image.sh should run

--- config/build/make_integration_test_image.sh
+++ config/build/make_integration_test_image.sh
@@ -1,78 +1,40 @@
 #!/bin/bash
 # Usage:
-# make_integration_test_image.sh <environment> <target_image> <release_tag> [push]
+# ./make_integration_test_image.sh <environment> <image_source_name_base> <release_tag> [push]
 
-
-#######################################
-# GET INPUT ARGMUNETS
-#######################################
-
-# Stop on first error.
+# Stop on first error
 set -e
 set -x
 
-build_type=$1;         # rel | dbg
-environment=$2;        # gnu ( | intel)
-image_name_base=$3;    # ci (continuous integration build, commit hash part of the resutulting image name)
-release_tag=$4;        # version
-can_push=$5;           # push
+environment=$1              # gnu
+image_source_name_base=$2   # flow123d | ci
+release_tag=$3              # version
+docker_push=$4              # push
 
-# TODO: use push or echo check by if [ "$can_push" == "push" ]
-# Use push for testing purposes
 docker_push="docker push"
 
-# what todo next we can add tests file to docker for testing, but it will be only
-# for the local testing. Need to use ci_gnu sa a base image for the testing and 
-# add extra libraries for the testing. Like vtk.
-# Base image (ci_gnu) should contain correct runtest from _CPack_Packages
-
-
-#######################################
-# SET VARIABLES
-#######################################
-
-# Paths on host
+# Paths
 flow_repo_host="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/../.. && pwd )"
 cd ${flow_repo_host}
-destination="`pwd`/publish_${environment}"
-
-# Paths inside docker container
-flow_install_location=/opt/flow123d
-flow_repo_location=`pwd`
 
 # Docker calls
-build_container=itcontgnurelease
+build_container=contgnurelease
+source_image="stepanmoc/${image_source_name_base}-${environment}:${release_tag}"
+target_image="stepanmoc/it-${environment}:${release_tag}"
 
-# Image version
-imagesversion=`cat ${flow_repo_host}/config/build/image_tag`
+docker rmi -f ${target_tagged}
+docker pull ${source_image}
 
-# Git hash and branch
-git_hash=`git rev-parse --short=6 HEAD`
-git_branch=`git rev-parse --abbrev-ref HEAD`
+# Build a new Docker image by adding VTK and copying the test directory
+docker build --build-arg source_image=${source_image} \
+             --build-arg flow_version=${release_tag} \
+             --build-arg git_hash=$(git rev-parse --short HEAD) \
+             --build-arg build_date=$(date -u +"%Y-%m-%dT%H:%M:%SZ") \
+             --tag ${target_image} \
+             ${flow_repo_host}/config/build/it_docker_file
 
-# Build directory
-build_dir_host=build-${git_branch}
+# Push the image to Docker Hub
+docker push ${target_image}
 
-# Base image
-base_image=${image_name_base}-${environment}
-base_tagged=${base_image}:${imagesversion}
-
-# Target image	
-target_image=it-${environment}
-target_tagged=${target_image}:${imagesversion}
-
-
-
-#######################################
-# VARIABLES SUMMARY CHECK
-#######################################
-echo "VARIABLES SUMMARY CHECK"
-echo "build_type: ${build_type}"
-echo "environment: ${environment}"
-echo "image_name_base: ${image_name_base}"
-echo "release_tag: ${release_tag}"
-echo "can_push: ${can_push}"
-echo "docker_push: ${docker_push}"
-echo "imagesversion: ${imagesversion}"
-echo "git_hash: ${git_hash}"
- 
+# Done
+echo "Docker image '${target_image}' has been built and pushed."
