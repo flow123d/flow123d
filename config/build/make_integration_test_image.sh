00bde29aeff48b2f697e5f5c7e4a8ebb13300369
MocStepan <stepan.moc@tul.cz>
2024-10-15 21:27:22 +0200

    changed make_integration_test_image

--- config/build/make_integration_test_image.sh
+++ config/build/make_integration_test_image.sh
@@ -6,26 +6,27 @@
 set -e
 set -x
 
-environment=$1              # gnu
-image_source_name_base=$2   # flow123d | ci
-release_tag=$3              # version
-docker_push=$4              # push
+environment=$1              # gnu | intel
+release_tag=$2              # version
+docker_command=$3           # push
 
-docker_push="docker push"
+if [ "${docker_command}" == "push" ]
+then
+    docker_command="docker push"
+else
+    docker_command="echo docker push"
+fi
 
-# Paths
+
 flow_repo_host="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/../.. && pwd )"
 cd ${flow_repo_host}
 
-# Docker calls
-build_container=contgnurelease
-source_image="stepanmoc/${image_source_name_base}-${environment}:${release_tag}"
+source_image="stepanmoc/ci-${environment}:${release_tag}"
 target_image="stepanmoc/it-${environment}:${release_tag}"
 
-docker rmi -f ${target_tagged}
+docker rmi -f ${target_image}
 docker pull ${source_image}
 
-# Build a new Docker image by adding VTK and copying the test directory
 docker build --build-arg source_image=${source_image} \
              --build-arg flow_version=${release_tag} \
              --build-arg git_hash=$(git rev-parse --short HEAD) \
@@ -33,8 +34,7 @@ docker build --build-arg source_image=${source_image} \
              --tag ${target_image} \
              ${flow_repo_host}/config/build/it_docker_file
 
-# Push the image to Docker Hub
-${docker_push} ${target_image}
 
-# Done
+${docker_command} ${target_image}
+
 echo "Docker image '${target_image}' has been built and pushed."
