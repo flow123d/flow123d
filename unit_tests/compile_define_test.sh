8d388f03d09426806f379d237cd16edc8c90db83
MocStepan <stepan.moc@actis.cz>
2025-02-10 18:12:18 +0100

    small repairs for cmakelist.txt and compile define test script

--- unit_tests/compile_define_test.sh
+++ unit_tests/compile_define_test.sh
@@ -6,15 +6,13 @@ COMPILE_OUTPUT="$2"      # Cesta k výstupnímu souboru kompilace
 XML_REPORT="$3"          # Cesta k XML reportu
 
 
-COMPILE_LOG="${COMPILE_OUTPUT}.log"
-
 {
     eval "$COMPILE_CMD"
-} &> "$COMPILE_LOG"
+} &> "$COMPILE_OUTPUT"
 
 STATUS=$?
 
 printf "Compile status: %d\n" "$STATUS"
-python3 "$(dirname "$0")/generate_compile_xml.py" "$STATUS" "$COMPILE_LOG" "$XML_REPORT"
+python3 "$(dirname "$0")/generate_compile_xml.py" "$STATUS" "$COMPILE_OUTPUT" "$XML_REPORT"
 
 exit $STATUS
