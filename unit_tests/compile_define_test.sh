708856e72b8dbded19fa8f38901d0ecdb8255668
MocStepan <stepan.moc@actis.cz>
2025-02-10 22:37:48 +0100

    add debug step for test reporter

--- unit_tests/compile_define_test.sh
+++ unit_tests/compile_define_test.sh
@@ -10,14 +10,19 @@ COMPILE_OUTPUT="$2"      # Path to output file
 XML_REPORT="$3"          # Path to XML report
 
 COMPILE_CMD=$(echo "$COMPILE_CMD" | sed 's/;/ /g')
+mkdir -p "$(dirname "$XML_REPORT")"
 
 {
     eval "$COMPILE_CMD"
-} &> "$COMPILE_OUTPUT"
+} 2>&1 | tee "$COMPILE_OUTPUT"
 
 STATUS=$?
 
 printf "Compile status: %d\n" "$STATUS"
+cat "$COMPILE_OUTPUT"
 python3 "$(dirname "$0")/generate_compile_xml.py" "$STATUS" "$COMPILE_OUTPUT" "$XML_REPORT"
 
 exit $STATUS
+
+
+STATUS=$?
