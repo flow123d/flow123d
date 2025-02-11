9c44a6612ab729e458da41c819d80717cae207d5
MocStepan <stepan.moc@actis.cz>
2025-02-11 12:53:32 +0100

    Removed unsused binary path

--- unit_tests/compile_define_test.sh
+++ unit_tests/compile_define_test.sh
@@ -9,24 +9,12 @@ COMPILE_CMD="$1"         # Commands to compile
 COMPILE_OUTPUT="$2"      # Path to output file
 XML_REPORT="$3"          # Path to XML report
 
-COMPILE_CMD=$(echo "$COMPILE_CMD" | sed 's/;/ /g')
-
 {
     eval "$COMPILE_CMD"
 } > "$COMPILE_OUTPUT" 2>&1
 
 STATUS=$?
 printf "Compile status: %d\n" "$STATUS"
-
-BINARY_PATH=$(echo "$COMPILE_CMD" | awk '{for (i=1; i<=NF; i++) if ($i == "-o") print $(i+1)}')
-
-if [ -f "$BINARY_PATH" ]; then
-    echo "Binary exists: $BINARY_PATH"
-    ls -l "$BINARY_PATH"
-else
-    echo "Binary not found: $BINARY_PATH"
-    STATUS=127
-fi
 
 echo "DEBUG: Starting python script"
 
