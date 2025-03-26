89985293f85098889837cc03d7104f6abad9fdff
David Flanderka <t426.001@centrum.cz>
2025-03-26 15:55:29 +0100

    Remove import of pathfix.

--- src/py123d/scripts/core/base.py
+++ src/py123d/scripts/core/base.py
@@ -28,8 +28,7 @@ def find_base_dir():
     Attempts to find base directory from pathfix's module's location
     """
     import os
-    import pathfix
-    path = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(pathfix.__file__)), '../..', '..'))
+    path = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../..', '../..'))
     return path
 
 
