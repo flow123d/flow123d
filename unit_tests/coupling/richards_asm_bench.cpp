dd70fde53e65d99036553c42a122865fafbc589a
David Flanderka <t426.001@centrum.cz>
2025-12-16 13:03:59 +0100

    Proposal of Richard benchmark test.

---
+++ unit_tests/coupling/richards_asm_bench.cpp
@@ -0,0 +1,76 @@
+/*
+ * richards_asm_bench.cpp
+ *
+ *  Created on: Nov 26, 2025
+ *      Author: David Flanderka
+ *
+ *  Speed tests of DarcyFlowassembly
+ */
+
+#define TEST_USE_PETSC
+#define FEAL_OVERRIDE_ASSERTS
+#include <flow_gtest_mpi.hh>
+
+#include "richards_mockup.impl.hh"
+#include "bench_meshes_handler.hh"
+
+
+TEST_F(RichardsMockupTest, simple_asm) {
+    string eq_data_input = R"YAML(
+    nonlinear_solver:
+      linear_solver: !Petsc
+        a_tol: 1.0e-12
+        r_tol: 1.0e-12
+    input_fields:
+      - region: .BOUNDARY
+        bc_type: dirichlet
+        bc_pressure: !FieldFormula
+          value: X[0]
+      - region: BULK
+        anisotropy: 10
+        cross_section: 1
+        sigma: 0.05
+        init_pressure: !FieldFormula
+          value: -0.2*X[1]
+    )YAML";
+
+    BenchMeshesHandler mesh_handler;
+    std::vector<std::string> meshes_table = mesh_handler.get_mesh_names("richards_asm");
+    std::vector<std::string> meshes_sizes = mesh_handler.get_mesh_sizes();
+
+    std::vector< std::shared_ptr<CodePoint> > cp_vec;
+    std::vector< std::shared_ptr<CodePoint> > cp_vec_in;
+    for (uint i=0; i<meshes_table.size(); ++i)
+    {
+        // replace START_TIMER tag, we can't set constexpr string converted from meshes_table[i]
+    	cp_vec.emplace_back( new CODE_POINT(meshes_table[i].c_str()) );
+        TimerFrame timer = TimerFrame( *cp_vec[i] );
+
+        for (uint j=0; j<meshes_sizes.size(); ++j)
+        {
+        	cp_vec_in.emplace_back( new CODE_POINT(meshes_sizes[j].c_str()) );
+            TimerFrame timer = TimerFrame( *cp_vec_in[i*meshes_sizes.size()+j] );
+
+            std::string mesh_file = "../../../unit_tests/benchmark_meshes/" + meshes_table[i] + "_" + meshes_sizes[j] + ".msh";
+            uint n_repeats = 1;
+            if (meshes_sizes[j] == "small") {
+                // 10 repeats of simulation for small meshes
+                n_repeats = 10;
+            }
+
+            for (uint k=0; k<n_repeats; ++k) {
+                // measure needs call of assembly algorithm in separate methods
+                this->run_fullassembly_model(eq_data_input, mesh_file);
+                this->run_computelocal_model(eq_data_input, mesh_file);
+                this->run_evalfields_const(eq_data_input, mesh_file);
+                this->run_evalfields_model(eq_data_input, mesh_file);
+            }
+            Profiler::instance()->stop_timer( *cp_vec_in[i*meshes_sizes.size()+j] );
+
+        }
+        // replace END_TIMER equivalent as START_TIMER
+        Profiler::instance()->stop_timer( *cp_vec[i] );
+    }
+    this->profiler_output("richards_asm");
+    Profiler::uninitialize();
+}
---
+++ unit_tests/coupling/richards_mockup.impl.hh
@@ -0,0 +1,121 @@
+#ifndef RICHARDS_MOCKUP_IMPL_HH_
+#define RICHARDS_MOCKUP_IMPL_HH_
+
+#include "richards_mockup.hh"
+#include "linsys_null.hh"
+#include "flow/assembly_richards.hh"
+#include "fields/field_add_potential.hh"
+
+
+void RichardsMockupTest::run_fullassembly_const(const string &eq_data_input, const std::string &mesh_file) {
+    // FullAssembly + field_const
+//    START_TIMER("FullAssembly_const");
+//    START_TIMER("full_mesh"); // necessary for correct process of profiler output
+//    RichardsMockup<MHMatrixAssemblyRichardsDim> test_full_asm_const(true);
+//    test_full_asm_const.create_and_set_mesh(mesh_file);
+//    test_full_asm_const.initialize( eq_data_input );
+//    test_full_asm_const.eq_fields_->init_field_constants(0.5, arma::vec3("0.8 0.6 0"));
+//    test_full_asm_const.run_simulation();
+//    END_TIMER("full_mesh");
+//    END_TIMER("FullAssembly_const");
+}
+
+void RichardsMockupTest::run_fullassembly_model(const string &eq_data_input, const std::string &mesh_file) {
+    // FullAssembly + field_model
+    START_TIMER("FullAssembly_model");
+    START_TIMER("full_mesh"); // necessary for correct process of profiler output
+    RichardsMockup<MHMatrixAssemblyRichardsDim> test_full_asm_model(true);
+    test_full_asm_model.create_and_set_mesh(mesh_file);
+    std::cout << "test 1" << std::endl;
+    test_full_asm_model.initialize( eq_data_input );
+    std::cout << "test 2" << std::endl;
+//    test_full_asm_model.eq_fields_->init_field_models();
+//    std::cout << "test 3" << std::endl;
+//    test_full_asm_model.run_simulation();
+//    std::cout << "test 4" << std::endl;
+    END_TIMER("full_mesh");
+    END_TIMER("FullAssembly_model");
+}
+
+void RichardsMockupTest::run_computelocal_const(const string &eq_data_input, const std::string &mesh_file) {
+    // ComputeLocal + field_const
+//    START_TIMER("ComputeLocal_const");
+//    START_TIMER("full_mesh"); // necessary for correct process of profiler output
+//    RichardsMockup<MHMatrixAssemblyRichardsDim> test_comp_local_const(false);
+//    test_comp_local_const.create_and_set_mesh(mesh_file);
+//    test_comp_local_const.initialize( eq_data_input );
+//    test_comp_local_const.eq_fields_->init_field_constants(0.5, arma::vec3("0.8 0.6 0"));
+//    test_comp_local_const.run_simulation();
+//    END_TIMER("full_mesh");
+//    END_TIMER("ComputeLocal_const");
+}
+
+void RichardsMockupTest::run_computelocal_model(const string &eq_data_input, const std::string &mesh_file) {
+    // ComputeLocal + field_model
+//    START_TIMER("ComputeLocal_model");
+//    START_TIMER("full_mesh"); // necessary for correct process of profiler output
+//    RichardsMockup<MHMatrixAssemblyRichardsDim> test_comp_local_model(false);
+//    test_comp_local_model.create_and_set_mesh(mesh_file);
+//    test_comp_local_model.initialize( eq_data_input );
+//    test_comp_local_model.eq_fields_->init_field_models();
+//    test_comp_local_model.run_simulation();
+//    END_TIMER("full_mesh");
+//    END_TIMER("ComputeLocal_model");
+}
+
+void RichardsMockupTest::run_evalfields_const(const string &eq_data_input, const std::string &mesh_file) {
+    // EvalFields + field_const
+//    START_TIMER("EvalFields_const");
+//    START_TIMER("full_mesh"); // necessary for correct process of profiler output
+//    RichardsMockup<MHMatrixAssemblyRichardsDim> test_eval_fields_const(false);
+//    test_eval_fields_const.create_and_set_mesh(mesh_file);
+//    test_eval_fields_const.initialize( eq_data_input );
+//    test_eval_fields_const.eq_fields_->init_field_constants(0.5, arma::vec3("0.8 0.6 0"));
+//    test_eval_fields_const.run_simulation();
+//    END_TIMER("full_mesh");
+//    END_TIMER("EvalFields_const");
+}
+
+void RichardsMockupTest::run_evalfields_model(const string &eq_data_input, const std::string &mesh_file) {
+    // EvalFields + field_model
+//    START_TIMER("EvalFields_model");
+//    START_TIMER("full_mesh"); // necessary for correct process of profiler output
+//    RichardsMockup<MHMatrixAssemblyRichardsDim> test_eval_fields_model(false);
+//    test_eval_fields_model.create_and_set_mesh(mesh_file);
+//    test_eval_fields_model.initialize( eq_data_input );
+//    test_eval_fields_model.eq_fields_->init_field_models();
+//    test_eval_fields_model.run_simulation();
+//    END_TIMER("full_mesh");
+//    END_TIMER("EvalFields_model");
+}
+
+
+template<template<IntDim...> class MhMatrix>
+void RichardsMockup<MhMatrix>::initialize_asm() {
+    this->mh_matrix_assembly_ = new GenericAssembly< MhMatrix >(this->eq_data_.get());
+}
+
+template<template<IntDim...> class MhMatrix>
+void RichardsMockup<MhMatrix>::assembly_linear_system()
+{
+    eq_data_->p_edge_solution.local_to_ghost_begin();
+    eq_data_->p_edge_solution.local_to_ghost_end();
+
+    eq_data_->is_linear = eq_fields_->genuchten_p_head_scale.field_result(this->mesh_->region_db().get_region_set("BULK")) == result_zeros;
+
+    //DebugOut() << "Assembly linear system\n";
+    START_TIMER("full assembly");
+    eq_data_->lin_sys_schur->start_add_assembly();
+
+    eq_data_->time_step_ = this->time_->dt();
+
+    eq_data_->lin_sys_schur->mat_zero_entries();
+    eq_data_->lin_sys_schur->rhs_zero_entries();
+
+    this->mh_matrix_assembly_->assemble(eq_data_->dh_); // fill matrix
+
+    eq_data_->lin_sys_schur->finish_assembly();
+    eq_data_->lin_sys_schur->set_matrix_changed();
+}
+
+#endif /*RICHARDS_MOCKUP_IMPL_HH_ */
