c5605d5f3e77f22276b45fc4508f7f6ccb2d0cff
David Flanderka <t426.001@centrum.cz>
2026-08-12 11:29:21 +0200

    Implementation of int and uint operator.
    - add missing specialization of operator, it solves errors in integer and enum FieldFE

--- src/fem/patch_op_impl.hh
+++ src/fem/patch_op_impl.hh
@@ -117,14 +117,16 @@ inline Tensor PatchOp<3>::point_value<Tensor>(uint point_idx, uint i_dof) const 
 
 template <>
 template <>
-inline unsigned int PatchOp<3>::point_value<unsigned int>(FMT_UNUSED uint point_idx, FMT_UNUSED uint i_dof) const {
-    return 0;
+inline unsigned int PatchOp<3>::point_value<unsigned int>(uint point_idx, uint i_dof) const {
+	PatchPointValues<3> &ppv = patch_fe_->patch_point_vals_[domain_][dim_];
+    return result_(i_dof)(ppv.points_map_[point_idx]);
 }
 
 template <>
 template <>
-inline int PatchOp<3>::point_value<int>(FMT_UNUSED uint point_idx, FMT_UNUSED uint i_dof) const {
-    return 0;
+inline int PatchOp<3>::point_value<int>(uint point_idx, uint i_dof) const {
+    PatchPointValues<3> &ppv = patch_fe_->patch_point_vals_[domain_][dim_];
+    return result_(i_dof)(ppv.points_map_[point_idx]);
 }
 
 
