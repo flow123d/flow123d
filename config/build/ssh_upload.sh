d538c0380665bef3da06be27782456732d824484
Jan Brezina <jan.brezina@tul.cz>
2026-09-25 13:57:25 +0200

    Fix fterm and use SSH keys for uploads.

--- config/build/ssh_upload.sh
+++ config/build/ssh_upload.sh
@@ -5,22 +5,22 @@ set -e
 source_dir=$1   # local source dir which contents will be uploaded
 target_dir=$2   # target image on the remote, will be created
 ssh_remote=$3   # user@host
-ssh_pass=$4     # password
+# ssh_pass=$4     # password ; just SSH key login allowed
 
 tar -cf package.tar ${source_dir}
 
-pass_cmd="sshpass -p ${ssh_pass}"
+#pass_cmd="sshpass -p ${ssh_pass}"
 opt="-o PreferredAuthentications=password -o PubkeyAuthentication=no -o StrictHostKeyChecking=no"
 
 # Make directory with parents (-p also does not complain if the directory exists)
-${pass_cmd} ssh ${opt} ${ssh_remote} mkdir -p ${target_dir}
+ssh ${opt} ${ssh_remote} mkdir -p ${target_dir}
 
 # copy the tar archive
-${pass_cmd} scp -vr ${opt} package.tar ${ssh_remote}:${target_dir}/package.tar
+scp -vr ${opt} package.tar ${ssh_remote}:${target_dir}/package.tar
 
-# unter archive
-${pass_cmd} ssh ${opt} ${ssh_remote} \
+# untar archive
+ssh ${opt} ${ssh_remote} \
 tar -xf ${target_dir}/package.tar -C ${target_dir} --strip-components=1
 
 # remove archive
-${pass_cmd} ssh ${opt} ${ssh_remote} rm ${target_dir}/package.tar
+ssh ${opt} ${ssh_remote} rm ${target_dir}/package.tar
